34f159d4d00af62a0891a807c0ef2e1f9e238ee2
kenny_choo <kenny.choo@uzh.ch>
2018-06-12 15:17:12 -0400

    a

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kennychoo/Documents/UZH/Neural_Networks/netket
-# Build directory: /Users/kennychoo/Documents/UZH/Neural_Networks/netket/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Test")
