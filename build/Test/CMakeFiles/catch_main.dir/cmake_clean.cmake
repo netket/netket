32efd0ffc383c3892f4cbb7aee1bc94dbca7bf47
kenny_choo <kenny.choo@uzh.ch>
2018-06-12 15:17:12 -0400

    a

--- build/Test/CMakeFiles/catch_main.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/catch_main.dir/unit-tests.cc.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/catch_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
