45c4353754112d0fca08e2a8879e627d4eb302d5
guglielmo <guly@dhcp-192-033-102-052.ethz.ch>
2018-08-08 13:18:52 +0200

    add bosehubbard test in 2D

--- Test/Machine/machine_input_tests.hpp
+++ Test/Machine/machine_input_tests.hpp
@@ -62,14 +62,14 @@ std::vector<netket::json> GetMachineInputs() {
           {"Hamiltonian", {{"Name", "Heisenberg"}}}};
   input_tests.push_back(pars);
 
-  // Bose-Hubbard 1d with symmetric Jastrow machine
+  // Bose-Hubbard 2d with symmetric Jastrow machine
   pars = {{"Graph",
            {{"Name", "Hypercube"}, {"L", 8}, {"Dimension", 2}, {"Pbc", true}}},
           {"Machine", {{"Name", "JastrowSymm"}}},
           {"Hamiltonian", {{"Name", "BoseHubbard"}, {"U", 4.0}, {"Nmax", 4}}}};
   input_tests.push_back(pars);
 
-  // Bose-Hubbard 1d with non-symmetric Jastrow machine
+  // Bose-Hubbard 2d with non-symmetric Jastrow machine
   pars = {{"Graph",
            {{"Name", "Hypercube"}, {"L", 8}, {"Dimension", 2}, {"Pbc", true}}},
           {"Machine", {{"Name", "Jastrow"}}},
