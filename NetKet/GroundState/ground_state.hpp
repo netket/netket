cad4feb54baf286b0907711b50b38b65cfecc431
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2018-09-08 17:01:18 +0200

    Return reference of sparse matrix instead of value. Improve formatting.

--- NetKet/GroundState/ground_state.hpp
+++ NetKet/GroundState/ground_state.hpp
@@ -23,84 +23,83 @@
 #include "Observable/observable.hpp"
 #include "Optimizer/optimizer.hpp"
 
+#include "exact_diagonalization.hpp"
 #include "imaginary_time.hpp"
 #include "variational_montecarlo.hpp"
-#include "exact_diagonalization.hpp"
 
 namespace netket {
 
-  class GroundState {
-  public:
-    explicit GroundState(const json &pars) {
-      std::string method_name;
+class GroundState {
+ public:
+  explicit GroundState(const json &pars) {
+    std::string method_name;
 
-      if (FieldExists(pars, "GroundState")) {
-        method_name = FieldVal(pars["GroundState"], "Method", "GroundState");
-      } else if (FieldExists(pars, "Learning")) {
-        method_name = FieldVal(pars["Learning"], "Method", "Learning");
-        // DEPRECATED (to remove for v2.0.0)
-        WarningMessage()
+    if (FieldExists(pars, "GroundState")) {
+      method_name = FieldVal(pars["GroundState"], "Method", "GroundState");
+    } else if (FieldExists(pars, "Learning")) {
+      method_name = FieldVal(pars["Learning"], "Method", "Learning");
+      // DEPRECATED (to remove for v2.0.0)
+      WarningMessage()
           << "Use of the Learning section is "
-          "deprecated.\n Please use the dedicated GroundState section.\n";
-      } else {
-        std::stringstream s;
-        s << "The GroundState section has not been specified.\n";
-        throw InvalidInputError(s.str());
-      }
+             "deprecated.\n Please use the dedicated GroundState section.\n";
+    } else {
+      std::stringstream s;
+      s << "The GroundState section has not been specified.\n";
+      throw InvalidInputError(s.str());
+    }
 
-      Graph graph(pars);
-      Hamiltonian hamiltonian(graph, pars);
+    Graph graph(pars);
+    Hamiltonian hamiltonian(graph, pars);
 
-      if (method_name == "Gd" || method_name == "Sr") {
-        using MachineType = Machine<std::complex<double>>;
-        MachineType machine(graph, hamiltonian, pars);
+    if (method_name == "Gd" || method_name == "Sr") {
+      using MachineType = Machine<std::complex<double>>;
+      MachineType machine(graph, hamiltonian, pars);
 
-        Sampler<MachineType> sampler(graph, hamiltonian, machine, pars);
-        Optimizer optimizer(pars);
+      Sampler<MachineType> sampler(graph, hamiltonian, machine, pars);
+      Optimizer optimizer(pars);
 
-        VariationalMonteCarlo vmc(hamiltonian, sampler, optimizer, pars);
-        vmc.Run();
-
+      VariationalMonteCarlo vmc(hamiltonian, sampler, optimizer, pars);
+      vmc.Run();
 
     } else if (method_name == "ImaginaryTimePropagation") {
-        int size;
-        MPI_Comm_size(MPI_COMM_WORLD, &size);
-        if (size > 1) {
-          throw InvalidInputError(
+      int size;
+      MPI_Comm_size(MPI_COMM_WORLD, &size);
+      if (size > 1) {
+        throw InvalidInputError(
             "Imaginary time propagation method currently only supports a "
             "single MPI process.");
-        }
-
-        auto observables = Observable::FromJson(hamiltonian.GetHilbert(), pars);
-
-        const auto pars_gs = FieldVal(pars, "GroundState");
-        auto driver = ImaginaryTimePropagation::FromJson(hamiltonian,
-							 observables, pars_gs);
-
-        // Start with random initial vector
-        Eigen::VectorXcd initial =
-          Eigen::VectorXcd::Random(driver.GetDimension());
-        initial.normalize();
-
-        driver.Run(initial);
-
-      } else if (method_name == "Ed") {
-        std::vector<double> eigs = eigenvalues_lanczos(hamiltonian);
-
-        std::string file_base = FieldVal(pars["GroundState"], "OutputFile");
-        std::string file_name = file_base + std::string(".log");
-        std::ofstream file_ed(file_name);
-        json j(eigs);
-        file_ed << j << std::endl;
-        file_ed.close();
-
-      } else {
-        std::stringstream s;
-        s << "Unknown GroundState method: " << method_name;
-        throw InvalidInputError(s.str());
       }
+
+      auto observables = Observable::FromJson(hamiltonian.GetHilbert(), pars);
+
+      const auto pars_gs = FieldVal(pars, "GroundState");
+      auto driver =
+          ImaginaryTimePropagation::FromJson(hamiltonian, observables, pars_gs);
+
+      // Start with random initial vector
+      Eigen::VectorXcd initial =
+          Eigen::VectorXcd::Random(driver.GetDimension());
+      initial.normalize();
+
+      driver.Run(initial);
+
+    } else if (method_name == "Ed") {
+      std::vector<double> eigs = eigenvalues_lanczos(hamiltonian);
+
+      std::string file_base = FieldVal(pars["GroundState"], "OutputFile");
+      std::string file_name = file_base + std::string(".log");
+      std::ofstream file_ed(file_name);
+      json j(eigs);
+      file_ed << j << std::endl;
+      file_ed.close();
+
+    } else {
+      std::stringstream s;
+      s << "Unknown GroundState method: " << method_name;
+      throw InvalidInputError(s.str());
     }
-  };
+  }
+};
 
 }  // namespace netket
 
