a275b038bf76dd4840f5c75b8c9e08f7da9c7c66
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2018-11-03 19:21:04 -0400

    Fix assertion problem, and possibly travis issue

--- NetKet/Hilbert/bosons.hpp
+++ NetKet/Hilbert/bosons.hpp
@@ -129,7 +129,7 @@ class Boson : public AbstractHilbert {
     }
   }
 
-  bool CheckConstraint(Eigen::VectorXd &v) const {
+  bool CheckConstraint(const Eigen::VectorXd &v) const {
     int tot = 0;
     for (int i = 0; i < v.size(); i++) {
       tot += int(v(i));
