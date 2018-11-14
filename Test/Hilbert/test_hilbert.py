195ac27f69381e1517a4053535d22c3dcb696d8d
jamesETsmith <jasm3285@colorado.edu>
2018-11-13 21:42:36 -0700

    Finishing off the hilbert test. Adding bindings for HilbertIndex to pynetket

--- Test/Hilbert/test_hilbert.py
+++ Test/Hilbert/test_hilbert.py
@@ -1,3 +1,17 @@
+# Copyright 2018 The Simons Foundation, Inc. - All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import netket as nk
 import networkx as nx
 import numpy as np
@@ -40,47 +54,43 @@ hilberts["Heisenberg 1d"] = nk.operator.Heisenberg(hilbert=hi).GetHilbert()
 # Bose Hubbard
 g = nk.graph.Hypercube(L=20, ndim=1, pbc=True)
 hi = nk.hilbert.Boson(n_max=4, n_bosons=20, graph=g)
-hilberts["Bose Hubbards"] = nk.operator.BoseHubbard(
+hilberts["Bose Hubbard"] = nk.operator.BoseHubbard(
     U=4.0, hilbert=hi).GetHilbert()
-''' TODO (jamesETsmith)
-#  // Small hilbert spaces
-#  // Spin 1/2
-  pars = {{"Hilbert", {{"Name", "Spin"}, {"Nspins", 10}, {"S", 0.5}}}};
-  input_tests.push_back(pars);
 
-  // Spin 3
-  pars = {{"Hilbert", {{"Name", "Spin"}, {"Nspins", 4}, {"S", 3}}}};
-  input_tests.push_back(pars);
+#
+# Small hilbert space tests
+#
 
-  // Boson
-  pars = {{"Hilbert", {{"Name", "Boson"}, {"Nsites", 5}, {"Nmax", 3}}}};
-  input_tests.push_back(pars);
+# Spin 1/2
+hilberts["Spin 1/2 Small"] = nk.hilbert.Spin(
+    s=0.5, graph=nk.graph.Hypercube(L=10, ndim=1))
 
-  // Qubit
-  pars = {{"Hilbert", {{"Name", "Qubit"}, {"Nqubits", 11}}}};
-  input_tests.push_back(pars);
+# Spin 3
+hilberts["Spin 1/2 with total Sz Small"] = nk.hilbert.Spin(
+    s=3, total_sz=1.0, graph=nk.graph.Hypercube(L=4, ndim=1))
 
-  // Custom Hilbert
-  pars = {{"Hilbert", {{"QuantumNumbers", {-1232, 132, 0}}, {"Size", 5}}}};
-  input_tests.push_back(pars);
+# Boson
+hilberts["Boson Small"] = nk.hilbert.Boson(
+    n_max=3, graph=nk.graph.Hypercube(L=5, ndim=1))
 
-  // Heisenberg 1d
-  pars = {{"Graph",
-           {{"Name", "Hypercube"}, {"L", 9}, {"Dimension", 1}, {"Pbc", true}}},
-          {"Hamiltonian", {{"Name", "Heisenberg"}}}};
-  input_tests.push_back(pars);
+# Qubit
+hilberts["Qubit Small"] = nk.hilbert.Qubit(
+    graph=nk.graph.Hypercube(L=1, ndim=1))
 
-  // Bose Hubbard
-  pars = {{"Graph",
-           {{"Name", "Hypercube"}, {"L", 2}, {"Dimension", 1}, {"Pbc", false}}},
-          {"Hamiltonian", {{"Name", "BoseHubbard"}, {"U", 4.0}, {"Nmax", 4}}}};
-  input_tests.push_back(pars);
-'''
+# Custom Hilbert
+hilberts["Custom Hilbert Small"] = nk.hilbert.CustomHilbert(
+    local_states=[-1232, 132, 0], graph=nk.graph.Hypercube(L=5, ndim=1))
+
+#
+# Tests
+#
 
 
 def test_consistent_size():
+    """"""
+
     for name, hi in hilberts.items():
-        print("Hilbert test: %s" % name)
+        #print("Hilbert test: %s" % name)
         assert (hi.size() > 0)
         assert (hi.local_size() > 0)
         if hi.is_discrete():
@@ -90,6 +100,8 @@ def test_consistent_size():
 
 
 def test_random_states():
+    """"""
+
     for name, hi in hilberts.items():
         assert (hi.size() > 0)
         assert (hi.local_size() > 0)
@@ -100,19 +112,23 @@ def test_random_states():
             rg = nk.RandomEngine(seed=1234)
             local_states = hi.local_states()
 
-            #print(type(local_states))
             for i in range(100):
                 hi.random_vals(rstate, rg)
                 for state in rstate:
                     assert (state in local_states)
 
 
-""" TODO (jamesETsmith)
+#TODO (jamesETsmith)
 def test_mapping():
+    """"""
 
-    for hi in hilberts:
-        assert(hi.Size() > 0)
-        assert(hi.LocalSize() > 0)
+    for name, hi in hilberts.items():
+        assert (hi.size() > 0)
+        assert (hi.local_size() > 0)
 
-        if hi.Size() * np.log(hi.LocalSize() < np.log
-"""
+        if hi.size() * np.log(hi.local_size()) < np.log(nk.max_states):
+            hilb_index = nk.HilbertIndex(hi)
+
+            for k in range(hilb_index.n_states()):
+                state = hilb_index.number_to_state(k)
+                assert (hilb_index.state_to_number(state) == k)
