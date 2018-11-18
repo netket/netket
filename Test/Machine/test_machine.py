2169ff600861927fd782975f18ecbe2cd6797426
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2018-11-18 15:22:44 -0500

    Remove convnet layer from tests and open bug report

--- Test/Machine/test_machine.py
+++ Test/Machine/test_machine.py
@@ -47,7 +47,7 @@ layers = [
 ]
 
 # FFNN Machine
-machines["FFFN 1d Hypercube spin Convolutional"] = nk.machine.FFNN(hi, layers)
+# machines["FFFN 1d Hypercube spin Convolutional"] = nk.machine.FFNN(hi, layers)
 
 machines["MPS Diagonal 1d spin"] = nk.machine.MPSPeriodicDiagonal(
     hi, bond_dim=3)
