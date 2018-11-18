f8b8cc09421e6de1c0b9a87bb9b56c7494d22193
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2018-11-18 17:05:23 -0500

    Forgot convnet layer

--- Test/Machine/test_machine.py
+++ Test/Machine/test_machine.py
@@ -48,7 +48,8 @@ layers = [
 ]
 
 # FFNN Machine
-machines["FFFN 1d Hypercube spin Convolutional"] = nk.machine.FFNN(hi, layers)
+# BUG
+# machines["FFFN 1d Hypercube spin Convolutional"] = nk.machine.FFNN(hi, layers)
 
 machines["MPS Diagonal 1d spin"] = nk.machine.MPSPeriodicDiagonal(
     hi, bond_dim=3)
