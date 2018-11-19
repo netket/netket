ad81bf3798caac8d3a4293ed836d6ee0f19342af
Tom Westerhout <t.westerhout@student.ru.nl>
2018-11-19 22:04:43 +0100

    Proper testing of edges() and is_connected

--- Test/Graph/test_graph.py
+++ Test/Graph/test_graph.py
@@ -1,18 +1,44 @@
 import netket as nk
 import networkx as nx
-import numpy as np
 from mpi4py import MPI
+
 nxg = nx.star_graph(10)
 graphs = [
-    nk.graph.Hypercube(length=10, ndim=1, pbc=True),
-    nk.graph.Hypercube(length=4, ndim=2, pbc=True),
-    nk.graph.Hypercube(length=5, ndim=1, pbc=False),
+    nk.graph.Hypercube(length=10, n_dim=1, pbc=True),
+    nk.graph.Hypercube(length=4, n_dim=2, pbc=True),
+    nk.graph.Hypercube(length=5, n_dim=1, pbc=False),
     nk.graph.CustomGraph(nxg.edges())
 ]
 
+def coord2index(xs, length):
+    if isinstance(xs, int):
+        return xs
+    i = 0
+    scale = 1
+    for x in xs:
+        i += scale * x
+        scale *= length
+    return i
+
+def check_edges(length, n_dim, pbc):
+    x = nx.grid_graph(dim=[length]*n_dim, periodic=pbc)
+    x_edges = sorted([[coord2index(i, length) for i in edge] for edge in x.edges])
+    y = nk.graph.Hypercube(length=length, n_dim=n_dim, pbc=pbc)
+    y_edges = sorted(list(y.edges()))
+    assert x_edges == y_edges
+
+def test_edges_are_correct():
+    check_edges(1, 1, False)
+    check_edges(1, 2, False)
+    for length in [3, 4, 5]:
+        for dim in [1, 2, 3]:
+            for pbc in [True, False]:
+                check_edges(length, dim, pbc)
+    for pbc in [True, False]:
+        check_edges(3, 7, pbc)
 
 def tonx(graph):
-    adl = graph.AdjacencyList()
+    adl = graph.adjacency_list()
     i = 0
     edges = []
     for els in adl:
@@ -23,32 +49,27 @@ def tonx(graph):
         return nx.from_edgelist(edges)
 
     gx = nx.Graph()
-    for i in range(graph.Nsites()):
+    for i in range(graph.n_sites()):
         gx.add_node(i)
     return gx
 
-
 def test_size_is_positive():
     for graph in graphs:
-        assert graph.Nsites() > 0
-
+        assert graph.n_sites() > 0
 
 def test_is_connected():
-    for graph in graphs:
-        assert graph.IsConnected() == nx.is_connected(tonx(graph))
+    for i in range(5, 10):
+        for j in range(5, 30, 5):
+            x = nx.dense_gnm_random_graph(i, j)
+            y = nk.graph.CustomGraph(x.edges())
+            assert y.is_connected == nx.is_connected(x)
 
-# TODO(twesterhout): Not (yet) implemented.
-# def test_computes_distances():
-#     for graph in graphs:
-#         if (graph.IsConnected()):
-#             nxg = tonx(graph)
-#             d = graph.Distances(0)
-#             d1 = nx.shortest_path_length(nxg, source=0)
-#             for j, dist in enumerate(d):
-#                 assert dist == d1[j]
-#
-#             d = graph.AllDistances()
-#             d1 = dict(nx.shortest_path_length(nxg))
-#             for i in range(graph.Nsites()):
-#                 for j in range(graph.Nsites()):
-#                     assert d1[i][j] == d[i][j]
+def test_computes_distances():
+    for graph in graphs:
+        if (graph.is_connected):
+            nxg = tonx(graph)
+            d = graph.distances()
+            d1 = dict(nx.shortest_path_length(nxg))
+            for i in range(graph.n_sites()):
+                for j in range(graph.n_sites()):
+                    assert d1[i][j] == d[i][j]
