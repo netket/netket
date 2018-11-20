86a20be9780e3356d7d6d07054b9ff3832b0c279
kenny_choo <kenny.choo@uzh.ch>
2018-11-20 10:37:01 +0100

    edit comments

--- NetKet/Machine/ffnn.hpp
+++ NetKet/Machine/ffnn.hpp
@@ -161,12 +161,12 @@ class FFNN : public AbstractMachine<T> {
 
   void InitLookup(VisibleConstType v, LookupType &lt) override {
     if (lt.VVSize() == 0) {
-      lt.AddVV(1);                   // contains the output of layer 0
-      lt.AddVector(layersizes_[1]);  // contains the lookup of layer 0
+      lt.AddVV(1);                   // contains the lookup of layer 0
+      lt.AddVector(layersizes_[1]);  // contains the output of layer 0
       layers_[0]->InitLookup(v, lt.VV(0), lt.V(0));
       for (int i = 1; i < nlayer_; ++i) {
-        lt.AddVV(1);                       // contains the output of layer i
-        lt.AddVector(layersizes_[i + 1]);  // contains the lookup of layer i
+        lt.AddVV(1);                       // contains the lookup of layer i
+        lt.AddVector(layersizes_[i + 1]);  // contains the output of layer i
         layers_[i]->InitLookup(lt.V(i - 1), lt.VV(i), lt.V(i));
       }
     } else {
