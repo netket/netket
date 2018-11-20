3d23ca9ed40f1c95be5093f58c169c92eb4171e6
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2018-11-20 14:06:19 -0500

    Revert changes to travis and setup

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ import sys
 import platform
 import subprocess
 
-
-def check_env_flag(name, default=''):
-    return os.getenv(name, default).upper() in ['ON', '1', 'YES', 'TRUE', 'Y']
-
-
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
@@ -21,7 +16,6 @@ class CMakeExtension(Extension):
 
 
 class CMakeBuild(build_ext):
-
     def run(self):
         try:
             out = subprocess.check_output(['cmake', '--version'])
@@ -30,8 +24,7 @@ class CMakeBuild(build_ext):
                                ", ".join(e.name for e in self.extensions))
 
         if platform.system() == "Windows":
-            cmake_version = LooseVersion(
-                re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
             if cmake_version < '3.1.0':
                 raise RuntimeError("CMake >= 3.1.0 is required on Windows")
 
@@ -39,32 +32,20 @@ class CMakeBuild(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(
-            self.get_ext_fullpath(ext.name)))
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
-        self.debug = check_env_flag('DEBUG')
-        self.sanitizer = check_env_flag('SANITIZER')
-
         cfg = 'Debug' if self.debug else 'Release'
-
         build_args = ['--config', cfg]
 
         if platform.system() == "Windows":
-            cmake_args += [
-                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
             build_args += ['--', '/m']
         else:
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            if(self.sanitizer):
-                print("---------Building with sanitizer support---------")
-                cmake_args += ['-DNETKET_Sanitizer=ON']
-            if(self.debug):
-                print("----------Building in debug mode-------------")
-                cmake_args += ['-DCMAKE_CXX_FLAGS_RELEASE=-O3']
             build_args += ['--', '-j2']
 
         env = os.environ.copy()
@@ -72,11 +53,8 @@ class CMakeBuild(build_ext):
                                                               self.distribution.get_version())
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] +
-                              cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] +
-                              build_args, cwd=self.build_temp)
-
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 setup(
     name='netket',
