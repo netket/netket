ebdedcbe25bbfb8f05fc402782393c4a1776802f
jamesETsmith <jasm3285@colorado.edu>
2018-11-21 09:47:58 -0700

    Adding python tests for graph hamiltonian.

--- Test/Operator/test_operator.py
+++ Test/Operator/test_operator.py
@@ -22,6 +22,38 @@ operators["Bose Hubbard"] = nk.operator.BoseHubbard(U=4.0, hilbert=hi)
 
 # Graph Hamiltonian
 # TODO (jamesETsmith)
+"""
+  std::vector<std::vector<double>> sigmax = {{0, 1}, {1, 0}};
+  std::vector<std::vector<double>> mszsz = {
+      {1, 0, 0, 0}, {0, -1, 0, 0}, {0, 0, -1, 0}, {0, 0, 0, 1}};
+
+  std::vector<std::vector<int>> edges = {
+      {0, 1},   {1, 2},   {2, 3},   {3, 4},   {4, 5},   {5, 6},   {6, 7},
+      {7, 8},   {8, 9},   {9, 10},  {10, 11}, {11, 12}, {12, 13}, {13, 14},
+      {14, 15}, {15, 16}, {16, 17}, {17, 18}, {18, 19}, {19, 0}};
+
+  pars.clear();
+
+  pars["Graph"]["Edges"] = edges;
+  pars["Hilbert"]["QuantumNumbers"] = {1, -1};
+  pars["Hilbert"]["Size"] = edges.size();
+  pars["Hamiltonian"]["Name"] = "Graph";
+  pars["Hamiltonian"]["SiteOps"] = {sigmax};
+  pars["Hamiltonian"]["BondOps"] = {mszsz};
+  pars["Hamiltonian"]["BondOpColors"] = {0};
+  input_tests.push_back(pars);
+"""
+sigmax = [[0, 1], [1, 0]]
+mszsz = [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]]
+edges = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7], [7, 8],
+         [8, 9], [9, 10], [10, 11], [11, 12], [12, 13], [13, 14], [14, 15],
+         [15, 16], [16, 17], [17, 18], [18, 19], [19, 0]]
+
+g = nk.graph.CustomGraph(edges=edges)
+hi = nk.hilbert.CustomHilbert(local_states=[-1, 1], graph=g)
+ha = nk.operator.GraphHamiltonian(
+    hi, siteops=[sigmax], bondops=[mszsz], bondops_colors=[0])
+operators["Graph Hamiltonian"] = ha
 
 # Custom Hamiltonian
 # TODO (jamesETsmith)
@@ -52,4 +84,4 @@ def test_produce_elements_in_hilbert():
                 hi.update_conf(rstatet, connector, newconf)
 
                 for rs in rstatet:
-                    assert(rs in local_states)
+                    assert (rs in local_states)
