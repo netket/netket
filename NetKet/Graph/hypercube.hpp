e69d81c10c422812114594196696d42850a0fb0f
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2018-12-03 11:57:58 -0500

    Remove dangerous move returns

--- NetKet/Graph/hypercube.hpp
+++ NetKet/Graph/hypercube.hpp
@@ -136,9 +136,7 @@ class Hypercube : public AbstractGraph {
 
   int Ndim() const noexcept { return n_dim_; }
 
-  const std::vector<Edge> Edges() const noexcept override {
-    return std::move(edges_);
-  }
+  const std::vector<Edge> Edges() const noexcept override { return edges_; }
 
   std::vector<std::vector<int>> AdjacencyList() const override {
     return detail::AdjacencyListFromEdges(Edges(), Nsites());
@@ -153,9 +151,7 @@ class Hypercube : public AbstractGraph {
   bool Pbc() const noexcept { return pbc_; }
 
   // Returns map of the edge and its respective color
-  const ColorMap EdgeColors() const noexcept override {
-    return std::move(colors_);
-  }
+  const ColorMap EdgeColors() const noexcept override { return colors_; }
 
   std::vector<std::vector<int>> SymmetryTable() const override {
     return symm_table_;
