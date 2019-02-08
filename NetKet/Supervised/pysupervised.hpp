6207b3d7db1ca71c2e6169e2909ae3cfffe5b098
ShHsLin <shenghsuan.lin@tum.de>
2019-02-08 17:48:04 +0100

    add comment/docstrings

--- NetKet/Supervised/pysupervised.hpp
+++ NetKet/Supervised/pysupervised.hpp
@@ -32,7 +32,7 @@ namespace netket {
 void AddSupervisedModule(py::module &m) {
   auto subm = m.def_submodule("supervised");
 
-  py::class_<Supervised>(subm, "supervised")
+  py::class_<Supervised>(subm, "supervised", R"EOF(Supervised learning scheme to learn data, i.e. the given state, by stochastic gradient descent with log overlap loss or MSE loss.)EOF")
       .def(py::init([](MachineType &ma, AbstractOptimizer &op, int batch_size,
                        std::vector<Eigen::VectorXd> samples,
                        std::vector<Eigen::VectorXcd> targets) {
@@ -44,14 +44,45 @@ void AddSupervisedModule(py::module &m) {
            }),
            py::keep_alive<1, 2>(), py::keep_alive<1, 3>(), py::arg("machine"),
            py::arg("optimizer"), py::arg("batch_size"), py::arg("samples"),
-           py::arg("targets"))
-      .def_property_readonly("loss_log_overlap", &Supervised::GetLogOverlap)
-      .def_property_readonly("loss_mse", &Supervised::GetMse)
-      .def_property_readonly("loss_mse_log", &Supervised::GetMseLog)
-      .def("run", &Supervised::Run, py::arg("niter_opt"),
-           py::arg("loss_function") = "MSE", py::arg("output_prefix") = "output",
-	   py::arg("save_params_every") = 50)
-      .def("iterate", &Supervised::Iterate, py::arg("loss_function") = "MSE");
+           py::arg("targets"),
+	   R"EOF(
+           Construct a Supervised object given a machine, an optimizer, batch size and
+           data, including samples and targets.
+
+           Args:
+               ma: The machine representing the wave function.
+               op: The optimizer object that determines how the SGD optimization.
+               batch_size: The batch size used in SGD.
+               samples: The input data, i.e. many-body basis.
+               targets: The output label, i.e. amplitude of the corresponding basis.
+
+           )EOF")
+      .def_property_readonly("loss_log_overlap", &Supervised::GetLogOverlap,
+           R"EOF(Log Overlap loss: The negative log fidelity.)EOF")
+      .def_property_readonly("loss_mse", &Supervised::GetMse,
+           R"EOF(MSE loss: The mean square error of amplitudes.)EOF")
+      .def_property_readonly("loss_mse_log", &Supervised::GetMseLog,
+           R"EOF(MSE log loss: The mean square error of the log of amplitudes.)EOF")
+      .def("run", &Supervised::Run, py::arg("n_iter"),
+           py::arg("loss_function") = "Overlap_phi", py::arg("output_prefix") = "output",
+	   py::arg("save_params_every") = 50, R"EOF(
+           Run supervised learning.
+
+           Args:
+               n_iter: The number of iterations for running.
+               loss_function: The loss function choosing for learning, Default: Overlap_phi
+               output_prefix: The output file name, without extension.
+               save_params_every: Frequency to dump wavefunction parameters. The default is 50.
+
+           )EOF")
+      .def("iterate", &Supervised::Iterate, py::arg("loss_function") = "Overlap_phi", R"EOF(
+           Run one iteration of supervised learning. This should be helpful for testing and
+           having self-defined control sequence in python.
+
+           Args:
+               loss_function: The loss function choosing for learning, Default: Overlap_phi
+
+           )EOF");
 }
 
 }  // namespace netket
