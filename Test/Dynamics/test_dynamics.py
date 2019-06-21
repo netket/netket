97b3586081790299c05754a55922dd509cb66ae7
Damian Hofmann <damian.hofmann@mpsd.mpg.de>
2019-06-21 15:46:53 -0400

    Fix dynamics test

--- Test/Dynamics/test_dynamics.py
+++ Test/Dynamics/test_dynamics.py
@@ -31,7 +31,7 @@ def overlap(phi, psi):
 
 def test_real_time_evolution():
     hi, ham, ts, psi0 = _setup_model()
-    ham_matrix = ham.to_dense()
+    ham_matrix = ham.to_sparse()
 
     driver = ExactTimePropagation(
         ham, ts, t0=0.0, initial_state=psi0, propagation_type="real"
@@ -43,7 +43,7 @@ def test_real_time_evolution():
     assert driver.t == TIME
 
     def rhs_real(t, x):
-        return -1.0j * ham_matrix @ x
+        return -1.0j * ham_matrix.dot(x)
 
     res = solve_ivp(rhs_real, (0.0, TIME), psi0, atol=ATOL, rtol=RTOL)
     psi_scipy = res.y[:, -1]
@@ -54,7 +54,7 @@ def test_real_time_evolution():
 
 def test_imag_time_evolution():
     hi, ham, ts, psi0 = _setup_model()
-    ham_matrix = ham.to_dense()
+    ham_matrix = ham.to_sparse()
 
     driver = ExactTimePropagation(
         ham, ts, t0=0.0, initial_state=psi0, propagation_type="imaginary"
