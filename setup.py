e55b1139787124c8639912dfd3676ade42f7403f
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2019-06-25 12:50:13 -0400

    Add requirements to setup.py and change ED example to use scipy

--- setup.py
+++ setup.py
@@ -165,4 +165,5 @@ setup(
          neural networks and machine learning techniques.""",
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
+    install_requires=["numpy>=1.16", "cmake>3.1", "scipy>=1.3"],
 )
