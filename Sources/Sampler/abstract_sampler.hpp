452bb5206ca19a2c8c21bde78784fb3d85ddb622
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2019-08-15 12:41:49 -0400

    Address comments in #279

--- Sources/Sampler/abstract_sampler.hpp
+++ Sources/Sampler/abstract_sampler.hpp
@@ -94,61 +94,6 @@ inline Eigen::Ref<const Eigen::VectorXd> VisibleLegacy(
   return visible.row(0);
 }
 
-/// \brief A helper class to keep track of the logarithm of the wavefunction.
-///
-/// It uses Kahan's summation algorithm (Neumaier's adaptation of it) under
-/// the hood to reduce accumulation errors for long Markov chains.
-class LogValAccumulator {
-  using Real = long double;
-
-  struct Accumulator {
-    Real sum;
-    Real correction;
-
-    /// \brief Adds \p value to the accumulator.
-    Accumulator& operator+=(Real value) noexcept {
-      const auto t = sum + value;
-      correction += std::abs(sum) >= std::abs(value) ? (sum - t) + value
-                                                     : (value - t) + sum;
-      sum = t;
-      return *this;
-    }
-
-    /// \brief Conversion to `double` as a way to get the current sum.
-    explicit operator double() const noexcept {
-      return static_cast<double>(sum + correction);
-    }
-  };
-
-  Accumulator real_;
-  Accumulator imag_;
-  mutable Complex result_;
-
- public:
-  explicit LogValAccumulator(const Complex log_val = {0, 0})
-      : real_{log_val.real(), 0}, imag_{log_val.imag(), 0}, result_{0, 0} {}
-
-  LogValAccumulator(const LogValAccumulator&) = default;
-  LogValAccumulator(LogValAccumulator&&) = default;
-  LogValAccumulator& operator=(const LogValAccumulator&) = default;
-  LogValAccumulator& operator=(LogValAccumulator&&) = default;
-
-  const Complex& LogVal() const noexcept {
-    result_ = Complex{static_cast<double>(real_), static_cast<double>(imag_)};
-    return result_;
-  }
-
-  LogValAccumulator& operator=(const Complex log_val) noexcept {
-    return *this = LogValAccumulator{log_val};
-  }
-
-  LogValAccumulator& operator+=(const Complex log_val) noexcept {
-    real_ += log_val.real();
-    imag_ += log_val.imag();
-    return *this;
-  }
-};
-
 namespace detail {
 inline Index CheckBatchSize(const char* func, const Index batch_size) {
   if (batch_size <= 0) {
---
+++ Sources/Sampler/local_kernel.cc
@@ -0,0 +1,54 @@
+// Copyright 2018 The Simons Foundation, Inc. - All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "local_kernel.hpp"
+
+namespace netket {
+
+LocalKernel::LocalKernel(const AbstractMachine& psi)
+    : local_states_(psi.GetHilbert().LocalStates()),
+      n_states_(psi.GetHilbert().LocalSize()),
+      nv_(psi.GetHilbert().Size()) {
+  NETKET_CHECK(psi.GetHilbert().IsDiscrete(), InvalidInputError,
+               "Local Kernel sampler works only for discrete "
+               "Hilbert spaces");
+
+  // operator() relies on the fact that locat_states_ are sorted.
+  std::sort(local_states_.begin(), local_states_.end());
+}
+
+void LocalKernel::operator()(
+    Eigen::Ref<const RowMatrix<double>> v, Eigen::Ref<RowMatrix<double>> vnew,
+    Eigen::Ref<Eigen::ArrayXd> log_acceptance_correction) {
+  vnew = v;
+
+  for (int i = 0; i < v.rows(); i++) {
+    // picking a random site to be changed
+    Index si =
+        std::uniform_int_distribution<Index>(0, nv_ - 1)(GetRandomEngine());
+
+    // There are `local_states_.size() - 1` possible values (minus one is
+    // because we don't want to stay in the same state). Thus first, we
+    // generate a random number in `[0, local_states_.size() - 2]`. Next step
+    // is to transform the result to avoid the gap.
+    Index rs = std::uniform_int_distribution<Index>(
+        0, n_states_ - 2)(GetRandomEngine());
+
+    vnew(i, si) = local_states_[rs + (local_states_[rs] >= v(i, si))];
+  }
+
+  log_acceptance_correction.setZero();
+}
+
+}  // namespace netket
