18f2e61ecb5847fe850a0f77f4322a8cac24a037
Filippo Vicentini <filippovicentini@gmail.com>
2019-11-12 18:16:43 +0100

    Add Doubled Hilbert space

--- Sources/Hilbert/doubled_hilbert.cc
+++ Sources/Hilbert/doubled_hilbert.cc
@@ -3,3 +3,92 @@
 //
 
 #include "doubled_hilbert.hpp"
+
+namespace netket {
+
+DoubledHilbert::DoubledHilbert(
+    std::shared_ptr<const AbstractHilbert> hilbert,
+    std::unique_ptr<const AbstractGraph> doubled_graph)
+    : graph_doubled_(std::move(doubled_graph)),
+      hilbert_physical_(std::move(hilbert)) {
+  size_ = graph_doubled_->Size();
+}
+
+bool DoubledHilbert::IsDiscrete() const {
+  return hilbert_physical_->IsDiscrete();
+}
+
+int DoubledHilbert::LocalSize() const { return hilbert_physical_->LocalSize(); }
+
+int DoubledHilbert::Size() const {
+  return size_;  // it is 2 times the number of physical sites
+}
+
+int DoubledHilbert::SizePhysical() const { return hilbert_physical_->Size(); }
+
+std::vector<double> DoubledHilbert::LocalStates() const {
+  return hilbert_physical_->LocalStates();
+}
+
+void DoubledHilbert::RandomVals(Eigen::Ref<Eigen::VectorXd> state,
+                                netket::default_random_engine& rgen) const {
+  assert(state.size() == size_);
+
+  auto N = SizePhysical();
+  hilbert_physical_->RandomVals(state.head(N), rgen);
+  hilbert_physical_->RandomVals(state.tail(N), rgen);
+}
+
+void DoubledHilbert::UpdateConf(Eigen::Ref<Eigen::VectorXd> v,
+                                nonstd::span<const int> tochange,
+                                nonstd::span<const double> newconf) const {
+  auto N = SizePhysical();
+  hilbert_physical_->UpdateConfRowCol(v.head(N), v.tail(N), tochange, newconf);
+}
+
+void DoubledHilbert::RandomValsRows(Eigen::Ref<Eigen::VectorXd> state,
+                                    netket::default_random_engine& rgen) const {
+  assert(state.size() == size_);
+
+  hilbert_physical_->RandomVals(state.head(SizePhysical()), rgen);
+}
+
+void DoubledHilbert::RandomValsCols(Eigen::Ref<Eigen::VectorXd> state,
+                                    netket::default_random_engine& rgen) const {
+  assert(state.size() == size_);
+
+  hilbert_physical_->RandomVals(state.tail(SizePhysical()), rgen);
+}
+
+void DoubledHilbert::RandomValsPhysical(
+    Eigen::Ref<Eigen::VectorXd> state,
+    netket::default_random_engine& rgen) const {
+  assert(state.size() == size_);
+
+  hilbert_physical_->RandomVals(state, rgen);
+}
+
+void DoubledHilbert::UpdateConfPhysical(
+    Eigen::Ref<Eigen::VectorXd> v, nonstd::span<const int> tochange,
+    nonstd::span<const double> newconf) const {
+  hilbert_physical_->UpdateConf(v, tochange, newconf);
+}
+
+const AbstractHilbert& DoubledHilbert::GetHilbertPhysical() const noexcept {
+  return *hilbert_physical_;
+}
+
+std::shared_ptr<const AbstractHilbert>
+DoubledHilbert::GetHilbertPhysicalShared() const {
+  return hilbert_physical_;
+}
+
+const AbstractGraph& DoubledHilbert::GetGraphPhysical() const noexcept {
+  return hilbert_physical_->GetGraph();
+}
+
+const AbstractGraph& DoubledHilbert::GetGraph() const noexcept {
+  return *graph_doubled_;
+}
+
+}  // namespace netket
--- Sources/Hilbert/doubled_hilbert.hpp
+++ Sources/Hilbert/doubled_hilbert.hpp
@@ -5,6 +5,60 @@
 #ifndef NETKET_DOUBLED_HILBERT_HPP
 #define NETKET_DOUBLED_HILBERT_HPP
 
-class doubled_hilbert {};
+#include <utility>
+
+#include "Graph/abstract_graph.hpp"
+#include "Graph/doubled_graph.hpp"
+#include "Hilbert/abstract_hilbert.hpp"
+
+namespace netket {
+
+class DoubledHilbert : public AbstractHilbert {
+  std::unique_ptr<const AbstractGraph> graph_doubled_;
+  std::shared_ptr<const AbstractHilbert> hilbert_physical_;
+
+  int size_;
+
+ protected:
+  DoubledHilbert(std::shared_ptr<const AbstractHilbert> hilbert,
+                 std::unique_ptr<const AbstractGraph> doubled_graph);
+
+ public:
+  explicit DoubledHilbert(std::shared_ptr<const AbstractHilbert> hilbert)
+      : DoubledHilbert(hilbert, DoubledGraph(hilbert->GetGraph())){};
+
+  // -- DoubledHilbert Interface  -- //
+  int SizePhysical() const;
+  const AbstractGraph &GetGraphPhysical() const noexcept;
+  const AbstractHilbert &GetHilbertPhysical() const noexcept;
+  std::shared_ptr<const AbstractHilbert> GetHilbertPhysicalShared() const;
+
+  void RandomValsRows(Eigen::Ref<Eigen::VectorXd> state,
+                      netket::default_random_engine &rgen) const;
+  void RandomValsCols(Eigen::Ref<Eigen::VectorXd> state,
+                      netket::default_random_engine &rgen) const;
+  void RandomValsPhysical(Eigen::Ref<Eigen::VectorXd> state,
+                          default_random_engine &rgen) const;
+
+  void UpdateConfPhysical(Eigen::Ref<Eigen::VectorXd> v,
+                          nonstd::span<const int> tochange,
+                          nonstd::span<const double> newconf) const;
+
+  // -- AbstractHilbert interface -- //
+
+  bool IsDiscrete() const override;
+  int LocalSize() const override;
+  int Size() const override;
+  std::vector<double> LocalStates() const override;
+  const AbstractGraph &GetGraph() const noexcept override;
+
+  void RandomVals(Eigen::Ref<Eigen::VectorXd> state,
+                  netket::default_random_engine &rgen) const override;
+
+  void UpdateConf(Eigen::Ref<Eigen::VectorXd> v,
+                  nonstd::span<const int> tochange,
+                  nonstd::span<const double> newconf) const override;
+};
+}  // namespace netket
 
 #endif  // NETKET_DOUBLED_HILBERT_HPP
