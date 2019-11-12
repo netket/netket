151b18e55da287735a1a6272b34f7ddba35f573d
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2019-11-11 19:04:52 -0500

    Add back absolute_import

--- netket/__init__.py
+++ netket/__init__.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from __future__ import absolute_import
+
 from . import (
     _C_netket,
     dynamics,
