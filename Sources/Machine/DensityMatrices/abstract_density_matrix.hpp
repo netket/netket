abaa888ac7863ec39e39c8a6eb7d430375e2365a
Filippo Vicentini <filippovicentini@gmail.com>
2019-11-12 02:36:04 +0100

    move `DoubledGraph` to /Graph folders

--- Sources/Machine/DensityMatrices/abstract_density_matrix.hpp
+++ Sources/Machine/DensityMatrices/abstract_density_matrix.hpp
@@ -16,7 +16,7 @@
 #ifndef NETKET_ABSTRACT_DENSITY_MATRIX_HPP
 #define NETKET_ABSTRACT_DENSITY_MATRIX_HPP
 
-#include "Graph/custom_graph.hpp"
+#include "Graph/doubled_graph.hpp"
 #include "Hilbert/custom_hilbert.hpp"
 #include "Machine/abstract_machine.hpp"
 #include "Utils/memory_utils.hpp"
@@ -34,46 +34,6 @@ class AbstractDensityMatrix : public AbstractMachine {
   std::unique_ptr<const AbstractGraph> graph_doubled_;
 
   using Edge = AbstractGraph::Edge;
-
-  /**
-   * Returns the disjoint union G of a graph g with itself. The resulting graph
-   * has twice the number of vertices and edges of g.
-   * The automorpisms of G are the automorphisms of g applied identically
-   * to both it's subgraphs.
-   */
-  static std::unique_ptr<CustomGraph> DoubledGraph(const AbstractGraph &graph) {
-    auto n_sites = graph.Nsites();
-    std::vector<Edge> d_edges(graph.Edges().size());
-    auto eclist = graph.EdgeColors();
-
-    // same graph
-    auto d_eclist = graph.EdgeColors();
-    for (auto edge : graph.Edges()) {
-      d_edges.push_back(edge);
-    }
-
-    // second graph
-    for (auto edge : graph.Edges()) {
-      Edge new_edge = edge;
-      new_edge[0] += n_sites;
-      new_edge[1] += n_sites;
-
-      d_edges.push_back(new_edge);
-      d_eclist.emplace(new_edge, eclist[edge]);
-    }
-
-    std::vector<std::vector<int>> d_automorphisms;
-    for (auto automorphism : graph.SymmetryTable()) {
-      std::vector<int> d_automorphism = automorphism;
-      for (auto s : automorphism) {
-        d_automorphism.push_back(s + n_sites);
-      }
-      d_automorphisms.push_back(d_automorphism);
-    }
-
-    return make_unique<CustomGraph>(
-        CustomGraph(d_edges, d_eclist, d_automorphisms));
-  }
 
  protected:
   AbstractDensityMatrix(std::shared_ptr<const AbstractHilbert> physical_hilbert,
