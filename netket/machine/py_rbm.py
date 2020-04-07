f15a895daafbee6a439d0bb9cc0eae386dc34f32
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2020-04-07 17:41:37 -0400

    Write Rbm kernel in numba

--- netket/machine/py_rbm.py
+++ netket/machine/py_rbm.py
@@ -14,46 +14,123 @@
 
 from .abstract_machine import AbstractMachine
 import numpy as _np
-from netket.utils import sum_log_cosh_complex
-from .._C_netket.machine import RbmSpinKernel
+
+from numba import jit, optional, jitclass, int64, complex128
 
 __all__ = ["PyRbm"]
 
 
+@jit(fastmath=True)
+def _log_cosh_sum(x, out):
+    x = x * _np.sign(x.real)
+    for i in range(x.shape[0]):
+        out[i] = _np.sum(x[i] - _np.log(2.) +
+                         _np.log(1. + _np.exp(-2. * x[i])))
+    return out
+
+
+spec = [
+    ('_n_visible', int64),
+    ('_n_hidden', int64),
+    ('_W', complex128[:, ::1]),
+    ('_a', optional(complex128[::1])),
+    ('_b', optional(complex128[::1])),
+    ('_r', complex128[:, ::1]),
+]
+
+
+@jitclass(spec)
+class RbmSpinKernel():
+    def __init__(self, W, a, b):
+        self._n_visible = W.shape[1]
+        self._n_hidden = W.shape[0]
+        self._W = W
+        self._a = a
+        self._b = b
+        self._r = _np.empty((1, self._n_hidden), dtype=_np.complex128)
+
+    def log_val(self, x, out):
+        if(out is None):
+            out = _np.empty(x.shape[0], dtype=_np.complex128)
+        self._r = x.dot(self._W.T)
+
+        if self._b is None:
+            _log_cosh_sum(self._r, out)
+        else:
+            _log_cosh_sum(self._r + self._b, out)
+
+        if self._a is not None:
+            out = out + x.dot(self._a)
+
+        return out
+
+
 class PyRbm(AbstractMachine):
-    """
-    __Do not use me in production code!__
+    r"""
+    A fully connected Restricted Boltzmann Machine (RBM). This type of
+    RBM has spin 1/2 hidden units and is defined by:
 
-    A proof of concept implementation of a complex-valued RBM in pure Python.
+    .. math:: \Psi(s_1,\dots s_N) = e^{\sum_i^N a_i s_i} \times \Pi_{j=1}^M \cosh
+     \left(\sum_i^N W_{ij} s_i + b_j \right)
 
-    This class can be used as a drop-in replacement for `RbmSpin`.
+    for arbitrary local quantum numbers :math:`s_i`.
     """
 
     def __init__(
-        self, hilbert, alpha=None, use_visible_bias=True, use_hidden_bias=True
+        self, hilbert, n_hidden=None, alpha=None, use_visible_bias=True, use_hidden_bias=True
     ):
-        r"""Constructs a new RBM.
+        r"""
+        Constructs a new ``RbmSpin`` machine:
 
         Args:
-            hilbert: Hilbert space.
-            alpha: `alpha * hilbert.size` is the number of hidden spins.
-            use_visible_bias: specifies whether to use a bias for visible
-                              spins.
-            use_hidden_bias: specifies whether to use a bias for hidden spins.
+           hilbert: Hilbert space object for the system.
+           n_hidden: The number of hidden spin units. If n_hidden=None, the number
+                   of hidden units is deduced from the parameter alpha.
+           alpha: `alpha * hilbert.size` is the number of hidden spins.
+                   If alpha=None, the number of hidden units must
+                   be explicitely set in the argument n_hidden.
+           use_visible_bias: If ``True`` then there would be a
+                            bias on the visible units.
+                            Default ``True``.
+           use_hidden_bias: If ``True`` then there would be a
+                           bias on the visible units.
+                           Default ``True``.
+
+        Examples:
+           A ``RbmSpin`` machine with hidden unit density
+           alpha = 2 for a one-dimensional L=20 spin-half system:
+
+           >>> from netket.machine import RbmSpin
+           >>> from netket.hilbert import Spin
+           >>> from netket.graph import Hypercube
+           >>> g = Hypercube(length=20, n_dim=1)
+           >>> hi = Spin(s=0.5, total_sz=0, graph=g)
+           >>> ma = RbmSpin(hilbert=hi,alpha=2)
+           >>> print(ma.n_par)
+           860
         """
 
         n = hilbert.size
         if alpha < 0:
             raise ValueError("`alpha` should be non-negative")
-        m = int(round(alpha * n))
+
+        if alpha is None:
+            m = n_hidden
+        else:
+            m = int(round(alpha * n))
+            if n_hidden is not None:
+                if n_hidden != m:
+                    raise RuntimeError('''n_hidden is inconsistent with the given alpha.
+                                       Remove one of the two or provide consistent values.''')
+
         self._w = _np.empty([m, n], dtype=_np.complex128)
-        self._a = _np.empty(n, dtype=_np.complex128) if use_visible_bias else None
-        self._b = _np.empty(m, dtype=_np.complex128) if use_hidden_bias else None
+        self._a = _np.empty(
+            n, dtype=_np.complex128) if use_visible_bias else None
+        self._b = _np.empty(
+            m, dtype=_np.complex128) if use_hidden_bias else None
 
         self.n_hidden = m
         self.n_visible = n
-
-        self._r = _np.empty((1, m), dtype=_np.complex128, order="C")
 
         self._npar = (
             self._w.size
@@ -61,36 +138,20 @@ class PyRbm(AbstractMachine):
             + (self._b.size if self._b is not None else 0)
         )
 
-        self._kernel = RbmSpinKernel()
+        self._kernel = RbmSpinKernel(self._w, self._a, self._b)
 
         super().__init__(hilbert)
 
     @property
     def n_par(self):
-        r"""Returns the number of parameters in the machine. We just sum the
-        sizes of all the tensors we hold.
-        """
+        r"""The number of variational parameters in the machine."""
         return self._npar
 
     def log_val(self, x, out=None):
         r"""Computes the logarithm of the wave function given a spin
         configuration ``x``.
         """
-        if out is None:
-            out = _np.empty(x.shape[0], dtype=_np.complex128)
-
-        self._kernel.log_val(x, out, self._w, self._a, self._b)
-
-        # self._r = x.dot(self._w.T)
-        # if self._b is not None:
-        #     self._r += self._b
-        #
-        # sum_log_cosh_complex(self._r, out)
-        #
-        # if self._a is not None:
-        #     out += _np.dot(x, self._a)
-
-        return out
+        return self._kernel.log_val(x.astype(dtype=_np.complex128), out)
 
     def der_log(self, x, out=None):
 
@@ -101,7 +162,7 @@ class PyRbm(AbstractMachine):
 
         i = 0
         if self._a is not None:
-            out[:, i : i + x.shape[1]] = x
+            out[:, i: i + x.shape[1]] = x
             i += self.n_visible
 
         r = _np.dot(x, self._w.T)
@@ -110,10 +171,10 @@ class PyRbm(AbstractMachine):
         _np.tanh(r, out=r)
 
         if self._b is not None:
-            out[:, i : i + self.n_hidden] = r
+            out[:, i: i + self.n_hidden] = r
             i += self.n_hidden
 
-        t = out[:, i : i + self._w.size]
+        t = out[:, i: i + self._w.size]
         t.shape = (batch_size, self._w.shape[0], self._w.shape[1])
         _np.einsum("ij,il->ijl", r, x, out=t)
 
@@ -124,12 +185,13 @@ class PyRbm(AbstractMachine):
 
     @property
     def is_holomorphic(self):
-        r"""Complex valued RBM a holomorphic function.
+        r"""Complex valued RBM is a holomorphic function.
         """
         return True
 
     @property
     def state_dict(self):
+        r"""A dictionary containing the parameters of this machine"""
         from collections import OrderedDict
 
         od = OrderedDict()
