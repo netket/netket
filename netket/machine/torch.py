2260429a26be22473f7cf32780b9d8cd0e40dc13
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2020-04-16 08:51:32 -0400

    black format

--- netket/machine/torch.py
+++ netket/machine/torch.py
@@ -6,9 +6,7 @@ import warnings
 
 def _get_number_parameters(m):
     r"""Returns total number of variational parameters in a torch.nn.Module."""
-    return sum(
-        map(lambda p: p.numel(), _get_differentiable_parameters(m))
-    )
+    return sum(map(lambda p: p.numel(), _get_differentiable_parameters(m)))
 
 
 def _get_differentiable_parameters(m):
@@ -18,8 +16,7 @@ def _get_differentiable_parameters(m):
 
 class Torch(AbstractMachine):
     def __init__(self, module, hilbert):
-        self._module = _torch.jit.load(
-            module) if isinstance(module, str) else module
+        self._module = _torch.jit.load(module) if isinstance(module, str) else module
         self._module.double()
         self._n_par = _get_number_parameters(self._module)
         self._parameters = list(_get_differentiable_parameters(self._module))
@@ -30,8 +27,9 @@ class Torch(AbstractMachine):
     @property
     def parameters(self):
         return (
-            _torch.cat(tuple(p.view(-1)
-                             for p in _get_differentiable_parameters(self._module)))
+            _torch.cat(
+                tuple(p.view(-1) for p in _get_differentiable_parameters(self._module))
+            )
             .detach()
             .numpy()
             .astype(_np.complex128)
@@ -63,8 +61,10 @@ class Torch(AbstractMachine):
                 )
             )
         i = 0
-        for x in map(lambda x: x.view(-1), _get_differentiable_parameters(self._module)):
-            x.data.copy_(torch_pars[i: i + len(x)].data)
+        for x in map(
+            lambda x: x.view(-1), _get_differentiable_parameters(self._module)
+        ):
+            x.data.copy_(torch_pars[i : i + len(x)].data)
             i += len(x)
 
     @property
@@ -80,8 +80,7 @@ class Torch(AbstractMachine):
         batch_shape = x.shape[:-1]
 
         with _torch.no_grad():
-            t_out = self._module(_torch.from_numpy(
-                x)).numpy().view(_np.complex128)
+            t_out = self._module(_torch.from_numpy(x)).numpy().view(_np.complex128)
 
         if out is None:
             return t_out.reshape(batch_shape)
@@ -97,23 +96,25 @@ class Torch(AbstractMachine):
         x = x.reshape(-1, x.shape[-1])
 
         x = _torch.tensor(x, dtype=_torch.float64)
-        out = x.new_empty([x.size(0), 2, self._n_par],
-                          dtype=self._parameters[0].dtype)
+        out = x.new_empty([x.size(0), 2, self._n_par], dtype=self._parameters[0].dtype)
         m = self._module(x)
 
         for i in range(x.size(0)):
             dws_real = _torch.autograd.grad(
-                m[i, 0], self._parameters, retain_graph=True)
+                m[i, 0], self._parameters, retain_graph=True
+            )
             dws_imag = _torch.autograd.grad(
-                m[i, 1], self._parameters, retain_graph=True)
+                m[i, 1], self._parameters, retain_graph=True
+            )
             _torch.cat([dw.flatten() for dw in dws_real], out=out[i, 0, ...])
             _torch.cat([dw.flatten() for dw in dws_imag], out=out[i, 1, ...])
 
-        out_complex = _np.zeros(
-            (out.size(0), out.size(2)), dtype=_np.complex128)
+        out_complex = _np.zeros((out.size(0), out.size(2)), dtype=_np.complex128)
         out_complex = out[:, 0, :].numpy() + 1.0j * out[:, 1, :].numpy()
 
-        return out_complex.reshape(tuple(list(batch_shape) + list(out_complex.shape[-1:])))
+        return out_complex.reshape(
+            tuple(list(batch_shape) + list(out_complex.shape[-1:]))
+        )
 
     def vector_jacobian_prod(self, x, vec, out=None):
 
@@ -126,7 +127,7 @@ class Torch(AbstractMachine):
             for g in (
                 p.grad.flatten() for p in self._module.parameters() if p.requires_grad
             ):
-                dst[i: i + g.numel()].copy_(g)
+                dst[i : i + g.numel()].copy_(g)
                 i += g.numel()
 
         def zero_grad():
@@ -166,8 +167,7 @@ class Torch(AbstractMachine):
         from collections import OrderedDict
 
         return OrderedDict(
-            [(k, v.detach().numpy())
-             for k, v in self._module.state_dict().items()]
+            [(k, v.detach().numpy()) for k, v in self._module.state_dict().items()]
         )
 
 
