a6e5f92a3b29a56acc4b83c1b1454b808fffce97
chrisrothUT <christopher_roth@utexas.edu>
2020-05-11 15:42:48 -0400

    Update _vmc.py

--- netket/_vmc.py
+++ netket/_vmc.py
@@ -146,7 +146,7 @@ class Vmc(AbstractVariationalDriver):
             self._grads, self._jac = self._machine.vector_jacobian_prod(
                 samples_r, eloc_r / self._n_samples, self._grads, return_jacobian=True
             )
-            
+
             self._grads = tree_map(_sum_inplace, self._grads)
 
             self._dp = self._sr.compute_update(self._jac, self._grads, self._dp)
@@ -156,7 +156,7 @@ class Vmc(AbstractVariationalDriver):
             self._grads = self._machine.vector_jacobian_prod(
                 samples_r, eloc_r / self._n_samples, self._grads
             )
-            
+
             self._grads = tree_map(_sum_inplace, self._grads)
 
             self._dp = self._grads
