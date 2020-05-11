d4b2f1d2930f7c617286a29fced40db2ec282e52
chrisrothUT <christopher_roth@utexas.edu>
2020-05-11 15:11:51 -0400

    Update vmc_common.py

--- netket/vmc_common.py
+++ netket/vmc_common.py
@@ -20,7 +20,7 @@ def tree_map(fun, tree):
 import numpy as np
 from netket.utils import jax_available
 if jax_available:
-    from jax.tree_util import tree_flatten, tree_unflatten
+    from jax.tree_util import tree_flatten, tree_unflatten, tree_map
     import jax.numpy as jnp
 
 
@@ -33,15 +33,11 @@ def shape_for_sr(grads,jac):
     Returns:
         A 1D array of gradients and a 2D array of the jacobian
     """
-
-    if isinstance(grads,np.ndarray):
-        return grads, jac
-    else:
-        from jax.tree_util import tree_flatten
-        import jax.numpy as jnp
-        grads = jnp.concatenate(tuple(fd.reshape(-1) for fd in tree_flatten(grads)[0]))
-        jac = jnp.concatenate(tuple(fd.reshape(len(fd),-1) for fd in tree_flatten(jac)[0]),-1)
-        return grads, jac
+    
+    grads = jnp.concatenate(tuple(fd.reshape(-1) for fd in tree_flatten(grads)[0]))
+    jac = jnp.concatenate(tuple(fd.reshape(len(fd),-1) for fd in tree_flatten(jac)[0]),-1)
+    
+    return grads, jac 
     
 
 def shape_for_update(update,shape_like):
@@ -56,17 +52,16 @@ def shape_for_update(update,shape_like):
         A possibly non-flat structure of jax arrays containing a copy of data
         compatible with the given shape if jax_available and a copy of update otherwise
     """
-    if isinstance(shape_like, np.ndarray):
-        return update
-    else:
-        shf, tree = tree_flatten(shape_like)
+    
+    
+    shf, tree = tree_flatten(shape_like)
 
-        updatelist = []
-        k = 0
-        for s in shf:
-            size = s.size
-            updatelist.append(jnp.asarray(update[k : k + size]).reshape(s.shape))
-            k += size
+    updatelist = []
+    k = 0
+    for s in shf:
+        size = s.size
+        updatelist.append(jnp.asarray(update[k : k + size]).reshape(s.shape))
+        k += size
 
-        return tree_unflatten(tree, updatelist)
+    return tree_unflatten(tree, updatelist)
     
