8b6ef8c68951af8ad493bd55144d82a5dc819738
Filippo Vicentini <filippovicentini@gmail.com>
2020-05-13 16:05:19 +0200

    Add a pad=False option to get_conn_flattened of liouvillian

--- netket/operator/local_liouvillian.py
+++ netket/operator/local_liouvillian.py
@@ -47,9 +47,9 @@ class LocalLiouvillian(AbstractOperator):
         max_conn_size = 0
         for L in self._jump_ops:
             Hnh += -0.5j * L.conjugate().transpose() * L
-            max_conn_size += L.n_operators * L._max_op_size
+            max_conn_size += (L.n_operators * L._max_op_size) ** 2
 
-        self._max_dissipator_conn_size = max_conn_size ** 2
+        self._max_dissipator_conn_size = max_conn_size
 
         self._Hnh = Hnh
         self._Hnh_dag = Hnh.conjugate().transpose()
@@ -82,7 +82,6 @@ class LocalLiouvillian(AbstractOperator):
 
     def get_conn(self, x):
         n_sites = x.shape[0] // 2
-        batch_size = x.shape[0]
 
         xr, xc = x[0:n_sites], x[n_sites : 2 * n_sites]
         i = 0
@@ -114,7 +113,11 @@ class LocalLiouvillian(AbstractOperator):
 
         return _np.copy(self._xprime[0:i, :]), _np.copy(self._mels[0:i])
 
-    def get_conn_flattened(self, x, sections):
+    # pad option pads all sections to have the same (biggest) size.
+    # to avoid using the biggest possible size, we dynamically check what is
+    # the biggest size for the current size...
+    # TODO: investigate if this is worthless
+    def get_conn_flattened(self, x, sections, pad=False):
         batch_size = x.shape[0]
         N = x.shape[1] // 2
         n_jops = len(self.jump_ops)
@@ -128,6 +131,18 @@ class LocalLiouvillian(AbstractOperator):
         sections_c = _np.empty(batch_size, dtype=_np.int64)
         xr_prime, mels_r = self._Hnh_dag.get_conn_flattened(xr, sections_r)
         xc_prime, mels_c = self._Hnh.get_conn_flattened(xc, sections_c)
+
+        if pad:
+            # if else to accomodate for batches of 1 element, because
+            # sections don't start from 0-index...
+            # TODO: if we ever switch to 0-indexing, remove this.
+            if batch_size > 1:
+                max_conns_r = _np.max(_np.diff(sections_r))
+                max_conns_c = _np.max(_np.diff(sections_c))
+            else:
+                max_conns_r = sections_r[0]
+                max_conns_c = sections_c[0]
+            max_conns_Lrc = 0
 
         L_xrps = List()
         L_xcps = List()
@@ -148,10 +163,25 @@ class LocalLiouvillian(AbstractOperator):
             L_mel_rs.append(L_mel_r)
             L_mel_cs.append(L_mel_c)
 
+            if pad:
+                if batch_size > 1:
+                    max_lr = _np.max(_np.diff(sections_Lr[i * batch_size : (i + 1) * batch_size]))
+                    max_lc = _np.max(_np.diff(sections_Lc[i * batch_size : (i + 1) * batch_size]))
+                else:
+                    max_lr = sections_Lr[i * batch_size]
+                    max_lc = sections_Lc[i * batch_size]
+
+                max_conns_Lrc += max_lr * max_lc 
+                
         # compose everything again
         if self._xprime.shape[0] < self._max_conn_size * batch_size:
             self._xprime_f.resize(self._max_conn_size * batch_size, self.hilbert.size)
             self._mels_f.resize(self._max_conn_size * batch_size)
+
+        if pad:
+            pad = max_conns_Lrc + max_conns_r + max_conns_c
+        else:
+            pad=0
 
         return self._get_conn_flattened_kernel(
             self._xprime_f,
@@ -174,6 +204,7 @@ class LocalLiouvillian(AbstractOperator):
             n_jops,
             batch_size,
             N,
+            pad,
         )
 
     @staticmethod
@@ -199,6 +230,7 @@ class LocalLiouvillian(AbstractOperator):
         n_jops,
         batch_size,
         N,
+        pad,
     ):
         sec = 0
         off = 0
@@ -247,6 +279,9 @@ class LocalLiouvillian(AbstractOperator):
 
                 n_Lr_is[j] = n_Lr_f
                 n_Lc_is[j] = n_Lc_f
+
+            if pad != 0:
+                off = (i+1) * pad
 
             sections[i] = off
 
