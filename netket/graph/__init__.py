ac3f3d235b5edb9dc446edb2338910c0abea6e37
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2020-05-20 09:29:30 -0400

    Revert to old graphs for now

--- netket/graph/__init__.py
+++ netket/graph/__init__.py
@@ -3,4 +3,5 @@ from .abstract_graph import AbstractGraph
 from .graph import *
 from .grid import *
 
-from .._C_netket.graph import Lattice
+from .._C_netket.graph import Hypercube, Lattice
+from .._C_netket.graph import CustomGraph as Graph
