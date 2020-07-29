fa9e0a240f89dd2022de71a4383842bb6627e20f
Filippo Vicentini <filippovicentini@gmail.com>
2020-07-29 16:50:45 +0300

    Remove deprecations from v2.1

--- netket/abstract_variational_driver.py
+++ netket/abstract_variational_driver.py
@@ -1,6 +1,5 @@
 import abc
 
-from netket._core import deprecated, warn_deprecation
 import netket as _nk
 import numpy as _np
 
@@ -35,7 +34,6 @@ class AbstractVariationalDriver(abc.ABC):
     def __init__(self, machine, optimizer, minimized_quantity_name=""):
         self._mynode = _rank
         self._mpi_nodes = _n_nodes
-        self._obs = {}  # to deprecate
         self._loss_stats = None
         self._loss_name = minimized_quantity_name
         self._step_count = 0
@@ -168,10 +166,6 @@ class AbstractVariationalDriver(abc.ABC):
         in the output `logger`. If no logger is specified, creates a json file at `out`,
         overwriting files with the same prefix.
 
-        !! Compatibility v2.1
-            Before v2.1 the order of the first two arguments, `n_iter` and `out` was
-            reversed. The reversed ordering will still be supported until v3.0, but is deprecated.
-
         Args:
             :n_iter: the total number of iterations
             :out: A logger object, or an iterable of loggers, to be used to store simulation log and data.
@@ -185,22 +179,8 @@ class AbstractVariationalDriver(abc.ABC):
             :show_progress: If true displays a progress bar (default=True)
         """
 
-        # TODO Remove this deprecated code in v3.0
-        # manage deprecated where argument names are not specified, and
-        # prefix is passed as the first positional argument and the number
-        # of iterations as a second argument.
-        if type(n_iter) is str and type(out) is int:
-            n_iter, out = out, n_iter
-            warn_deprecation(
-                "The positional syntax run(out, n_iter, **args) is deprecated, use run(n_iter, out, **args) instead."
-            )
-
         if obs is None:
-            # TODO remove the first case after deprecation of self._obs in 3.0
-            if len(self._obs) != 0:
-                obs = self._obs
-            else:
-                obs = {}
+            obs = {}
 
         if out is None:
             out = tuple()
@@ -265,51 +245,3 @@ class AbstractVariationalDriver(abc.ABC):
         """
         self._machine.parameters = self._optimizer.update(dp, self._machine.parameters)
         self._step_count += 1
-
-    @deprecated()
-    def add_observable(self, obs, name):
-        """
-        Add an observables to the set of observables that will be computed by default
-        in get_obervable_stats.
-
-        This function is deprecated in favour of `estimate`.
-
-        Args:
-            obs: the operator encoding the observable
-            name: a string, representing the name of the observable
-        """
-        self._obs[name] = obs
-
-    @deprecated()
-    def get_observable_stats(self, observables=None, include_energy=True):
-        """
-        Return MCMC statistics for the expectation value of observables in the
-        current state of the driver.
-
-        Args:
-            observables: A dictionary of the form {name: observable} or a list
-                of tuples (name, observable) for which statistics should be computed.
-                If observables is None or not passed, results for those observables
-                added to the driver by add_observables are computed.
-            include_energy: Whether to include the energy estimate (which is already
-                computed as part of the VMC step) in the result.
-
-        Returns:
-            A dictionary of the form {name: stats} mapping the observable names in
-            the input to corresponding Stats objects.
-
-            If `include_energy` is true, then the result will further contain the
-            energy statistics with key "Energy".
-        """
-        if not observables:
-            observables = self._obs
-
-        if self._loss_name is None:
-            include_energy = False
-
-        result = self.estimate(observables)
-
-        if include_energy:
-            result[self._loss_name] = self._loss_stats
-
-        return result
--- netket/variational.py
+++ netket/variational.py
@@ -4,65 +4,6 @@ import warnings
 from netket.vmc_common import tree_map
 
 
-def Vmc(
-    hamiltonian,
-    sampler,
-    optimizer,
-    n_samples,
-    discarded_samples=None,
-    discarded_samples_on_init=0,
-    target="energy",
-    method="Sr",
-    diag_shift=0.01,
-    use_iterative=False,
-    use_cholesky=None,
-    sr_lsq_solver=None,
-):
-
-    if use_cholesky and sr_lsq_solver != "LLT":
-        raise ValueError(
-            "Inconsistent options specified: `use_cholesky && sr_lsq_solver != 'LLT'`."
-        )
-
-    if discarded_samples_on_init != 0:
-        warnings.warn(
-            "discarded_samples_on_init does not have any effect and should not be used",
-            DeprecationWarning,
-        )
-
-    warnings.warn(
-        "netket.variational.Vmc will be deprecated in version 3, use netket.Vmc instead",
-        PendingDeprecationWarning,
-    )
-
-    if method == "Gd":
-        return _Vmc(
-            hamiltonian=hamiltonian,
-            sampler=sampler,
-            optimizer=optimizer,
-            n_samples=n_samples,
-            n_discard=discarded_samples,
-            sr=None,
-        )
-    elif method == "Sr":
-        sr = _SR(
-            sampler.machine,
-            lsq_solver=sr_lsq_solver,
-            diag_shift=diag_shift,
-            use_iterative=use_iterative,
-        )
-        return _Vmc(
-            hamiltonian=hamiltonian,
-            sampler=sampler,
-            optimizer=optimizer,
-            n_samples=n_samples,
-            n_discard=discarded_samples,
-            sr=sr,
-        )
-    else:
-        raise ValueError("Allowed method options are Gd and Sr")
-
-
 # Higher-level VMC functions:
 
 
