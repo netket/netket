c511cace47a43887d5f0589df37aad7da7725486
Filippo Vicentini <filippovicentini@gmail.com>
2020-10-11 15:40:10 +0200

    attempt to use coverage and github actions Fix CI

--- Test/MPI/test_mpi_setup.py
+++ Test/MPI/test_mpi_setup.py
@@ -6,11 +6,13 @@ size = comm.Get_size()
 rank = comm.Get_rank()
 
 
+@pytest.mark.skipif(size < 2, reason="need at least 2 processes to test MPI")
 def test_is_running_with_multiple_procs():
     msg = "The Test_MPI tests should be run with more than one MPI process"
     assert size > 1, msg
 
 
+@pytest.mark.skipif(size < 2, reason="need at least 2 processes to test MPI")
 def test_mpi_setup():
     recv = comm.bcast(rank)
     assert recv == 0, "rank={}".format(rank)
