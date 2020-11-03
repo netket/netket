9f14635ef7e444b16231857193b3e9e9e8a5bd12
Filippo Vicentini <filippovicentini@gmail.com>
2020-11-03 14:44:22 +0100

    Add extras requires

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name="netket",
@@ -7,29 +8,13 @@ setup(
     url="http://github.com/netket/netket",
     author_email="netket@netket.org",
     license="Apache 2.0",
-    packages=[
-        "netket",
-        "netket.graph",
-        "netket.hilbert",
-        "netket.logging",
-        "netket.callbacks",
-        "netket.machine",
-        "netket.machine.density_matrix",
-        "netket.sampler",
-        "netket.sampler.numpy",
-        "netket.sampler.jax",
-        "netket.stats",
-        "netket.operator",
-        "netket.optimizer",
-        "netket.optimizer.numpy",
-        "netket.optimizer.jax",
-    ],
+    packages=find_packages(),
     long_description="""NetKet is an open - source project delivering cutting - edge
          methods for the study of many - body quantum systems with artificial
          neural networks and machine learning techniques.""",
     install_requires=[
         "numpy>=1.16",
-        "scipy>=1.2.1",
+        "scipy>=1.5.2",
         "tqdm>=4.42.1",
         "numba>=0.49.0",
         "networkx>=2.4",
@@ -37,6 +22,7 @@ setup(
     python_requires=">=3.6",
     extras_require={
         "dev": ["pytest", "python-igraph", "pre-commit", "black==20.8b1"],
-        "jax": ["jax", "mpi4jax>=0.2.6"],
+        "jax": ["jax"],
+        "mpi": ["mpi4py>=3.0.1"],
     },
 )
