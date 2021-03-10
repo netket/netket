1f151ee7d19d9f1b8ceb26a3941257920613ae29
Filippo Vicentini <filippovicentini@gmail.com>
2021-03-10 21:29:28 +0100

    re-export all initialisers from flax

--- netket/nn/initializers.py
+++ netket/nn/initializers.py
@@ -3,8 +3,6 @@ from jax import numpy as jnp
 
 from functools import partial
 
-from jax.nn.initializers import zeros, ones, variance_scaling
-
-lecun_normal = partial(variance_scaling, 1.0, "fan_in", "normal")
+from flax.linen.initializers import *
 
 lecun_complex = partial(variance_scaling, 1.0, "fan_in", "normal", dtype=jnp.complex64)
