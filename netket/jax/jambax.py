cc3d242033b43184417f5f7ff11d5fa007b4d5d9
Filippo Vicentini <filippovicentini@gmail.com>
2021-03-15 10:45:54 +0100

    attempt to support gpu

--- netket/jax/jambax.py
+++ netket/jax/jambax.py
@@ -87,7 +87,14 @@ from jaxlib import xla_extension
 import numba
 from numba import types as nb_types
 import numba.typed as nb_typed
-import numpy as onp
+import numpy as np
+
+
+def _shape_size(shape):
+    sz = 1
+    for dim in shape:
+        sz *= dim
+    return sz
 
 
 def _xla_shape_to_abstract(xla_shape):
@@ -109,10 +116,8 @@ def _create_xla_target_capsule(ptr):
 
 def _np_evaluation_rule(call_fn, abstract_eval_fn, *args, **kwargs):
     output_shapes = abstract_eval_fn(*args)
-    outputs = tuple(
-        onp.empty(shape.shape, dtype=shape.dtype) for shape in output_shapes
-    )
-    inputs = tuple(onp.asarray(arg) for arg in args)
+    outputs = tuple(np.empty(shape.shape, dtype=shape.dtype) for shape in output_shapes)
+    inputs = tuple(np.asarray(arg) for arg in args)
     call_fn(outputs + inputs, **kwargs)
     return tuple(outputs)
 
@@ -124,7 +129,7 @@ def _naive_batching(call_fn, args, batch_axes):
     return tuple(jax.lax.map(lambda x: call_fn(*x), args)), batch_axes
 
 
-def _xla_translation(numba_fn, abstract_eval_fn, xla_builder, *args):
+def _xla_translation_cpu(numba_fn, abstract_eval_fn, xla_builder, *args):
     """Returns the XLA CustomCall for the given numba function.
 
     Args:
@@ -135,6 +140,7 @@ def _xla_translation(numba_fn, abstract_eval_fn, xla_builder, *args):
     Returns:
       The XLA CustomCall operation calling into the numba function.
     """
+    print("encoding jamax cpu")
     input_shapes = [xla_builder.get_shape(arg) for arg in args]
     # TODO(josipd): Check that the input layout is the numpy default.
     output_abstract_arrays = abstract_eval_fn(
@@ -145,7 +151,7 @@ def _xla_translation(numba_fn, abstract_eval_fn, xla_builder, *args):
         dim for array in output_abstract_arrays for dim in array.shape
     )
     output_ndims = tuple(array.ndim for array in output_abstract_arrays)
-    output_ndims_offsets = tuple(onp.cumsum(onp.concatenate([[0], output_ndims])))
+    output_ndims_offsets = tuple(np.cumsum(np.concatenate([[0], output_ndims])))
     output_dtypes = tuple(array.dtype for array in output_abstract_arrays)
     layout_for_shape = lambda shape: range(len(shape) - 1, -1, -1)
     output_layouts = map(layout_for_shape, output_shapes)
@@ -165,7 +171,8 @@ def _xla_translation(numba_fn, abstract_eval_fn, xla_builder, *args):
     n_in = len(input_dimensions)
 
     xla_call_sig = nb_types.void(
-        nb_types.CPointer(nb_types.voidptr), nb_types.CPointer(nb_types.voidptr)
+        nb_types.CPointer(nb_types.voidptr),  # output_ptrs
+        nb_types.CPointer(nb_types.voidptr),  # input_ptrs
     )
 
     @numba.cfunc(xla_call_sig)
@@ -222,7 +229,8 @@ def _xla_translation(numba_fn, abstract_eval_fn, xla_builder, *args):
 
     target_name = xla_custom_call_target.native_name.encode("ascii")
     capsule = _create_xla_target_capsule(xla_custom_call_target.address)
-    xla_extension.register_custom_call_target(target_name, capsule, "Host")
+    xla_client.register_custom_call_target(target_name, capsule, "cpu")
+    # xla_extension.register_custom_call_target(target_name, capsule, "Host")
     return xla_client.ops.CustomCallWithLayout(
         xla_builder,
         target_name,
@@ -230,6 +238,197 @@ def _xla_translation(numba_fn, abstract_eval_fn, xla_builder, *args):
         shape_with_layout=xla_output_shape,
         operand_shapes_with_layout=input_shapes,
     )
+
+
+##
+
+try:
+    import ctypes
+    from numba.extending import get_cython_function_address
+    import cupy_backends
+
+    addr = get_cython_function_address("cupy_backends.cuda.api.runtime", "memcpy")
+    functype = ctypes.CFUNCTYPE(
+        ctypes.c_int, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_size_t, ctypes.c_int
+    )
+    cudaMemcpy = functype(addr)
+
+    #
+    addr = get_cython_function_address("cupy_backends.cuda.api.runtime", "memcpyAsync")
+    functype = ctypes.CFUNCTYPE(
+        ctypes.c_int,
+        ctypes.c_void_p,
+        ctypes.c_void_p,
+        ctypes.c_size_t,
+        ctypes.c_int,
+        ctypes.c_void_p,
+    )
+    cudaMemcpyAsync = functype(addr)
+
+    #
+    addr = get_cython_function_address(
+        "cupy_backends.cuda.api.runtime", "streamSynchronize"
+    )
+    functype = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_void_p)
+    cudaStreamSynchronize = functype(addr)
+
+    has_cupy = True
+
+    def _xla_translation_gpu(numba_fn, abstract_eval_fn, xla_builder, *args):
+        """Returns the XLA CustomCall for the given numba function.
+
+        Args:
+          numba_fn: A numba function. For its signature, see the module docstring.
+          abstract_eval_fn: The abstract shape evaluation function.
+          xla_builder: The XlaBuilder instance.
+          *args: The positional arguments to be passed to `numba_fn`.
+        Returns:
+          The XLA CustomCall operation calling into the numba function.
+        """
+        print("encoding jambax gpu")
+        input_shapes = [xla_builder.get_shape(arg) for arg in args]
+        input_dtypes = tuple(shape.element_type() for shape in input_shapes)
+        input_dimensions = tuple(shape.dimensions() for shape in input_shapes)
+        input_byte_size = tuple(
+            np.prod(shape) * dtype.itemsize
+            for (shape, dtype) in zip(input_dimensions, input_dtypes)
+        )
+
+        input_i = tuple(i for i in range(len(input_dimensions)))
+        n_in = len(input_dimensions)
+
+        # TODO(josipd): Check that the input layout is the numpy default.
+        output_abstract_arrays = abstract_eval_fn(
+            *[_xla_shape_to_abstract(shape) for shape in input_shapes]
+        )
+        output_shapes = tuple(array.shape for array in output_abstract_arrays)
+
+        output_ndims = tuple(array.ndim for array in output_abstract_arrays)
+        output_ndims_offsets = tuple(np.cumsum(np.concatenate([[0], output_ndims])))
+        output_dtypes = tuple(array.dtype for array in output_abstract_arrays)
+        output_byte_size = tuple(
+            np.prod(shape) * dtype.itemsize
+            for (shape, dtype) in zip(output_shapes, output_dtypes)
+        )
+
+        layout_for_shape = lambda shape: range(len(shape) - 1, -1, -1)
+        output_layouts = map(layout_for_shape, output_shapes)
+        xla_output_shapes = [
+            xla_client.Shape.array_shape(*arg)
+            for arg in zip(output_dtypes, output_shapes, output_layouts)
+        ]
+        xla_output_shape = xla_client.Shape.tuple_shape(xla_output_shapes)
+
+        output_i = tuple(i for i in range(len(output_shapes)))
+
+        n_out = len(output_shapes)
+
+        xla_call_sig = nb_types.void(
+            nb_types.voidptr,  # cudaStream_t* stream
+            nb_types.CPointer(nb_types.voidptr),  # void** buffers
+            nb_types.voidptr,  # const char* opaque
+            nb_types.uint64,  # size_t opaque_len
+        )
+
+        print(f"With N_in={n_in} and n_out={n_out}")
+
+        @numba.cfunc(xla_call_sig)
+        def xla_custom_call_target(stream, inout_gpu_ptrs, opaque, opaque_len):
+            # manually unroll input and output args because numba is
+            # relatively dummb and cannot always infer getitem on inhomogeneous tuples
+            if n_out == 1:
+                args_out = (np.empty(output_shapes[0], dtype=output_dtypes[0]),)
+            elif n_out == 2:
+                args_out = (
+                    np.empty(output_shapes[0], dtype=output_dtypes[0]),
+                    np.empty(output_shapes[1], dtype=output_dtypes[1]),
+                )
+            elif n_out == 3:
+                args_out = (
+                    np.empty(output_shapes[0], dtype=output_dtypes[0]),
+                    np.empty(output_shapes[1], dtype=output_dtypes[1]),
+                    np.empty(output_shapes[2], dtype=output_dtypes[2]),
+                )
+            elif n_out == 4:
+                args_out = (
+                    np.empty(output_shapes[0], dtype=output_dtypes[0]),
+                    np.empty(output_shapes[1], dtype=output_dtypes[1]),
+                    np.empty(output_shapes[2], dtype=output_dtypes[2]),
+                    np.empty(output_shapes[3], dtype=output_dtypes[3]),
+                )
+
+            if n_in == 1:
+                args_in = (np.empty(input_dimensions[0], dtype=input_dtypes[0]),)
+            #    cudaMemcpy(args_in[0].ctypes.data, inout_gpu_ptrs[0], input_byte_size[0], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost))
+            elif n_in == 2:
+                args_in = (
+                    np.empty(input_dimensions[0], dtype=input_dtypes[0]),
+                    np.empty(input_dimensions[1], dtype=input_dtypes[1]),
+                )
+                cudaMemcpy(
+                    args_in[0].ctypes.data,
+                    inout_gpu_ptrs[0],
+                    1,
+                    nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost),
+                )
+                cudaMemcpy(
+                    args_in[1].ctypes.data,
+                    inout_gpu_ptrs[1],
+                    1,
+                    nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost),
+                )
+            elif n_in == 3:
+                args_in = (
+                    np.empty(input_dimensions[0], dtype=input_dtypes[0]),
+                    np.empty(input_dimensions[1], dtype=input_dtypes[1]),
+                    np.empty(input_dimensions[2], dtype=input_dtypes[2]),
+                )
+            #    cudaMemcpy(args_in[0].ctypes.data, inout_gpu_ptrs[0], input_byte_size[0], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost))
+            #    cudaMemcpy(args_in[1].ctypes.data, inout_gpu_ptrs[1], input_byte_size[1], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost))
+            #    cudaMemcpy(args_in[2].ctypes.data, inout_gpu_ptrs[2], input_byte_size[2], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost))
+            elif n_in == 4:
+                args_in = (
+                    np.empty(input_dimensions[0], dtype=input_dtypes[0]),
+                    np.empty(input_dimensions[1], dtype=input_dtypes[1]),
+                    np.empty(input_dimensions[2], dtype=input_dtypes[2]),
+                    np.empty(input_dimensions[3], dtype=input_dtypes[3]),
+                )
+            #    cudaMemcpy(args_in[0].ctypes.data, inout_gpu_ptrs[0], input_byte_size[0], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost))
+            #    cudaMemcpy(args_in[1].ctypes.data, inout_gpu_ptrs[1], input_byte_size[1], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost))
+            #    cudaMemcpy(args_in[2].ctypes.data, inout_gpu_ptrs[2], input_byte_size[2], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost))
+            #    cudaMemcpy(args_in[3].ctypes.data, inout_gpu_ptrs[3], input_byte_size[3], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyDeviceToHost))
+
+            # numba_fn(args_out + args_in)
+
+            # if n_out == 1:
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+0], args_out[0].ctypes.data, output_byte_size[0], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+            # elif n_out == 2:
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+0], args_out[0].ctypes.data, output_byte_size[0], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+1], args_out[1].ctypes.data, output_byte_size[1], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+            # elif n_out == 3:
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+0], args_out[0].ctypes.data, output_byte_size[0], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+1], args_out[1].ctypes.data, output_byte_size[1], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+2], args_out[2].ctypes.data, output_byte_size[2], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+            # elif n_out == 4:
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+0], args_out[0].ctypes.data, output_byte_size[0], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+1], args_out[1].ctypes.data, output_byte_size[1], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+2], args_out[2].ctypes.data, output_byte_size[2], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+            #    cudaMemcpy(inout_gpu_ptrs[n_in+3], args_out[3].ctypes.data, output_byte_size[3], nb_types.int32(cupy_backends.cuda.api.runtime.memcpyHostToDevice))
+
+        target_name = xla_custom_call_target.native_name.encode("ascii")
+        capsule = _create_xla_target_capsule(xla_custom_call_target.address)
+        xla_client.register_custom_call_target(target_name, capsule, "gpu")
+        return xla_client.ops.CustomCallWithLayout(
+            xla_builder,
+            target_name,
+            operands=args,
+            shape_with_layout=xla_output_shape,
+            operand_shapes_with_layout=input_shapes,
+        )
+
+
+except:
+    has_cupy = False
 
 
 def numba_to_jax(name: str, numba_fn, abstract_eval_fn, batching_fn=None):
@@ -275,8 +474,14 @@ def numba_to_jax(name: str, numba_fn, abstract_eval_fn, batching_fn=None):
             _naive_batching, _primitive_bind
         )
     xla.backend_specific_translations["cpu"][primitive] = partial(
-        _xla_translation, numba_fn, abstract_eval_fn_always
+        _xla_translation_cpu, numba_fn, abstract_eval_fn_always
     )
+
+    if has_cupy:
+        xla.backend_specific_translations["gpu"][primitive] = partial(
+            _xla_translation_gpu, numba_fn, abstract_eval_fn_always
+        )
+
     return _primitive_bind
 
 
