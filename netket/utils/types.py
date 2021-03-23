88389a3975eebd3441e7c636946a83c48a93d5de
Filippo Vicentini <filippovicentini@gmail.com>
2021-03-24 00:34:36 +0100

    Update netket/utils/types.py
    Co-authored-by: Damian Hofmann <femtobit@users.noreply.github.com>

--- netket/utils/types.py
+++ netket/utils/types.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Tuple, Callable
+from typing import Any, Sequence, Callable
 
 PRNGKey = Any
-Shape = Tuple[int]
+Shape = Sequence[int]
 Dtype = Any  # this could be a real type?
 Array = Any
 
