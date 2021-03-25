cec592b665f204c2be9614f55281ead1d0817654
R Bharathkumar <r.bharathkumar@outlook.com>
2021-03-25 18:07:30 +0530

    Cleanup using black

--- Benchmarks/SR_solving.py
+++ Benchmarks/SR_solving.py
@@ -11,21 +11,29 @@ class Example:
         # self.operator = nk.operator.Ising(hilbert=self.hilbertSpace, graph=self.lattice, h=1.0)  # Is this even needed?
         self.rbmachine = nk.models.RBM(alpha=alpha, dtype=complex)
         self.sampler = nk.sampler.MetropolisLocal(self.hilbertSpace, n_chains=32)
-        self.state = nk.variational.MCState(self.sampler, self.rbmachine, n_samples=n_samp, seed=123)
+        self.state = nk.variational.MCState(
+            self.sampler, self.rbmachine, n_samples=n_samp, seed=123
+        )
 
 
-@pytest.fixture(scope='module', params=[[64, 1024, 1, 1]])
+@pytest.fixture(scope="module", params=[[64, 1024, 1, 1]])
 def example(request):
-    return Example(L=request.param[0], n_samp=request.param[1], alpha=request.param[2], dimensions=request.param[3])
+    return Example(
+        L=request.param[0],
+        n_samp=request.param[1],
+        alpha=request.param[2],
+        dimensions=request.param[3],
+    )
 
 
-@pytest.mark.parametrize('centered', [True, False])
-@pytest.mark.benchmark(
-    min_rounds=10
-)
+@pytest.mark.parametrize("centered", [True, False])
+@pytest.mark.benchmark(min_rounds=10)
 def test_sr_solver(example, benchmark, centered):
     @benchmark
     def sr_solver():
-        qgt = example.state.quantum_geometric_tensor(nk.optimizer.SR(diag_shift=0.01, centered=centered))
-        return (qgt @ example.state.parameters)['Dense']['kernel'].block_until_ready()
+        qgt = example.state.quantum_geometric_tensor(
+            nk.optimizer.SR(diag_shift=0.01, centered=centered)
+        )
+        return (qgt @ example.state.parameters)["Dense"]["kernel"].block_until_ready()
+
     assert True
