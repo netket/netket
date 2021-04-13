44e60708543635453b6a2f76d91ef9e4a5b05b2a
chrisrothUT <christopher_roth@utexas.edu>
2021-04-13 13:24:12 -0500

    Update lattice.py
    add cutoff warning

--- netket/graph/lattice.py
+++ netket/graph/lattice.py
@@ -22,8 +22,12 @@ import warnings
 
 
 def get_edges(atoms_positions, cutoff):
-    tol = 1e-5
-    cutoff = cutoff + tol
+    cutoff_tol = 1e-5
+    """Tolerance for the maximum distance cutoff when computing the sparse distance matrix.
+    This is necessary because of floating-point errors when computing the distance in non-trivial 
+    lattices.
+    """
+    cutoff = cutoff + cutoff_tol
     kdtree = cKDTree(atoms_positions)
     dist_matrix = kdtree.sparse_distance_matrix(kdtree, cutoff)
     id1, id2, values = find(triu(dist_matrix))
