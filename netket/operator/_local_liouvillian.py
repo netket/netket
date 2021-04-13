8feb61544ef090be7ef0a30148fc5ddad838ba5c
Filippo Vicentini <filippovicentini@gmail.com>
2021-04-13 11:03:58 +0200

    Superop fix (#617)
    * switch from stacked-columns to stacked-rows for super-op representation
    
    * fix superop maxconn
    
    * fix test for new format
    
    * harden test
    
    * fix propagation of is_hermitian in mcmixed
    
    * add doc page

--- netket/operator/_local_liouvillian.py
+++ netket/operator/_local_liouvillian.py
@@ -58,7 +58,7 @@ class LocalLiouvillian(AbstractSuperOperator):
 
     .. math ::
 
-        \\mathcal{L} = -i \\hat{H}\\hat{\\rho} +i\\hat{\\rho}\\hat{H}^\\dagger + \\sum_i \\hat{L}_i\\hat{\\rho}\\hat{L}_i^\\dagger
+        \\mathcal{L} = -i \\hat{H}_{nh}\\hat{\\rho} +i\\hat{\\rho}\\hat{H}_{nh}^\\dagger + \\sum_i \\hat{L}_i\\hat{\\rho}\\hat{L}_i^\\dagger
 
     """
 
@@ -73,7 +73,6 @@ class LocalLiouvillian(AbstractSuperOperator):
         self._H = ham
         self._jump_ops = [op.copy(dtype=dtype) for op in jump_ops]  # to accept dicts
         self._Hnh = ham
-        self._Hnh_dag = ham.H
         self._max_dissipator_conn_size = 0
         self._max_conn_size = 0
 
@@ -110,12 +109,12 @@ class LocalLiouvillian(AbstractSuperOperator):
         max_conn_size = 0
         for L in self._jump_ops:
             Hnh = Hnh - 0.5j * L.conjugate().transpose() @ L
-            max_conn_size += (L.n_operators * L._max_op_size) ** 2
+            dim = L.n_operators * L._max_op_size
+            max_conn_size += dim ** 2 + 2 * dim
 
         self._max_dissipator_conn_size = max_conn_size
 
         self._Hnh = Hnh.collect()
-        self._Hnh_dag = Hnh.H.collect()
 
         max_conn_size = (
             self._max_dissipator_conn_size + Hnh.n_operators * Hnh._max_op_size
@@ -149,16 +148,16 @@ class LocalLiouvillian(AbstractSuperOperator):
         xr, xc = x[0:n_sites], x[n_sites : 2 * n_sites]
         i = 0
 
-        xrp, mel_r = self._Hnh_dag.get_conn(xr)
+        xrp, mel_r = self._Hnh.get_conn(xr)
         self._xrv[i : i + len(mel_r), :] = xrp
         self._xcv[i : i + len(mel_r), :] = xc
-        self._mels[i : i + len(mel_r)] = mel_r * 1j
+        self._mels[i : i + len(mel_r)] = -1j * mel_r
         i = i + len(mel_r)
 
         xcp, mel_c = self._Hnh.get_conn(xc)
         self._xrv[i : i + len(mel_c), :] = xr
         self._xcv[i : i + len(mel_c), :] = xcp
-        self._mels[i : i + len(mel_r)] = mel_c * (-1j)
+        self._mels[i : i + len(mel_r)] = 1j * np.conj(mel_c)
         i = i + len(mel_c)
 
         for L in self._jump_ops:
@@ -171,7 +170,7 @@ class LocalLiouvillian(AbstractSuperOperator):
             for r in range(nr):
                 self._xrv[i : i + nc, :] = L_xrp[r, :]
                 self._xcv[i : i + nc, :] = L_xcp
-                self._mels[i : i + nc] = np.conj(L_mel_r[r]) * L_mel_c
+                self._mels[i : i + nc] = L_mel_r[r] * np.conj(L_mel_c)
                 i = i + nc
 
         return np.copy(self._xprime[0:i, :]), np.copy(self._mels[0:i])
@@ -192,7 +191,7 @@ class LocalLiouvillian(AbstractSuperOperator):
         # Compute all flattened connections of each term
         sections_r = np.empty(batch_size, dtype=np.int64)
         sections_c = np.empty(batch_size, dtype=np.int64)
-        xr_prime, mels_r = self._Hnh_dag.get_conn_flattened(xr, sections_r)
+        xr_prime, mels_r = self._Hnh.get_conn_flattened(xr, sections_r)
         xc_prime, mels_c = self._Hnh.get_conn_flattened(xc, sections_c)
 
         if pad:
@@ -323,7 +322,7 @@ class LocalLiouvillian(AbstractSuperOperator):
             n_hr = n_hr_f - n_hr_i
             xs[off : off + n_hr, 0:N] = xr_prime[n_hr_i:n_hr_f, :]
             xs[off : off + n_hr, N : 2 * N] = xc[i, :]
-            mels[off : off + n_hr] = 1j * mels_r[n_hr_i:n_hr_f]
+            mels[off : off + n_hr] = -1j * mels_r[n_hr_i:n_hr_f]
             off += n_hr
             n_hr_i = n_hr_f
 
@@ -331,7 +330,7 @@ class LocalLiouvillian(AbstractSuperOperator):
             n_hc = n_hc_f - n_hc_i
             xs[off : off + n_hc, N : 2 * N] = xc_prime[n_hc_i:n_hc_f, :]
             xs[off : off + n_hc, 0:N] = xr[i, :]
-            mels[off : off + n_hc] = -1j * mels_c[n_hc_i:n_hc_f]
+            mels[off : off + n_hc] = 1j * np.conj(mels_c[n_hc_i:n_hc_f])
             off += n_hc
             n_hc_i = n_hc_f
 
@@ -349,8 +348,8 @@ class LocalLiouvillian(AbstractSuperOperator):
                 for r in range(n_Lr):
                     xs[off : off + n_Lc, 0:N] = L_xrp[n_Lr_i + r, :]
                     xs[off : off + n_Lc, N : 2 * N] = L_xcp[n_Lc_i:n_Lc_f, :]
-                    mels[off : off + n_Lc] = (
-                        np.conj(L_mel_r[n_Lr_i + r]) * L_mel_c[n_Lc_i:n_Lc_f]
+                    mels[off : off + n_Lc] = L_mel_r[n_Lr_i + r] * np.conj(
+                        L_mel_c[n_Lc_i:n_Lc_f]
                     )
                     off = off + n_Lc
 
@@ -409,7 +408,7 @@ class LocalLiouvillian(AbstractSuperOperator):
 
                 drho = np.zeros((M, M), dtype=rho.dtype)
 
-                drho += rho @ iHnh + iHnh.conj().T @ rho
+                drho += iHnh @ rho + rho @ iHnh.conj().T
                 for J, J_c in zip(J_ops, J_ops_c):
                     drho += (J @ rho) @ J_c
 
@@ -434,7 +433,7 @@ class LocalLiouvillian(AbstractSuperOperator):
                 out = np.zeros((M ** 2 + 1), dtype=rho.dtype)
                 drho = out[:-1].reshape((M, M))
 
-                drho += rho @ iHnh + iHnh.conj().T @ rho
+                drho += iHnh @ rho + rho @ iHnh.conj().T
                 for J, J_c in zip(J_ops, J_ops_c):
                     drho += (J @ rho) @ J_c
 
