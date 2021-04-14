45b15c32491fdbe82b411bdad769b3144f37773d
chrisrothUT <christopher_roth@utexas.edu>
2021-04-14 12:38:32 -0500

    Update __init__.py
    get rid of create method

--- netket/models/__init__.py
+++ netket/models/__init__.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .rbm import RBM, RBMModPhase, RBMMultiVal, create_RBMSymm as RBMSymm
-from .equivariant import create_GCNN as GCNN
+from .rbm import RBM, RBMModPhase, RBMMultiVal, RBMSymm
+from .equivariant import GCNN
 from .jastrow import Jastrow
 from .mps import MPSPeriodic
 
