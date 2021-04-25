7f696b488a2a1b9389bb7925495a1eb3a701c20b
Filippo Vicentini <filippovicentini@gmail.com>
2021-04-25 18:59:26 +0200

    examl

--- Examples/Ising1d/ising1d.py
+++ Examples/Ising1d/ising1d.py
@@ -13,9 +13,10 @@
 # limitations under the License.
 
 import netket as nk
+import numpy as np
 
 # 1D Lattice
-L = 20  # 10
+L = 12  # 10
 
 g = nk.graph.Hypercube(length=L, n_dim=1, pbc=True)
 
@@ -23,7 +24,91 @@ g = nk.graph.Hypercube(length=L, n_dim=1, pbc=True)
 hi = nk.hilbert.Spin(s=1 / 2, N=g.n_nodes)
 
 # Ising spin hamiltonian
-ha = nk.operator.Ising(hilbert=hi, graph=g, h=1.0)
+ha = nk.operator.Ising(hilbert=hi, graph=g, h=1.0).to_local_operator()
+
+
+# the pauli matrices are hard coded for simplicity
+sx = np.array([[0, 1], [1, 0]])
+sz = np.array([[1, 0], [0, -1]])
+sy = np.array([[0, -1j], [1j, 0]])
+zz = np.kron(sz, sz)
+yy = np.kron(sy, sy)
+xx = np.kron(sx, sx)
+jj = np.real(xx + yy + zz)
+
+
+def create_Hamiltonain_operator_list():
+    operators = []
+    sites = []
+
+    global sx, sz, jj
+
+    mu = 0.08333333333333333
+    clatt = {
+        (0.0, 4.0): -1.0,
+        (0.0, 5.0): -1.0,
+        (0.0, 6.0): -1.0,
+        (0.0, 7.0): -1.0,
+        (0.0, 8.0): 0.0,
+        (0.0, 9.0): 0.0,
+        (0.0, 10.0): 0.0,
+        (0.0, 11.0): 0.0,
+        (1.0, 4.0): -1.0,
+        (1.0, 5.0): -1.0,
+        (1.0, 6.0): -1.0,
+        (1.0, 7.0): -1.0,
+        (1.0, 8.0): 0.0,
+        (1.0, 9.0): 0.0,
+        (1.0, 10.0): 0.0,
+        (1.0, 11.0): 0.0,
+        (2.0, 4.0): -1.0,
+        (2.0, 5.0): -1.0,
+        (2.0, 6.0): -1.0,
+        (2.0, 7.0): -1.0,
+        (2.0, 8.0): 0.0,
+        (2.0, 9.0): 0.0,
+        (2.0, 10.0): 0.0,
+        (2.0, 11.0): 0.0,
+        (3.0, 4.0): -1.0,
+        (3.0, 5.0): -1.0,
+        (3.0, 6.0): -1.0,
+        (3.0, 7.0): -1.0,
+        (3.0, 8.0): 0.0,
+        (3.0, 9.0): 0.0,
+        (3.0, 10.0): 0.0,
+        (3.0, 11.0): 0.0,
+        (4.0, 8.0): 0.0,
+        (4.0, 9.0): 0.0,
+        (4.0, 10.0): 0.0,
+        (4.0, 11.0): 0.0,
+        (5.0, 8.0): 0.0,
+        (5.0, 9.0): 0.0,
+        (5.0, 10.0): 0.0,
+        (5.0, 11.0): 0.0,
+        (6.0, 8.0): 0.0,
+        (6.0, 9.0): 0.0,
+        (6.0, 10.0): 0.0,
+        (6.0, 11.0): 0.0,
+        (7.0, 8.0): 0.0,
+        (7.0, 9.0): 0.0,
+        (7.0, 10.0): 0.0,
+        (7.0, 11.0): 0.0,
+    }
+
+    # two-body interaction
+    for key in clatt:
+        operators.append((mu * (1 - clatt[key[0], key[1]]) * jj).tolist())
+        sites.append([key[0], key[1]])
+    return sites, operators
+
+
+def build_Hamiltonian(hilbert):
+    sites, operators = create_Hamiltonain_operator_list()
+    Hamiltonian = nk.operator.LocalOperator(hilbert, operators, sites)
+    return Hamiltonian
+
+
+ha = build_Hamiltonian(hi)
 
 # RBM Spin Machine
 ma = nk.models.RBM(alpha=1, use_visible_bias=True, dtype=float)
@@ -35,7 +120,7 @@ sa = nk.sampler.MetropolisLocal(hi, n_chains=16)
 op = nk.optimizer.Sgd(learning_rate=0.1)
 
 # Variational monte carlo driver
-gs = nk.VMC(ha, op, sa, ma, n_samples=1000)
+gs = nk.VMC(ha, op, sa, ma, n_samples=2, n_discard=0)
 
 # Run the optimization for 300 iterations
 gs.run(n_iter=300, out="test")
