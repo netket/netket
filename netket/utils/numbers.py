e5444b13b151722ed2ed112a4ba417e89a541cd7
Filippo Vicentini <filippovicentini@gmail.com>
2021-04-29 17:39:07 +0200

    support old jaxlib

--- netket/utils/numbers.py
+++ netket/utils/numbers.py
@@ -7,7 +7,17 @@ import numpy as np
 import jax
 import jaxlib
 
-ArrayT = Union[np.ndarray, jaxlib.xla_extension.DeviceArray, jax.core.Tracer]
+# compatibility with jaxlib<=0.1.61
+# we don't really support this old jaxlib, because previous
+# versions had bugs and dont work with mpi4jax, but some people
+# do use that because of old computer without AVX so...
+# eventually delete this.
+try:
+    _DeviceArray = jaxlib.xla_extension.DeviceArray
+except:
+    _DeviceArray = jax.interpreters.xla._DeviceArray
+
+ArrayT = Union[np.ndarray, _DeviceArray, jax.core.Tracer]
 
 
 @dispatch
