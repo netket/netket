8cbd5c2d6ea0e321821dc6881642d1ed4541b9f9
Attila Szabó <attila.szabo@physics.ox.ac.uk>
2021-05-05 22:34:56 +0100

    PyTree-based QGT implementation (#689)
    Logic streamlined and harmonised with JacobianDense, added scale-invariant regularisation
    
    fixes in logic
    
    Implemented JacobianPyTree, fixes in JacobianDense
    
    fixed to_dense, made __matmul__ jittable
    
    made scale-invariant regularisation default in JacobianDense
    
    Logic for splitting parameters in real&imaginary parts
    
    Adapted JacobianPyTree for new mode handling
    
    black

--- netket/optimizer/qgt/__init__.py
+++ netket/optimizer/qgt/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .qgt_jacobian import QGTJacobian
+from .qgt_jacobian_dense import QGTJacobianDense
 from .qgt_jacobian_pytree import QGTJacobianPyTree
 from .qgt_onthefly import QGTOnTheFly
 
--- netket/optimizer/qgt/qgt_jacobian_pytree.py
+++ netket/optimizer/qgt/qgt_jacobian_pytree.py
@@ -19,24 +19,45 @@ import jax
 from jax import numpy as jnp
 from flax import struct
 
-from netket.utils.types import PyTree
-from netket.utils import n_nodes
+from netket.utils.types import PyTree, Array
+from netket.utils.mpi import n_nodes
 from netket.stats import sum_inplace
 import netket.jax as nkjax
 
 from ..linear_operator import LinearOperator, Uninitialized
 
-from .treemv_logic import mat_vec
+from .qgt_jacobian_pytree_logic import mat_vec, prepare_doks
 
 
-def QGTJacobianPyTree(vstate, *, mode, rescale_shift=False) -> "QGTJacobianT":
-    O, scale = 0, 0  # compute gradient
+def QGTJacobianPyTree(
+    vstate, *, mode="auto", rescale_shift=True, **kwargs
+) -> "QGTJacobianPyTreeT":
+    # Choose sensible default mode
+    if mode == "auto":
+        complex_output = nkjax.is_complex(
+            jax.eval_shape(vstate._apply_fun, vstate.parameters, vstate.samples)
+        )
+        if complex_output:
+            mode = "complex"
+        else:
+            mode = "real"
 
-    return QGTJacobianT(O=O, scale=rescale_shift)
+    O, scale = prepare_doks(
+        vstate._apply_fun,
+        vstate.parameters,
+        vstate.samples,
+        vstate.model_state,
+        mode,
+        rescale_shift,
+    )
+
+    return QGTJacobianPyTreeT(
+        O=O, scale=scale, params=vstate.parameters, mode=mode, **kwargs
+    )
 
 
 @struct.dataclass
-class QGTJacobianPyTree(LinearOperator):
+class QGTJacobianPyTreeT(LinearOperator):
     """
     Semi-lazy representation of an S Matrix behaving like a linear operator.
 
@@ -46,31 +67,82 @@ class QGTJacobianPyTree(LinearOperator):
     the field `sr`.
     """
 
-    O: jnp.ndarray = Uninitialized
-    """Gradients O_ij = ∂log ψ(σ_i)/∂p_j of the neural network 
-    for all samples σ_i at given values of the parameters p_j
-    Average <O_j> subtracted for each parameter
+    O: PyTree = Uninitialized
+    """Centred gradients ΔO_ij = O_ij - <O_j> of the neural network, where
+    O_ij = ∂log ψ(σ_i)/∂p_j, for all samples σ_i at given values of the parameters p_j
     Divided through with sqrt(#samples) to normalise S matrix
-    If scale is not None, columns normalised to unit norm
+    If scale is not None, O_ij for is normalised to unit norm for each parameter j
     """
 
-    scale: Optional[jnp.ndarray] = None
+    scale: Optional[PyTree] = None
     """If not None, contains 2-norm of each column of the gradient matrix,
     i.e., the sqrt of the diagonal elements of the S matrix
     """
 
-    @jax.jit
-    def __matmul__(self, vec: Union[PyTree, jnp.ndarray]) -> Union[PyTree, jnp.ndarray]:
-        mat_vec(vec, self.O, self.scale)
+    params: PyTree = Uninitialized
+    """Parameters of the network. Its only purpose is to represent its own shape when scale is None"""
+
+    mode: str = struct.field(pytree_node=False, default=Uninitialized)
+    """Differentiation mode, "auto" is resolved into "real" or "complex" """
 
     @jax.jit
-    def _unscaled_matmul(self, vec: jnp.ndarray) -> jnp.ndarray:
-        return (
-            sum_inplace(((self.O @ vec).T.conj() @ self.O).T.conj())
-            + self.diag_shift * vec
-        )
+    def __matmul__(self, vec: Union[PyTree, Array]) -> Union[PyTree, Array]:
+        # Turn vector RHS into PyTree
+        if hasattr(vec, "ndim"):
+            _, unravel = nkjax.tree_ravel(self.params)
+            vec = unravel(vec)
+            ravel = True
+        else:
+            ravel = False
+
+        # Real-imaginary split RHS in R→R and R→C modes
+        if self.mode != "holomorphic":
+            vec, reassemble = nkjax.tree_to_real(vec)
+
+        if self.scale is not None:
+            vec = jax.tree_multimap(jnp.multiply, vec, self.scale)
+
+        result = mat_vec(vec, self.O, self.diag_shift)
+
+        if self.scale is not None:
+            result = jax.tree_multimap(jnp.multiply, result, self.scale)
+
+        # Reassemble real-imaginary split as needed
+        if self.mode != "holomorphic":
+            result = reassemble(result)
+
+        # Ravel PyTree back into vector as needed
+        if ravel:
+            result, _ = nkjax.tree_ravel(result)
+
+        return result
 
     @jax.jit
+    def _unscaled_matmul(self, vec: PyTree) -> PyTree:
+        return mat_vec(vec, self.O, self.diag_shift)
+
+    @jax.jit
+    def _split_matmul(self, vec: Array) -> Array:
+        if self.mode == "holomorphic":
+            pars = self.params
+        else:
+            pars = nkjax.tree_to_real(self.params)
+
+        _, unravel = nkjax.tree_ravel(pars)
+        vec = unravel(vec)
+
+        if self.scale is not None:
+            vec = jax.tree_multimap(jnp.multiply, vec, self.scale)
+
+        result = mat_vec(vec, self.O, self.diag_shift)
+
+        if self.scale is not None:
+            result = jax.tree_multimap(jnp.multiply, result, self.scale)
+
+        result, _ = nkjax.tree_ravel(result)
+        return result
+
+    @partial(jax.jit, static_argnums=1)
     def _solve(self, solve_fun, y: PyTree, *, x0: Optional[PyTree] = None) -> PyTree:
         """
         Solve the linear system x=⟨S⟩⁻¹⟨y⟩ with the chosen iterataive solver.
@@ -84,25 +156,25 @@ class QGTJacobianPyTree(LinearOperator):
             info: optional additional informations provided by the solver. Might be
                 None if there are no additional informations provided.
         """
-
-        # Ravel input PyTrees, record unravelling function too
-        grad, unravel = nkjax.tree_ravel(y)
-
-        if x0 is not None:
-            x0, _ = nkjax.tree_ravel(x0)
-            if self.scale is not None:
-                x0 = x0 * self.scale
+        # Real-imaginary split RHS in R→R and R→C modes
+        if self.mode != "holomorphic":
+            y, reassemble = nkjax.tree_to_real(y)
 
         if self.scale is not None:
-            grad = grad / self.scale
+            y = jax.tree_multimap(jnp.divide, y, self.scale)
+            if x0 is not None:
+                x0 = jax.tree_multimap(jnp.multiply, x0, self.scale)
 
-        solve_fun = self.sr.solve_fun()
-        out, info = solve_fun(self._unscaled_matmul, grad, x0=x0)
+        out, info = solve_fun(self._unscaled_matmul, y, x0=x0)
 
         if self.scale is not None:
-            out = out / self.scale
+            out = jax.tree_multimap(jnp.divide, out, self.scale)
 
-        return unravel(out), info
+        # Reassemble real-imaginary split as needed
+        if self.mode != "holomorphic":
+            out = reassemble(out)
+
+        return out, info
 
     @jax.jit
     def to_dense(self) -> jnp.ndarray:
@@ -111,27 +183,13 @@ class QGTJacobianPyTree(LinearOperator):
 
         Returns:
             A dense matrix representation of this S matrix.
+            In R→R and R→C modes, real and imaginary parts of parameters get own rows/columns
         """
-        if scale is None:
-            O = self.O
-            diag = jnp.eye(self.O.shape[1])
+        if self.mode == "holomorphic":
+            pars = self.params
         else:
-            O = self.O * self.scale[jnp.newaxis, :]
-            diag = jnp.diag(self.scale ** 2)
+            pars = nkjax.tree_to_real(self.params)
 
-        return sum_inplace(O.T.conj() @ O) + self.diag_shift * diag
-
-
-def _grad_vmap_minus_mean(
-    fun: Callable, params: jnp.ndarray, samples: jnp.ndarray, holomorphic: bool
-):
-    """Calculates the gradient of a neural network for a number of samples
-    efficiently using vmap(grad),
-    and subtracts their mean for each parameter, i.e., each column
-    """
-    grads = jax.vmap(
-        jax.grad(fun, holomorphic=holomorphic), in_axes=(None, 0), out_axes=0
-    )(params, samples)
-    return grads - sum_inplace(grads.sum(axis=0, keepdims=True)) / (
-        grads.shape[0] * n_nodes
-    )
+        Npars = nkjax.tree_size(pars)
+        I = jax.numpy.eye(Npars)
+        return jax.vmap(self._split_matmul, in_axes=0)(I)
