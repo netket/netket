26d6f9ed9f7ba460ba9b5e5168c5038ce6dcaa86
Filippo Vicentini <filippovicentini@gmail.com>
2021-05-07 12:11:19 +0200

    Update netket/optimizer/sr/api.py
    Co-authored-by: Clemens Giuliani <clemens@inailuig.it>

--- netket/optimizer/sr/api.py
+++ netket/optimizer/sr/api.py
@@ -99,7 +99,6 @@ def SR(*args, **kwargs):
                     "SR takes at most 2 positional arguments but len(args) where provided"
                 )
 
-        print(kwargs)
         args = tuple()
 
         solver = None
