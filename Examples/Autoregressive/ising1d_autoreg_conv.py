910342c8acc462d0e99efc26de8f248ccd2a7f81
Dian Wu <dian.wu@epfl.ch>
2021-06-02 17:57:27 +0200

    Move examples to Examples/Autoregressive

--- Examples/Autoregressive/ising1d_autoreg_conv.py
+++ Examples/Autoregressive/ising1d_autoreg_conv.py
@@ -38,13 +38,20 @@ op = nk.optimizer.Sgd(learning_rate=0.1)
 sr = nk.optimizer.SR(diag_shift=0.01)
 
 # Variational state
+# With direct sampling, we don't need many samples in each step to form a
+# Markov chain, and we don't need to discard samples
 vs = nk.vqs.MCState(sa, ma, n_samples=64, n_discard=0)
 
 # n_parameters also takes masked parameters into account
+# The number of non-masked parameters is about a half
 print("n_parameters:", vs.n_parameters)
 
 # Variational monte carlo driver with a variational state
 gs = nk.VMC(ha, op, variational_state=vs, preconditioner=sr)
 
-# Run the optimization for 300 iterations
-gs.run(n_iter=300, out="test")
+# Run the optimization for 1000 iterations
+gs.run(n_iter=1000, out="test")
+
+# Use more samples to estimate the Hamiltonian and the error bar
+vs.n_samples = 1000
+print(vs.expect(ha))
