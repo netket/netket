59eda7769349846e8376219cb294fd97a1ff0550
Dian Wu <dian.wu@epfl.ch>
2021-06-02 17:26:52 +0200

    Better example

--- Examples/Ising1d/ising1d_autoreg.py
+++ Examples/Ising1d/ising1d_autoreg.py
@@ -26,7 +26,7 @@ hi = nk.hilbert.Spin(s=1 / 2, N=g.n_nodes)
 ha = nk.operator.Ising(hilbert=hi, graph=g, h=1)
 
 # Autoregressive neural network
-ma = nk.models.ARNNDense(hilbert=hi, layers=2, features=10)
+ma = nk.models.ARNNConv1D(hilbert=hi, layers=2, features=10, kernel_size=10)
 
 # Autoregressive direct sampling
 sa = nk.sampler.ARDirectSampler(hi)
@@ -34,8 +34,17 @@ sa = nk.sampler.ARDirectSampler(hi)
 # Optimizer
 op = nk.optimizer.Sgd(learning_rate=0.1)
 
-# Variational Monte Carlo driver
-gs = nk.VMC(ha, op, sa, ma, n_samples=1000, n_discard=0)
+# SR
+sr = nk.optimizer.SR(diag_shift=0.01)
 
-# Run the optimization for 1000 iterations
-gs.run(n_iter=1000, out="test")
+# Variational state
+vs = nk.vqs.MCState(sa, ma, n_samples=64, n_discard=0)
+
+# n_parameters also takes masked parameters into account
+print("n_parameters:", vs.n_parameters)
+
+# Variational monte carlo driver with a variational state
+gs = nk.VMC(ha, op, variational_state=vs, preconditioner=sr)
+
+# Run the optimization for 300 iterations
+gs.run(n_iter=300, out="test")
