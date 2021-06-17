8aab982ec3bc68712a8dbb7a4adcc6d6a56bc47e
Attila Szabó <attila.szabo@physics.ox.ac.uk>
2021-06-17 14:56:49 +0200

    more benchmarks

--- Benchmarks/irreps.py
+++ Benchmarks/irreps.py
@@ -1,18 +1,35 @@
 from netket.utils.group.planar import C, D
-from netket.graph import Triangular
+from netket.graph import Triangular, Square
 from timeit import timeit
+import numpy as np
 
+def benchmark(gen):
+    g = gen()
+    t1 = timeit(lambda: g._irrep_matrices, number=1)
+    g = gen()
+    t2 = timeit(lambda: g.old_irrep_matrices, number=1)
+    
+    squares = np.diag(g.product_table[g.inverse])
+    frob = np.array(np.rint(np.sum(g.character_table()[:,squares], axis=1)/len(g)), dtype=int)
 
-for size in [5,10,15,20]:
+    real = np.sum(frob == 1)
+    cplx = np.sum(frob == 0)
+    quat = np.sum(frob == -1)
+
+    print(f'  * {real} real, {cplx} complex, {quat} quaternionic irreps')
+    print(f'  * New implementation: {t1:.6f} sec')
+    print(f'  * Old implementation: {t2:.6f} sec')
+
+for size in [4,6,9,12]:
     lattice = Triangular((size,size))
-    print(f'{size} x {size} lattice')
+    print(f'{size} x {size} triangular lattice')
     for pg,pgn in [(C(3),'C(3)'), (C(6),'C(6)'), (D(3),'D(3)'), (D(6),'D(6)')]:
         print(f'* {pgn}')
-        g = lattice.space_group(pg)
-        t = timeit(lambda: g._irrep_matrices, number=1)
-        print(f'  * New implementation: {t:.6f} sec')
-        # need to reset caches
-        g = lattice.space_group(pg)
-        t = timeit(lambda: g.old_irrep_matrices, number=1)
-        print(f'  * Old implementation: {t:.6f} sec')
-    
+        benchmark(lambda: lattice.space_group(pg))
+
+for size in [3,6,9,12]:
+    lattice = Square(size)
+    print(f'{size} x {size} square lattice')
+    for pg,pgn in [(C(2), 'C(2)'), (C(4), 'C(4)'), (D(2), 'D(2)'), (D(4), 'D(4)')]:
+        print(f'* {pgn}')
+        benchmark(lambda: lattice.space_group(pg))
