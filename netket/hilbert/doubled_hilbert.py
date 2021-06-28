066ab0836526dab621a184a34913a421da583899
Filippo Vicentini <filippovicentini@gmail.com>
2021-06-28 19:32:23 +0200

    make doubledhilbert a parametric class

--- netket/hilbert/doubled_hilbert.py
+++ netket/hilbert/doubled_hilbert.py
@@ -16,10 +16,12 @@ from typing import List, Optional
 
 import numpy as np
 
+from netket.utils.dispatch import parametric
+
 from .abstract_hilbert import AbstractHilbert
 
 
-# TODO: Create parametric class
+@parametric
 class DoubledHilbert(AbstractHilbert):
     r"""Superoperatorial hilbert space for states living in the
     tensorised state H\otimes H, encoded according to Choi's isomorphism."""
