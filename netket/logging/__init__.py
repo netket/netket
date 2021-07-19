2b4a9563b19f77addd13a577c718f691a037fea4
Filippo Vicentini <filippovicentini@gmail.com>
2021-07-19 11:10:20 +0200

    remove json_log_old

--- netket/logging/__init__.py
+++ netket/logging/__init__.py
@@ -17,7 +17,6 @@ from .json_log import JsonLog
 from .state_log import StateLog
 from .tensorboard import TBLog
 
-from .json_log_old import JsonLog as JsonLogOld
 from netket.utils import tensorboard_available as _tensorboard_available
 
 
