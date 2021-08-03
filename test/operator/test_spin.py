676ae8ba4e4391707ab7a729fb2037ea37eec355
Damian Hofmann <damian.hofmann@mpsd.mpg.de>
2021-08-03 15:22:11 +0200

    Test spin operator relations also for higher spins

--- test/operator/test_spin.py
+++ test/operator/test_spin.py
@@ -1,7 +1,8 @@
+import pytest
+
 from netket.operator import spin
 import netket as nk
 import numpy as np
-
 from numpy.testing import assert_almost_equal
 
 herm_operators = {}
@@ -10,8 +11,9 @@ generic_operators = {}
 N = 4
 
 
-def test_pauli_algebra():
-    hi = nk.hilbert.Spin(1 / 2) ** N
+@pytest.mark.parametrize("S", [1 / 2, 1, 3 / 2])
+def test_pauli_algebra(S):
+    hi = nk.hilbert.Spin(S) ** N
 
     for i in range(N):
         sx = spin.sigmax(hi, i)
@@ -26,8 +28,11 @@ def test_pauli_algebra():
         assert_almost_equal(0.5 * (sx.to_dense() - 1j * sy.to_dense()), sm.to_dense())
         assert_almost_equal(0.5 * (sx.to_dense() + 1j * sy.to_dense()), sp.to_dense())
 
-        Imat = np.eye(hi.n_states)
+        if S == 1 / 2:
+            Imat = np.eye(hi.n_states)
 
-        # check that -i sx sy sz = I
-        assert_almost_equal((-1j * sx @ sy @ sz).to_dense(), Imat)
-        assert_almost_equal((-1j * sx.to_dense() @ sy.to_dense() @ sz.to_dense()), Imat)
+            # check that -i sx sy sz = I
+            assert_almost_equal((-1j * sx @ sy @ sz).to_dense(), Imat)
+            assert_almost_equal(
+                (-1j * sx.to_dense() @ sy.to_dense() @ sz.to_dense()), Imat
+            )
