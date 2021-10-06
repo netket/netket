5afe701797f659cd53daaaabb0cd80c61547122c
Clemens Giuliani <clemens@inailuig.it>
2021-10-06 16:47:15 +0200

    make batch_utils work with pytrees

--- netket/jax/_batch_utils.py
+++ netket/jax/_batch_utils.py
@@ -1,10 +1,20 @@
+import jax
 from netket.jax import compose
 
 
+def _treeify(f):
+    def _f(x, *args, **kwargs):
+        return jax.tree_map(lambda y: f(y, *args, **kwargs), x)
+
+    return _f
+
+
+@_treeify
 def unbatch(x):
     return x.reshape((-1,) + x.shape[2:])
 
 
+@_treeify
 def batch(x, batchsize=None):
     # batchsize=None -> add just a dummy batch dimension, same as np.expand_dims(x, 0)
     n = x.shape[0]
