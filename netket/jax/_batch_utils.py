47071a6debbbb83901ec15b75962e7a67d1e46e7
Clemens Giuliani <clemens@inailuig.it>
2021-10-07 15:32:07 +0200

    batch_utils: return batch_fn/unbatch_fn to restore shape

--- netket/jax/_batch_utils.py
+++ netket/jax/_batch_utils.py
@@ -1,4 +1,5 @@
 import jax
+from functools import partial
 
 
 def _treeify(f):
@@ -9,16 +10,35 @@ def _treeify(f):
 
 
 @_treeify
-def unbatch(x):
+def _unbatch(x):
     return x.reshape((-1,) + x.shape[2:])
 
 
 @_treeify
-def batch(x, batchsize=None):
-    # batchsize=None -> add just a dummy batch dimension, same as np.expand_dims(x, 0)
+def _batch(x, batch_size=None):
+    # batch_size=None -> add just a dummy batch dimension, same as np.expand_dims(x, 0)
     n = x.shape[0]
-    if batchsize is None:
-        batchsize = n
-    else:
-        assert (n % batchsize) == 0
-    return x.reshape((n // batchsize, batchsize) + x.shape[1:])
+    if batch_size is None:
+        batch_size = n
+
+    n_batches, residual = divmod(n, batch_size)
+    if residual != 0:
+        raise ValueError("the first dimension of x must be divisible by batch_size")
+    return x.reshape((n_batches, batch_size) + x.shape[1:])
+
+
+def _batch_size(x):
+    b = set(map(lambda x: x.shape[:2], jax.tree_leaves(x)))
+    if len(b) != 1:
+        raise ValueError(
+            "The arrays in x have inconsistent batch_size or number of batches"
+        )
+    return b.pop()[1]
+
+
+def unbatch(x):
+    return _unbatch(x), partial(_batch, batch_size=_batch_size(x))
+
+
+def batch(x, batch_size=None):
+    return _batch(x, batch_size), _unbatch
