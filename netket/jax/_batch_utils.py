967bda84aec83e8f65b4d4a5f041eda09e025049
Clemens Giuliani <clemens@inailuig.it>
2021-10-08 16:49:10 +0200

    docs

--- netket/jax/_batch_utils.py
+++ netket/jax/_batch_utils.py
@@ -36,9 +36,28 @@ def _batch_size(x):
     return b.pop()[1]
 
 
-def unbatch(x):
-    return _unbatch(x), partial(_batch, batch_size=_batch_size(x))
+def unbatch(x_batched):
+    """
+    Merge the first two axes of an array (or a pytree of arrays)
+    Args:
+        x_batched: an array (or pytree of arrays) of at least 2 dimensions
+    Returns: a pair (x, batch_fn)
+        where x is x_batched reshaped to (-1,)+x.shape[2:]
+        and batch_fn is a function which restores x given x_batched
+    """
+    return _unbatch(x_batched), partial(_batch, batch_size=_batch_size(x_batched))
 
 
 def batch(x, batch_size=None):
+    """
+    Split an array (or a pytree of arrays) into batches along the first axis
+    Args:
+        x: an array (or pytree of arrays)
+        batch_size: an integer or None (default)
+            The first axis in x must be a multiple of batch_size
+    Returns: a pair (x_batched, unbatch_fn) where
+        - x_batched is x reshaped to (-1, batch_size)+x.shape[1:]
+          if batch_size is None then it defaults to x.shape[0], i.e. just one batch
+        - unbatch_fn is a function which restores x given x_batched
+    """
     return _batch(x, batch_size), _unbatch
