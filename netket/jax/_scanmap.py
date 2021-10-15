e081f83997e888d278020c3adfd33936af64e697
Filippo Vicentini <filippovicentini@gmail.com>
2021-10-15 10:59:25 +0200

    Fix test failures (#943)

--- netket/jax/_scanmap.py
+++ netket/jax/_scanmap.py
@@ -2,9 +2,11 @@ import jax
 import jax.numpy as jnp
 
 from jax import linear_util as lu
-from jax.api_util import argnums_partial
+from jax.api_util import argnums_partial as _argnums_partial
 
-from functools import partial
+from functools import partial, wraps
+
+from netket.utils import module_version
 
 _tree_add = partial(jax.tree_multimap, jax.lax.add)
 _tree_zeros_like = partial(jax.tree_map, lambda x: jnp.zeros(x.shape))
@@ -16,6 +18,19 @@ def _multimap(f, *args):
         return tuple(map(lambda a: f(*a), zip(*args)))
     except TypeError:
         return f(*args)
+
+
+# TODO: When minimum jax is v0.2.22, remove this function and import directly
+# argnums_partial.
+# This works around onled argnums_partial implementations that did not have
+# require_static_args_hashable kwarg (which was implicitly False).
+if module_version(jax) >= (0, 2, 22):
+    argnums_partial = _argnums_partial
+else:
+
+    @wraps(_argnums_partial)
+    def argnums_partial(*args, require_static_args_hashable=True, **kwargs):
+        return _argnums_partial(*args, **kwargs)
 
 
 def scan_append_reduce(f, x, append_cond, op=_tree_add):
@@ -122,7 +137,9 @@ def scanmap(fun, scan_fun, argnums=0):
 
     def f_(*args, **kwargs):
         f = lu.wrap_init(fun, kwargs)
-        f_partial, dyn_args = argnums_partial(f, argnums, args)
+        f_partial, dyn_args = argnums_partial(
+            f, argnums, args, require_static_args_hashable=False
+        )
         return scan_fun(lambda x: f_partial.call_wrapped(*x), dyn_args)
 
     return f_
