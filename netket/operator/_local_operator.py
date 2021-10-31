6522572fb9ed656b131ae1842c7977568f407bfa
Vladimir Vargas-Calderón <vvargasc@unal.edu.co>
2021-10-30 21:32:43 -0500

    Revert "Revert "[fix] imposing csr format on sparse kronecker products""
    This reverts commit e409083b0bf846997d6a1f6268d3e1ab65ba6f06.

--- netket/operator/_local_operator.py
+++ netket/operator/_local_operator.py
@@ -777,7 +777,7 @@ class LocalOperator(DiscreteOperator):
                 acting_on.append(act_i.tolist())
             elif inters.size == 0:
                 # disjoint supports
-                operators.append(sp.kron(self._operators[i], op))
+                operators.append(sp.kron(self._operators[i], op, format="csr"))
                 acting_on.append(act_i.tolist() + act.tolist())
             else:
                 _act = list(act)
@@ -794,10 +794,10 @@ class LocalOperator(DiscreteOperator):
                         )
                         if site < actmin:
                             _act = [site] + _act
-                            _op = sp.kron(I, _op)
+                            _op = sp.kron(I, _op, format="csr")
                         else:  #  site > actmax
                             _act = _act + [site]
-                            _op = sp.kron(_op, I)
+                            _op = sp.kron(_op, I, format="csr")
 
                 act_i_min = min(act_i)
                 for site in act:
@@ -807,10 +807,10 @@ class LocalOperator(DiscreteOperator):
                         )
                         if site < act_i_min:
                             _act_i = [site] + _act_i
-                            _op_i = sp.kron(I, _op_i)
+                            _op_i = sp.kron(I, _op_i, format="csr")
                         else:  #  site > actmax
                             _act_i = _act_i + [site]
-                            _op_i = sp.kron(_op_i, I)
+                            _op_i = sp.kron(_op_i, I, format="csr")
 
                 # reorder
                 _op, _act = _reorder_kronecker_product(self.hilbert, _op, _act)
