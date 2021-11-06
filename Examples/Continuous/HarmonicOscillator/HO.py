b88b4582216da57c62052d4a12bd6ab55117ddac
Gabriel Pescia <g.pescia@gmail.com>
2021-11-06 12:28:31 +0100

    format

--- Examples/Continuous/HarmonicOscillator/HO.py
+++ Examples/Continuous/HarmonicOscillator/HO.py
@@ -15,7 +15,7 @@ hilb = nk.hilbert.Particle(N=5, L=(jnp.inf,), pbc=(False,))
 sab = nk.sampler.MetropolisGaussian(hilb, sigma=1.0, n_chains=16, n_sweeps=1)
 model = nk.models.Gaussian(dtype=float)
 
-ekin = nk.operator.KineticEnergy(hilb, mass=1.)
+ekin = nk.operator.KineticEnergy(hilb, mass=1.0)
 pot = nk.operator.PotentialEnergy(hilb, v)
 ha = ekin + 0.5 * pot
 
