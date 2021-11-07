6eaf6aaf4401a3f66f842c138d4dfc2e2d85fdb9
Filippo Vicentini <filippovicentini@gmail.com>
2021-11-07 18:48:31 +0100

    fixes

--- netket/hilbert/__init__.py
+++ netket/hilbert/__init__.py
@@ -17,14 +17,14 @@ from .discrete_hilbert import DiscreteHilbert
 from .homogeneous import HomogeneousHilbert
 from .tensor_hilbert import TensorHilbert
 
-from .abstract_continuous_space_particle import ContinuousHilbert
+from .continuous_hilbert import ContinuousHilbert
 
 from .custom_hilbert import CustomHilbert
 from .doubled_hilbert import DoubledHilbert
 from .spin import Spin
 from .fock import Fock
 from .qubit import Qubit
-from .continuous_particle import Particle
+from .particle import Particle
 
 from .tensor_hilbert import TensorHilbert
 
