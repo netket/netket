b8aceb3d3f9a9166e87983f17bb382d61a34d6af
Damian Hofmann <damian.hofmann@mpsd.mpg.de>
2021-11-30 13:30:31 +0100

    Add Kahan summation

---
+++ netket/utils/summation.py
@@ -0,0 +1,50 @@
+# Copyright 2021 The NetKet Authors - All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import jax
+
+from .struct import dataclass
+from .types import Scalar
+
+
+@dataclass
+class KahanSum:
+    """
+    Accumulator implementing Kahan summation [1], which reduces
+    the effect of accumulated floating-point error.
+
+    [1] https://en.wikipedia.org/wiki/Kahan_summation_algorithm
+    """
+
+    value: Scalar
+    compensator: Scalar = 0.0
+
+    def tree_flatten(self):
+        return (self.value, self.compensator), None
+
+    @classmethod
+    def tree_unflatten(cls, _, children):
+        return cls(*children)
+
+    def __add__(self, other: Scalar):
+        delta = other - self.compensator
+        new_value = self.value + delta
+        new_compensator = (new_value - self.value) - delta
+        return KahanSum(new_value, new_compensator)
+
+    def __radd__(self, other: Scalar):
+        delta = other - self.compensator
+        new_value = self.value + delta
+        self.compensator = (new_value - self.value) - delta
+        self.value = new_value
