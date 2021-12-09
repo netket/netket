3c25d61b3da6bf2fde5d7ccd6554b18b0e7bbbf9
Filippo Vicentini <filippovicentini@gmail.com>
2021-12-09 18:07:50 +0100

    clean

--- netket/tools/info.py
+++ netket/tools/info.py
@@ -115,10 +115,12 @@ def info():
 
         if hasattr(mpi4jax, "has_cuda_support"):
             printfmt("HAS_GPU_EXT", mpi4jax.has_cuda_support, indent=1)
-        else:
+        elif hasattr(mpi4jax, "_src"):
             if hasattr(mpi4jax._src, "xla_bridge"):
                 if hasattr(mpi4jax._src.xla_bridge, "HAS_GPU_EXT"):
-                    printfmt("HAS_GPU_EXT", mpi4jax._src.xla_bridge.HAS_GPU_EXT, indent=1)
+                    printfmt(
+                        "HAS_GPU_EXT", mpi4jax._src.xla_bridge.HAS_GPU_EXT, indent=1
+                    )
         print()
 
     if is_available("mpi4py"):
