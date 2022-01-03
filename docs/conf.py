188a0f53ceea2161a91b7c55d3e7f3e95f9f72f5
Filippo Vicentini <filippovicentini@gmail.com>
2022-01-03 19:52:37 +0100

    fix-docs

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,7 @@ source_suffix = {
 }
 
 # Markdown parser latex support
-myst_enable_extensions = ["dollarmath", "amsmath"]
+myst_enable_extensions = ["dollarmath", "amsmath", "colon_fence"]
 myst_update_mathjax = False
 mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
 
