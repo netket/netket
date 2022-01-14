Fix factor of 2 on gradients of models with real parameters (#1069)
Multiply by 2 the gradient (`use_covariance=True`) of models with real parameters. The previous value was wrong, because when we take the real part we don't multiply by 2. See Eq.3.15 of https://gitlab.com/nqs/ictp_school/-/blob/master/notes.pdf

THIS IS BREAKING because some models will now have a gradient doubled (so learning rate must be changed) but I think it's a bug fix

netket/vqs/exact/expect.py
@@ -117,7 +117,7 @@ def _exp_grad(
     Ō_grad = vjp_fun(ΔOΨ)[0]
 
     Ō_grad = jax.tree_multimap(
-        lambda x, target: (x if jnp.iscomplexobj(target) else x.real).astype(
+        lambda x, target: (x if jnp.iscomplexobj(target) else 2 * x.real).astype(
             target.dtype
         ),
         Ō_grad,

test/variational/test_continuous_expect.py
@@ -56,4 +56,4 @@ def test_expect():
     :math:`<\nabla V> = \nabla_p \int_0^5 dx V(x) |\psi(x)|^2 / \int_0^5 |\psi(x)|^2 = -0.140256 (\psi = \exp(p^2 x))`
     """
     np.testing.assert_allclose(0.1975164, sol.mean, atol=10 ** (-3))
-    np.testing.assert_allclose(-0.140256, 2 * O_grad, atol=10 ** (-3))
+    np.testing.assert_allclose(-0.140256, O_grad, atol=10 ** (-3))