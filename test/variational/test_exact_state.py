Fix gradient of expectation value for exact state with complex parameters (#1110)
Hi all,
I noticed that there was a small error in the computation of the gradient of expectation values for the exact state when complex parameters are used in the model. This PR fixes that.

The issue can for example be seen with the following little script comparing the computed gradient with one obtained by numdifftools (yes, I know, it's a little bit strange to use numdifftools to verify the gradients in code based on jax but setting it up with numdifftools was just quicker than making the expectation value evaluation differentiable with jax).

```python
import netket as nk
import numpy as np
import numdifftools as nd
import jax
from jax.nn.initializers import normal

g = nk.graph.Chain(6)
hi = nk.hilbert.Spin(0.5, N=g.n_nodes, total_sz=0.0)
ha = nk.operator.Heisenberg(hi, g, sign_rule=False)
model = nk.models.RBM(dtype=complex, use_visible_bias=False, use_hidden_bias=False, kernel_init=normal(stddev=1.))

vs = nk.vqs.ExactState(hi, model=model, seed=jax.random.PRNGKey(123))
vs_sampled = nk.vqs.MCState(nk.sampler.MetropolisExchange(hi, graph=g), model=model, n_samples=1000000)

""" We are doing a little bit of reshaping and stacking to map the complex
parameters to a flattened array of real values (two times the total number of params). This way,
we can evaluate the gradient with numdifftool and compare."""

kernel_shape = vs.parameters["Dense"]["kernel"].shape
def calculate_expect(parameters):
    kernel = parameters[:len(parameters)//2] + 1.j*parameters[len(parameters)//2:]
    vs.parameters = {"Dense" : {"kernel" : kernel.reshape(kernel_shape)}}
    return vs.expect(ha).mean.real

def calculate_grad_flattened(parameters):
    kernel = parameters[:len(parameters)//2] + 1.j*parameters[len(parameters)//2:]
    vs.parameters = {"Dense" : {"kernel" : kernel.reshape(kernel_shape)}}
    grad_flattened = vs.expect_and_grad(ha)[1]["Dense"]["kernel"].flatt

test/variational/test_exact_state.py
@@ -54,6 +54,12 @@ machines["model:(C->C)"] = RBM(
     kernel_init=normal(stddev=0.1),
     hidden_bias_init=normal(stddev=0.1),
 )
+machines["model:(C->C,α=5)"] = RBM(
+    alpha=5,
+    dtype=complex,
+    kernel_init=normal(stddev=0.3),
+    hidden_bias_init=normal(stddev=0.3),
+)
 
 operators = {}
 
@@ -122,13 +128,15 @@ def test_qutip_conversion(vstate):
     np.testing.assert_allclose(q_obj.data.todense(), ket.reshape(q_obj.shape))
 
 
-def test_derivatives_agree():
+@pytest.mark.parametrize(
+    "machine", [pytest.param(ma, id=name) for name, ma in machines.items()]
+)
+def test_derivatives_agree(machine):
     err = 1e-3
-    g = nk.graph.Hypercube(length=10, n_dim=1, pbc=True)
+    g = nk.graph.Chain(length=8, pbc=True)
     hi = nk.hilbert.Spin(s=1 / 2, N=g.n_nodes)
     ha = nk.operator.Ising(hilbert=hi, graph=g, h=1)
-    ma = nk.models.RBM(alpha=10, dtype=float)
-    vs = nk.vqs.ExactState(hi, ma)
+    vs = nk.vqs.ExactState(hi, machine)
 
     _, grads_exact = vs.expect_and_grad(ha)
 
@@ -181,15 +189,20 @@ def central_diff_grad(func, x, eps, *args, dtype=None):
 
 
 @common.skipif_mpi
-@pytest.mark.parametrize("L,n_iterations,h", [(4, 100, 1), (6, 100, 2), (8, 100, 3)])
+@pytest.mark.parametrize(
+    "L,n_iterations,h",
+    [(4, 100, 1), (6, 100, 2), (8, 100, 3)],
+)
+@pytest.mark.parametrize(
+    "machine", [pytest.param(ma, id=name) for name, ma in machines.items()]
+)
 def test_TFIM_energy_strictly_decreases(
-    L, n_iterations, h, abs_eps=1.0e-3, rel_eps=1.0e-4
+    L, n_iterations, h, machine, abs_eps=1.0e-3, rel_eps=1.0e-4
 ):
     g = nk.graph.Hypercube(length=L, n_dim=1, pbc=True)
     hi = nk.hilbert.Spin(s=1 / 2, N=g.n_nodes)
     ha = nk.operator.Ising(hilbert=hi, graph=g, h=h)
-    ma = nk.models.RBM(alpha=10, dtype=float)
-    vs = nk.vqs.ExactState(hi, ma)
+    vs = nk.vqs.ExactState(hi, machine)
 
     op = nk.optimizer.Sgd(learning_rate=0.003)
 