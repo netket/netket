531eed6829cd78abb9d2302c082f0ce406f546b2
Dian Wu <dian.wu@epfl.ch>
2022-02-18 02:24:38 +0100

    Change flakehell to pyproject-flake8 in pre-commit

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ DEV_DEPENDENCIES = [
     "coverage>=5",
     "pytest-cov>=2.10.1",
     "networkx~=2.4",
-    "flaky>=3.7",
-    "pre-commit",
+    "pre-commit>=2.7",
     "black==22.1.0",
-    "flakehell>=0.9",
+    "pyproject-flake8==0.0.1a2.post1",
+    "flake8==4.0.1",
 ]
 MPI_DEPENDENCIES = ["mpi4py>=3.0.1, <4", "mpi4jax~=0.3.1"]
 EXTRA_DEPENDENCIES = ["tensorboardx>=2.0.0", "openfermion>=1.0.0"]
