fix wrong mpi reducation scaling in nkjax.expect

netket/jax/_expect.py
@@ -20,7 +20,7 @@ from functools import partial
 import jax
 from jax import numpy as jnp
 
-from netket.stats import statistics as mpi_statistics, mean as mpi_mean, Stats
+from netket.stats import statistics as mpi_statistics, sum as mpi_sum, Stats
 from netket.utils.types import PyTree
 
 from ._vjp import vjp as nkvjp
@@ -89,9 +89,7 @@ def _expect_bwd(n_chains, log_pdf, expected_fun, residuals, dout):
         log_p = log_pdf(pars, σ)
         term1 = jax.vmap(jnp.multiply)(ΔL_σ, log_p)
         term2 = expected_fun(pars, σ, *cost_args)
-        out = mpi_mean(term1 + term2, axis=0)
-        out = out.sum()
-        return out
+        return mpi_sum(term1 + term2)
 
     _, pb = nkvjp(f, pars, σ, *cost_args)
     grad_f = pb(dL̄)