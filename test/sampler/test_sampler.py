31af1d916e103242261b220e3df2a7ae2717bde7
Filippo Vicentini <filippovicentini@gmail.com>
2022-03-31 18:40:19 +0200

    mark them as xfail

--- test/sampler/test_sampler.py
+++ test/sampler/test_sampler.py
@@ -213,6 +213,9 @@ def findrng(rng):
 
 # Mark tests that we know are failing on correctedness
 def failing_test(sampler):
+    # PT Samplers are experimental and fail the correctness test.
+    if isinstance(sampler, nkx.sampler.MetropolisPtSampler):
+        return True
     return False
 
 
