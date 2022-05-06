netket/netket

commit 490d1ccf5deb924d122db9bf9fdd70a58e371506

refactor LocalOperator logic (#1095)
This PR completely refactors the front-end logic of LocalOperator to match what happens in PauliString, that is:
 
- `LocalOperator` merely stores a list of operators and their `acting_on` when constructed and manipulated. Arrays/structures needed to perform` get_conn`, etc are constructed only when needed. This should alleviate slow-downs seen by users when trying to construct huge localoperators.'
  - This should make it easy in the future to separate the `LocalOperator` front-end code from the numba implementation and maybe write a jax implementation.
- The logic is now _simplified_, in the sense that all matrices are never modified in-place. This makes it more memory efficient in some cases because we never duplicate arrays anymore (provided you build directly the LocalOperator with the correct matrices), which is what @VolodyaCO wanted.
- The logic is simpler to follow, IMHO, because we modify a very simple data structure in only one place, and user data can onlhy be inputted at the constructor. Therefore we can skip a lot of data validation in the internal methods.

This PR also gets us 98% of the way towards supporting both sparse and dense amtrices inside of Localoperators (there's just a function or two that must be fixed) which has been a longstanding feature request.

netket/operator/_local_operator_compile_helpers.py
@@ -1,0 +1,181 @@
+# Copyright 2022 The NetKet Authors - All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This file contains functions generating the numba-packed representation of local
+operators.
+"""
+
+from typing import Tuple, Union, List, Optional
+
+import numpy as np
+from scipy import sparse
+import numba
+
+from netket.hilbert import AbstractHilbert
+from netket.utils.types import DType, Array
+
+
+def pack_internals(
+    hilbert: AbstractHilbert,
+    operators_dict: dict,
+    constant,
+    dtype: DType,
+    mel_cutoff: float,
+):
+    """
+    Take the internal lazy representation of a local operator and returns the arrays
+    needed for the numba implementation.
+
+    This takes as input a dictionary with Tuples as keys, the `acting_on` and matrices as values.
+    The keys represent the sites upon which the matrix acts.
+    It is assumed that the integer in the tuples are sorted.
+
+    Returns a dictionary with all the data fields
+    """
+    op_acting_on = list(operators_dict.keys())
+    operators = list(operators_dict.values())
+    n_operators = len(operators_dict)
+
+    """Analyze the operator strings and precompute arrays for get_conn inference"""
+    acting_size = np.array([len(aon) for aon in op_acting_on], dtype=np.intp)
+    max_acting_on_sz = np.max(acting_size)
+    max_local_hilbert_size = max(
+        [max(map(hilbert.size_at_index, aon)) for aon in op_acting_on]
+    )
+    max_op_size = max(map(lambda x: x.shape[0], operators))
+
+    acting_on = np.full((n_operators, max_acting_on_sz), -1, dtype=np.intp)
+    for (i, aon) in enumerate(op_acting_on):
+        acting_on[i][: len(aon)] = aon
+
+    local_states = np.full(
+        (n_operators, max_acting_on_sz, max_local_hilbert_size), np.nan
+    )
+    basis = np.full((n_operators, max_acting_on_sz), 1e10, dtype=np.int64)
+
+    diag_mels = np.full((n_operators, max_op_size), np.nan, dtype=dtype)
+    mels = np.full(
+        (n_operators, max_op_size, max_op_size - 1),
+        np.nan,
+        dtype=dtype,
+    )
+    x_prime = np.full(
+        (n_operators, max_op_size, max_op_size - 1, max_acting_on_sz),
+        -1,
+        dtype=np.float64,
+    )
+    n_conns = np.full((n_operators, max_op_size), -1, dtype=np.intp)
+
+    for (i, (aon, op)) in enumerate(operators_dict.items()):
+        aon_size = len(aon)
+        n_local_states_per_site = np.asarray([hilbert.size_at_index(i) for i in aon])
+
+        ## add an operator to local_states
+        for (j, site) in enumerate(aon):
+            local_states[i, j, : hilbert.shape[site]] = np.asarray(
+                hilbert.states_at_index(site)
+            )
+
+        ba = 1
+        for s in range(aon_size):
+            basis[i, s] = ba
+            ba *= hilbert.shape[aon_size - s - 1]
+
+        # eventually could support sparse matrices
+        # if isinstance(op, sparse.spmatrix):
+        #    op = op.todense()
+
+        _append_matrix(
+            op,
+            diag_mels[i],
+            mels[i],
+            x_prime[i],
+            n_conns[i],
+            aon_size,
+            local_states[i],
+            mel_cutoff,
+            n_local_states_per_site,
+        )
+
+    nonzero_diagonal = (
+        np.any(np.abs(diag_mels) >= mel_cutoff) or np.abs(constant) >= mel_cutoff
+    )
+
+    max_conn_size = n_operators if nonzero_diagonal else 0
+    for op in operators:
+        nnz_mat = np.abs(op) > mel_cutoff
+        nnz_mat[np.diag_indices(nnz_mat.shape[0])] = False
+        nnz_rows = np.sum(nnz_mat, axis=1)
+        max_conn_size += np.max(nnz_rows)
+
+    return {
+        "acting_on": acting_on,
+        "acting_size": acting_size,
+        "diag_mels": diag_mels,
+        "mels": mels,
+        "x_prime": x_prime,
+        "n_conns": n_conns,
+        "local_states": local_states,
+        "basis": basis,
+        "nonzero_diagonal": nonzero_diagonal,
+        "max_conn_size": max_conn_size,
+    }
+
+
+@numba.jit(nopython=True)
+def _append_matrix(
+    operator,
+    diag_mels,
+    mels,
+    x_prime,
+    n_conns,
+    acting_size,
+    local_states_per_site,
+    epsilon,
+    hilb_size_per_site,
+):
+    op_size = operator.shape[0]
+    assert op_size == operator.shape[1]
+    for i in range(op_size):
+        diag_mels[i] = operator[i, i]
+        n_conns[i] = 0
+        for j in range(op_size):
+            if i != j and np.abs(operator[i, j]) > epsilon:
+                k_conn = n_conns[i]
+                mels[i, k_conn] = operator[i, j]
+                _number_to_state(
+                    j,
+                    hilb_size_per_site,
+                    local_states_per_site[:acting_size, :],
+                    x_prime[i, k_conn, :acting_size],
+                )
+                n_conns[i] += 1
+
+
+@numba.jit(nopython=True)
+def _number_to_state(number, hilbert_size_per_site, local_states_per_site, out):
+
+    out[:] = local_states_per_site[:, 0]
+    size = out.shape[0]
+
+    ip = number
+    k = size - 1
+    while ip > 0:
+        local_size = hilbert_size_per_site[k]
+        out[k] = local_states_per_site[k, ip % local_size]
+        ip = ip // local_size
+        k -= 1
+
+    return out

test/operator/test_local_operator.py
@@ -10,6 +10,8 @@ import numpy as np
 import pytest
 from pytest import raises
 
+from scipy import sparse
+
 import jax
 
 herm_operators = {}
@@ -24,6 +26,8 @@ sp = [[0, 1], [0, 0]]
 g = nk.graph.Graph(edges=[[i, i + 1] for i in range(8)])
 hi = nk.hilbert.CustomHilbert(local_states=[-1, 1], N=g.n_nodes)
 
+sy_sparse = sparse.csr_matrix(sy)
+
 
 def _loc(*args):
     return nk.operator.LocalOperator(hi, *args)
@@ -35,9 +39,11 @@ szsz_hat = _loc(sz, [0]) @ _loc(sz, [1])
 szsz_hat += _loc(sz, [4]) @ _loc(sz, [5])
 szsz_hat += _loc(sz, [6]) @ _loc(sz, [8])
 szsz_hat += _loc(sz, [7]) @ _loc(sz, [0])
+sy_sparse_hat = _loc([sy_sparse] * 3, [[0], [1], [5]])
 
 herm_operators["sx (real op)"] = sx_hat
 herm_operators["sy"] = sy_hat
+herm_operators["sy_sparse"] = sy_sparse_hat
 
 herm_operators["Custom Hamiltonian"] = sx_hat + sy_hat + szsz_hat
 herm_operators["Custom Hamiltonian Prod"] = sx_hat * 1.5 + (2.0 * sy_hat)
@@ -52,9 +58,13 @@ generic_operators["sigma +/-"] = (sm_hat, sp_hat)
 def assert_same_matrices(matl, matr, eps=1.0e-6):
     if isinstance(matl, AbstractOperator):
         matl = matl.to_dense()
+    elif isinstance(matl, sparse.csr_matrix):
+        matl = matl.todense()
 
     if isinstance(matr, AbstractOperator):
         matr = matr.to_dense()
+    elif isinstance(matr, sparse.csr_matrix):
+        matr = matr.todense()
 
     np.testing.assert_allclose(matl, matr, atol=eps, rtol=eps)
 
@@ -348,7 +358,7 @@ def test_copy(op):
     op_copy = op.copy()
     assert op_copy is not op
     for o1, o2 in zip(op._operators, op_copy._operators):
-        assert o1 is not o2
+        # assert o1 is not o2
         assert_same_matrices(o1, o2)
     assert_same_matrices(op, op_copy)
 
@@ -447,3 +457,15 @@ def test_correct_minus():
     assert_same_matrices(op, op2)
     # they commute
     assert_same_matrices(op, opd)
+
+
+def test_operator():
+    # check that heterogeneous hilbert spaces are ordered correctly #1106
+    n_max = 5
+    hi = nk.hilbert.Fock(n_max, N=1) * nk.hilbert.Qubit()
+    a = nk.operator.boson.destroy(hi, 0)
+    sp = nk.operator.spin.sigmap(hi, 1)
+    op1 = a * sp
+    op2 = sp * a
+
+    assert_same_matrices(op1, op2)

netket/operator/_graph_operator.py
@@ -100,7 +100,7 @@ class GraphOperator(LocalOperator):
          >>> op = nk.operator.GraphOperator(
          ... hi, site_ops=[sigmax], bond_ops=[mszsz], graph=g)
          >>> print(op)
-         GraphOperator(dim=20, #acting_on=40 locations, constant=0, dtype=float64, graph=Graph(n_nodes=20, n_edges=20))
+         GraphOperator(dim=20, #acting_on=40 locations, constant=0.0, dtype=float64, graph=Graph(n_nodes=20, n_edges=20))
         """
         acting_on_subspace = check_acting_on_subspace(
             acting_on_subspace, hilbert, graph

netket/operator/_hamiltonian.py
@@ -91,16 +91,15 @@ class SpecialHamiltonian(DiscreteOperator):
         return self.to_local_operator().__rmul__(other)
 
     def _op__matmul__(self, other):
+        if hasattr(other, "to_local_operator"):
+            other = other.to_local_operator()
         return self.to_local_operator().__matmul__(other)
 
     def _op__rmatmul__(self, other):
-        if self == other and self.is_hermitian:
-            return Squared(self)
+        if hasattr(other, "to_local_operator"):
+            other = other.to_local_operator()
 
         return self.to_local_operator().__matmul__(other)
-
-    def _concrete_matmul_(self, other):
-        return self.to_local_operator() @ other
 
 
 class Ising(SpecialHamiltonian):

netket/operator/_lazy.py
@@ -208,7 +208,7 @@ class Squared(WrappedOperator):
         return self.parent.dtype
 
     def collect(self):
-        return self.parent.H.collect()._concrete_matmul_(self.parent)
+        return self.parent.H.collect()._op__matmul__(self.parent)
 
     @property
     def T(self):

netket/operator/_local_operator_helpers.py
@@ -1,0 +1,252 @@
+# Copyright 2022 The NetKet Authors - All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Tuple, Union, List, Optional
+
+import numba
+
+import functools
+import numbers
+
+from textwrap import dedent
+
+import numpy as np
+from numba import jit
+
+from scipy.sparse import spmatrix
+
+from netket.hilbert import AbstractHilbert, Fock
+from netket.utils.types import DType, Array
+
+from ._discrete_operator import DiscreteOperator
+
+
+def _dtype(obj: Union[numbers.Number, Array, "LocalOperator"]) -> DType:
+    """
+    Returns the dtype of the input object
+    """
+    if isinstance(obj, numbers.Number):
+        return type(obj)
+    elif isinstance(obj, DiscreteOperator):
+        return obj.dtype
+    elif isinstance(obj, np.ndarray):
+        return obj.dtype
+    else:
+        raise TypeError(f"cannot deduce dtype of object type {type(obj)}: {obj}")
+
+
+def cast_operator_matrix_dtype(matrix, dtype):
+    """
+    Changes the dtype of a matrix, without changing the structural type of the object.
+
+    This makes sure that if you pass sparse arrays to a LocalOperator, they remain
+    sparse even if you change the dtype
+    """
+    # must copy
+    # return np.asarray(matrix, dtype=dtype)
+    return matrix.astype(dtype)
+
+
+def _standardize_matrix_input_type(op):
+    """
+    Standardize the structural type of operators stored in LocalOperator.
+
+    Eventually, we could also support spmatrices (but some work will be needed.)
+    """
+    if isinstance(op, list):
+        return np.asarray(op)
+    elif isinstance(op, spmatrix):
+        return op.todense()
+    else:
+        return op
+
+
+def canonicalize_input(
+    hilbert: AbstractHilbert, operators, acting_on, constant, *, dtype=None
+):
+    """
+    Takes as inputs the inputs to the constructor of LocalOperator and canonicalizes
+    them by ensuring the following holds:
+     - acting_on is a list of list
+     - acting_on[i] are sorted
+     - operators is list of matrices
+     - all dtypes match
+
+    Args:
+        hilbert: The hilbert space
+
+    Returns:
+        List of operators, acting ons and dtypes.
+    """
+    # check if passing a single operator or a list of operators
+    if isinstance(acting_on, numbers.Number):
+        acting_on = [acting_on]
+
+    is_nested = any(hasattr(i, "__len__") for i in acting_on)
+    if not is_nested:
+        operators = [operators]
+        acting_on = [acting_on]
+
+    if all(len(aon) == 0 for aon in acting_on):
+        operators = []
+        acting_on = []
+    else:
+        if max(map(max, acting_on)) >= hilbert.size or min(map(min, acting_on)) < 0:
+            raise ValueError("An operator acts on an invalid set of sites.")
+
+    acting_on = [tuple(aon) for aon in acting_on]
+    # operators = [np.asarray(operator) for operator in operators]
+    operators = [_standardize_matrix_input_type(op) for op in operators]
+
+    # If we asked for a specific dtype, enforce it.
+    if dtype is None:
+        dtype = np.promote_types(np.float32, _dtype(constant))
+        dtype = functools.reduce(
+            lambda dt, op: np.promote_types(dt, op.dtype), operators, dtype
+        )
+    dtype = np.empty((), dtype=dtype).dtype
+
+    canonicalized_operators = []
+    canonicalized_acting_on = []
+    for (operator, acting_on) in zip(operators, acting_on):
+        if operator.dtype is not dtype:
+            operator = cast_operator_matrix_dtype(operator, dtype=dtype)
+
+        # re-sort the operator
+        operator, acting_on = _reorder_kronecker_product(hilbert, operator, acting_on)
+        canonicalized_operators.append(operator)
+        canonicalized_acting_on.append(acting_on)
+
+    return canonicalized_operators, canonicalized_acting_on, dtype
+
+
+# TODO: support sparse arrays without returning dense arrays
+def _reorder_kronecker_product(hi, mat, acting_on) -> Tuple[Array, Tuple]:
+    """
+    Reorders the matrix resulting from a kronecker product of several
+    operators in such a way to sort acting_on.
+
+    A conceptual example is the following:
+    if `mat = Â ⊗ B̂ ⊗ Ĉ` and `acting_on = [[2],[1],[3]`
+    you will get `result = B̂ ⊗ Â ⊗ Ĉ, [[1], [2], [3]].
+
+    However, essentially, A,B,C represent some operators acting on
+    thei sub-space acting_on[1], [2] and [3] of the hilbert space.
+
+    This function also handles any possible set of values in acting_on.
+
+    The inner logic uses the Fock.all_states(), number_to_state and
+    state_to_number to perform the re-ordering.
+    """
+    acting_on_sorted = np.sort(acting_on)
+    if np.array_equal(acting_on_sorted, acting_on):
+        return mat, acting_on
+
+    # could write custom binary <-> int logic instead of using Fock...
+    # Since i need to work with bit-strings (where instead of bits i
+    # have integers, in order to support arbitrary size spaces) this
+    # is exactly what hilbert.to_number() and viceversa do.
+
+    # target ordering binary representation
+    hi_subspace = Fock(hi.shape[acting_on_sorted[0]] - 1)
+    for site in acting_on_sorted[1:]:
+        hi_subspace = hi_subspace * Fock(hi.shape[site] - 1)
+
+    hi_unsorted_subspace = Fock(hi.shape[acting_on[0]] - 1)
+    for site in acting_on[1:]:
+        hi_unsorted_subspace = hi_unsorted_subspace * Fock(hi.shape[site] - 1)
+
+    # find how to map target ordering back to unordered
+    acting_on_unsorted_ids = np.zeros(len(acting_on), dtype=np.intp)
+    for (i, site) in enumerate(acting_on):
+        acting_on_unsorted_ids[i] = np.argmax(site == acting_on_sorted)
+
+    # now it is valid that
+    # acting_on_sorted == acting_on[acting_on_unsorted_ids]
+
+    # generate n-bit strings in the target ordering
+    v = hi_subspace.all_states()
+
+    # convert them to origin (unordered) ordering
+    v_unsorted = v[:, acting_on_unsorted_ids]
+    # convert the unordered bit-strings to numbers in the target space.
+    n_unsorted = hi_unsorted_subspace.states_to_numbers(v_unsorted)
+
+    # reorder the matrix
+    mat_sorted = mat[n_unsorted, :][:, n_unsorted]
+
+    return mat_sorted, tuple(acting_on_sorted)
+
+
+# TODO: support sparse arrays without returning dense arrays
+def _multiply_operators(
+    hilbert, support_A: Tuple, A: "array", support_B: Tuple, B: "array", *, dtype
+) -> Tuple[Tuple, "array"]:
+    """
+    Returns the `Tuple[acting_on, Matrix]` representing the operator obtained by
+    multiplying the two input operators A and B.
+    """
+    support_A = np.asarray(support_A)
+    support_B = np.asarray(support_B)
+
+    inters = np.intersect1d(support_A, support_B, return_indices=False)
+
+    if support_A.size == support_B.size and np.array_equal(support_A, support_B):
+        return tuple(support_A), A @ B
+    elif inters.size == 0:
+        # disjoint supports
+        support = tuple(np.concatenate([support_A, support_B]))
+        operator = np.kron(A, B)
+        operator, support = _reorder_kronecker_product(hilbert, operator, support)
+        return tuple(support), operator
+    else:
+        _support_A = list(support_A)
+        _support_B = list(support_B)
+        _A = A.copy()
+        _B = B.copy()
+
+        # expand _act to match _act_i
+        supp_B_min = min(support_B)
+        for site in support_A:
+            if site not in support_B:
+                I = np.eye(hilbert.shape[site], dtype=dtype)
+                if site < supp_B_min:
+                    _support_B = [site] + _support_B
+                    _B = np.kron(I, _B)
+                else:  # site > actmax
+                    _support_B = _support_B + [site]
+                    _B = np.kron(_B, I)
+
+        supp_A_min = min(support_A)
+        for site in support_B:
+            if site not in support_A:
+                I = np.eye(hilbert.shape[site], dtype=dtype)
+                if site < supp_A_min:
+                    _support_A = [site] + _support_A
+                    _A = np.kron(I, _A)
+                else:  #  site > actmax
+                    _support_A = _support_A + [site]
+                    _A = np.kron(_A, I)
+
+        # reorder
+        _A, _support_A = _reorder_kronecker_product(hilbert, _A, _support_A)
+        _B, _support_B = _reorder_kronecker_product(hilbert, _B, _support_B)
+
+        if len(_support_A) == len(_support_B) and np.array_equal(
+            _support_A, _support_B
+        ):
+            # back to the case of non-interesecting with same support
+            return tuple(_support_A), _A @ _B
+        else:
+            raise ValueError("Something failed")

test/groundstate/test_vmc.py
@@ -35,7 +35,7 @@ def _setup_vmc(dtype=np.float32, sr=True):
 
     # Add custom observable
     X = [[0, 1], [1, 0]]
-    sx = nk.operator.LocalOperator(hi, [X] * L, [[i] for i in range(8)])
+    sx = nk.operator.LocalOperator(hi, [X] * L, [[i] for i in range(L)])
 
     if sr:
         sr_config = nk.optimizer.SR()

commit 94fbc6338de173283927f62ad44f36b232b586e3

fix #1192 (bug introduced in  #1095 ) (#1193)
add test
changelog

netket/operator/_local_operator_compile_helpers.py
@@ -88,7 +88,7 @@ def pack_internals(
         ba = 1
         for s in range(aon_size):
             basis[i, s] = ba
-            ba *= hilbert.shape[aon_size - s - 1]
+            ba *= hilbert.shape[aon[aon_size - s - 1]]
 
         # eventually could support sparse matrices
         # if isinstance(op, sparse.spmatrix):

test/operator/test_local_operator.py
@@ -478,3 +478,13 @@ def test_error_if_wrong_shape():
     mat = np.random.rand(3, 3)
     with pytest.raises(ValueError):
         nk.operator.LocalOperator(hi, mat, [0, 1])
+
+
+def test_inhomogeneous_hilb_issue_1192():
+    # Issue #1192
+    # https://github.com/netket/netket/issues/1192
+    hi = nk.hilbert.Fock(n_max=3) * nk.hilbert.Spin(1 / 2) * nk.hilbert.Fock(n_max=2)
+    c0 = bcreate(hi, 0)
+    d2 = bdestroy(hi, 2)
+
+    assert_same_matrices(c0 @ d2, c0.to_dense() @ d2.to_dense())