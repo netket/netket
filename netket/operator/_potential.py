fixes the SumOperator such that SumOperators can also be added and multiplied + allows for energy computation of complex wf

netket/operator/_potential.py
@@ -43,14 +43,14 @@ class PotentialEnergy(ContinuousOperator):
 
         self._afun = afun
 
-        self.coefficient = jnp.array(coefficient, dtype=dtype)
+        self.coefficient = jnp.asarray(coefficient, dtype=dtype)
 
-        super().__init__(hilbert, self.coefficient.dtype)
+        super().__init__(hilbert, dtype)
 
     def _expect_kernel(
         self, logpsi: Callable, params: PyTree, x: Array, coefficient: Optional[PyTree]
     ):
-        return coefficient * self._afun(x)
+        return jnp.array(coefficient) * self._afun(x)
 
     @partial(jax.vmap, in_axes=(None, None, None, 0, None))
     def _expect_kernel_batched(

netket/operator/_sumoperators.py
@@ -29,7 +29,7 @@ class SumOperator(ContinuousOperator):
 
     def __init__(
         self,
-        *operators: List,
+        operators: List,
         coefficients: Union[float, List[float]] = 1.0,
         dtype: Optional[DType] = None,
     ):
@@ -40,6 +40,19 @@ class SumOperator(ContinuousOperator):
             coefficients: A coefficient for each ContinuousOperator object
             dtype: Data type of the matrix elements. Defaults to `np.float64`
         """
+        new_operators = []
+        new_coeffs = []
+        for op, c in zip(operators, coefficients):
+            if isinstance(op, SumOperator):
+                for i, oi in enumerate(op._ops):
+                    new_operators.append(oi)
+                    new_coeffs.append(op._pack_arguments()[i] * c)
+            else:
+                new_operators.append(op)
+                new_coeffs.append(c * op._pack_arguments())
+
+        operators = new_operators
+        coefficients = new_coeffs
 
         hil = [op.hilbert for op in operators]
         if not all(_ == hil[0] for _ in hil):
@@ -85,7 +98,7 @@ class SumOperator(ContinuousOperator):
         return sum(result)
 
     def _pack_arguments(self):
-        return [self._coeff * jnp.array(op._pack_arguments()) for op in self._ops]
+        return list(self._coeff)
 
     def __repr__(self):
         return f"SumOperator(coefficients={self._pack_arguments()})"

test/operator/test_continuous_operator.py
@@ -39,7 +39,9 @@ kin10p52 = kin1 + 0.5 * kin2
 
 # sum of potential and kinetic operators
 etot = pottot + kintot
-
+etot2 = pot1 + pot2 + kin1 + kin2
+print(etot)
+print(etot2)
 model1 = lambda p, x: 1.0
 model2 = lambda p, x: jnp.sum(x**3)
 kinexact = lambda x: -0.5 * jnp.sum((3 * x**2) ** 2 + 6 * x, axis=-1)
@@ -102,5 +104,9 @@ def test_sumoperator():
     np.testing.assert_allclose(kinen10p52, kinenergy10p52ex)
 
     enertot = etot._expect_kernel_batched(model2, 0.0, x, etot._pack_arguments())
+    enertot2 = etot2._expect_kernel_batched(model2, 0.0, x, etot2._pack_arguments())
     enerexact = v1(x) + v2_vec(x) + kinexact(x) / kin1.mass + kinexact(x) / kin2.mass
+    print(enertot)
+    print(enertot2)
     np.testing.assert_allclose(enertot, enerexact)
+    np.testing.assert_allclose(enertot2, enerexact)