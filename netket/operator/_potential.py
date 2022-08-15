fixes the SumOperator such that SumOperators can also be added and multiplied + allows for energy computation of complex wf
Random state for periodic system changed + n_per_spin in Particle hilbert + complex wf

clean

netket/operator/_potential.py
@@ -43,9 +43,9 @@ class PotentialEnergy(ContinuousOperator):
 
         self._afun = afun
 
-        self.coefficient = jnp.asarray(coefficient, dtype=dtype)
+        self.coefficient = jnp.array(coefficient, dtype=dtype)
 
-        super().__init__(hilbert, dtype)
+        super().__init__(hilbert, self.coefficient.dtype)
 
     def _expect_kernel(
         self, logpsi: Callable, params: PyTree, x: Array, coefficient: Optional[PyTree]