1c10f5f0917feab1000661bc17c04afff64915b8
Filippo Vicentini <filippovicentini@gmail.com>
2023-01-26 09:45:26 +0100

    Fix netket for jax 0.4

--- netket/utils/types.py
+++ netket/utils/types.py
@@ -25,7 +25,13 @@ SeedT = Union[int, PRNGKeyT]
 Shape = Sequence[int]
 DType = Any  # this could be a real type?
 
-Array = Union[_np.ndarray, _jaxlib.xla_extension.DeviceArray, _jax.core.Tracer]
+if not hasattr(_jax, "Array"):
+	# pre jax 0.4
+	Array = Union[_np.ndarray, _jaxlib.xla_extension.DeviceArray, _jax.core.Tracer]
+else:
+	# TODO keep only this after jax>=0.4 is required
+	Array = Union[_np.ndarray, _jax.Array]
+
 ArrayLike = Any  # Objects that are valid inputs to (np|jnp).asarray.
 
 NNInitFunc = Callable[[PRNGKeyT, Shape, DType], Array]
