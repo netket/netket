3be046c220983e26ea61a0acaef0a4820d256517
Filippo Vicentini <filippovicentini@gmail.com>
2023-04-24 20:38:30 +0200

    add combinedrule
    rename to tensorrule
    
    
    add year
    
    
    add tensorrule
    
    
    t

--- netket/sampler/rules/__init__.py
+++ netket/sampler/rules/__init__.py
@@ -20,6 +20,7 @@ from .exchange import ExchangeRule
 from .hamiltonian import HamiltonianRule
 from .continuous_gaussian import GaussianRule
 from .langevin import LangevinRule
+from .tensor import tensorRule as TensorRule
 
 # numpy backend
 from .local_numpy import LocalRuleNumpy
