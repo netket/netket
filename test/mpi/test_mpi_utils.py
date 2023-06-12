e96e00e40cd63529611a654fd78fc7daad5c11bf
Clemens Giuliani <clemens@inailuig.it>
2023-06-12 10:13:01 +0200

    also test scalar arr

--- test/mpi/test_mpi_utils.py
+++ test/mpi/test_mpi_utils.py
@@ -49,7 +49,7 @@ def test_key_split(_mpi_size, _mpi_comm, _mpi_rank):
     assert len(keys) == size
 
 
-@common.onlyif_mpi
+# @common.onlyif_mpi
 @common.named_parametrize("complex_", [False, True])
 def test_mpi_logsumexp(_mpi_size, _mpi_comm, _mpi_rank, complex_):
     s = (16 * _mpi_size, 3, 2)
@@ -109,4 +109,26 @@ def test_mpi_logsumexp(_mpi_size, _mpi_comm, _mpi_rank, complex_):
 
     y1 = jax.scipy.special.logsumexp(a, b=b, axis=(0, 1))
     y2, _ = mpi.mpi_logsumexp(a_, b=b_, axis=(0, 1))
+    np.testing.assert_allclose(y1, y2)
+
+
+@common.named_parametrize("complex_", [False, True])
+def test_mpi_logsumexp_scalar(_mpi_size, _mpi_comm, _mpi_rank, complex_):
+    s = (_mpi_size,)
+    k1, k2, k3, k4 = jax.random.split(jax.random.PRNGKey(123), 4)
+    a = jax.random.uniform(k1, shape=s)
+    b = jax.random.uniform(k2, shape=s)
+    if complex_:
+        a = jax.lax.complex(a, jax.random.uniform(k3, shape=s))
+        b = jax.lax.complex(b, jax.random.uniform(k4, shape=s))
+
+    print(_mpi_rank, "a", a[_mpi_rank])
+    print(_mpi_rank, "a_", a)
+
+    # y1 = jax.scipy.special.logsumexp(a, b=b)
+    # y2, _ = mpi.mpi_logsumexp(a[_mpi_rank], b=b[_mpi_rank])
+    y1 = jax.scipy.special.logsumexp(a)
+    y2, _ = mpi.mpi_logsumexp(a[_mpi_rank])
+    print(_mpi_rank, "y1", y1.shape, y1)
+    print(_mpi_rank, "y2", y2.shape, y2)
     np.testing.assert_allclose(y1, y2)
