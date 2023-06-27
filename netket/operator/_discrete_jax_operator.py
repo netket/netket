010c8291349d68891463d56e5fa80b31604ee64c
Filippo Vicentini <filippovicentini@gmail.com>
2023-06-27 13:06:31 +0200

    add a mock base jax operator class
    fix

---
+++ netket/operator/_discrete_jax_operator.py
@@ -0,0 +1,114 @@
+# Copyright 2021-2023 The NetKet Authors - All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import abc
+from typing import Tuple
+import numpy as np
+
+from scipy.sparse import csr_matrix as _csr_matrix
+
+from netket.operator import DiscreteOperator
+
+
+class DiscreteJaxOperator(DiscreteOperator):
+    r"""This class is the base class for operators defined on a
+    discrete Hilbert space. Users interested in implementing new
+    quantum Operators for discrete Hilbert spaces should derive
+    their own class from this class
+    """
+
+    @property
+    def max_conn_size(self) -> int:
+        """The maximum number of non zero ⟨x|O|x'⟩ for every x."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_conn_padded(self, x: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        r"""Finds the connected elements of the Operator.
+
+        Starting from a batch of quantum numbers :math:`x={x_1, ... x_n}` of
+        size :math:`B \times M` where :math:`B` size of the batch and :math:`M`
+        size of the hilbert space, finds all states :math:`y_i^1, ..., y_i^K`
+        connected to every :math:`x_i`.
+
+        Returns a matrix of size :math:`B \times K_{max} \times M` where
+        :math:`K_{max}` is the maximum number of connections for every
+        :math:`y_i`.
+
+        Args:
+            x : A N-tensor of shape :math:`(...,hilbert.size)` containing
+                the batch/batches of quantum numbers :math:`x`.
+        Returns:
+            **(x_primes, mels)**: The connected states x', in a N+1-tensor and an
+            N-tensor containing the matrix elements :math:`O(x,x')`
+            associated to each x' for every batch.
+        """
+
+    def get_conn_flattened(
+        self, x: np.ndarray, sections: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        r"""Finds the connected elements of the Operator.
+
+        Starting from a given quantum number :math:`x`, it finds all
+        other quantum numbers  :math:`x'` such that the matrix element
+        :math:`O(x,x')` is different from zero. In general there will be
+        several different connected states :math:`x'` satisfying this
+        condition, and they are denoted here :math:`x'(k)`, for
+        :math:`k=0,1...N_{\mathrm{connected}}`.
+
+        This is a batched version, where x is a matrix of shape
+        :code:`(batch_size,hilbert.size)`.
+
+        Args:
+            x: A matrix of shape `(batch_size, hilbert.size)`
+                containing the batch of quantum numbers x.
+            sections: An array of sections for the flattened x'.
+                See numpy.split for the meaning of sections.
+
+        Returns:
+            (matrix, array): The connected states x', flattened together in
+                a single matrix.
+                An array containing the matrix elements :math:`O(x,x')`
+                associated to each x'.
+
+        """
+        xp, mels = self.get_conn_padded(x)
+        n_conns = mels.shape[1]
+        xp = xp.reshape(-1, xp.shape[-1])
+        mels = mels.reshape(-1)
+        sections[:] = n_conns
+        return xp, mels
+
+    def n_conn(self, x, out=None) -> np.ndarray:
+        r"""Return the number of states connected to x.
+
+        Args:
+            x (matrix): A matrix of shape (batch_size,hilbert.size) containing
+                        the batch of quantum numbers x.
+            out (array): If None an output array is allocated.
+
+        Returns:
+            array: The number of connected states x' for each x[i].
+
+        """
+        if out is None:
+            out = np.full(x.shape[0], self.max_conn_size, dtype=np.int32)
+
+        return out
+
+    def to_sparse(self) -> _csr_matrix:
+        raise NotImplementedError
+
+    def to_dense(self) -> np.ndarray:
+        return self.to_sparse().todense().A
