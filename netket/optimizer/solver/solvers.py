90819fbd18966d27e026f5dff8eba299ac43ee16
Filippo Vicentini <filippovicentini@gmail.com>
2023-07-09 12:48:11 +0200

    more details

--- netket/optimizer/solver/solvers.py
+++ netket/optimizer/solver/solvers.py
@@ -40,7 +40,10 @@ def pinv_smooth(A, b, rcond=1e-14, rcond_smooth=1e-14, x0=None):
 
         In general, we found that this custom implementation of
         the pseudo-inverse outperform
-        jax's :func:`~jax.numpy.linalg.pinv`.
+        jax's :func:`~jax.numpy.linalg.pinv`. This might be
+        because :func:`~jax.numpy.linalg.pinv` internally calls
+        :obj:`~jax.numpy.linalg.svd`, while this solver internally
+        uses :obj:`~jax.numpy.linalg.eigh`.
 
         For that reason, we suggest you use this solver instead of
         :obj:`~netket.optimizer.solver.pinv`.
@@ -82,6 +85,10 @@ def pinv(A, b, rcond=1e-12, x0=None):
     Solve the linear system using jax's implementation of the
     pseudo-inverse.
 
+    Internally it calls :ref:`~jax.numpy.linalg.pinv` which
+    uses a :ref:`~jax.numpy.linalg.svd` decomposition with
+    the same value of **rcond**.
+
     .. note::
 
         In general, we found that our custom implementation of
@@ -98,8 +105,6 @@ def pinv(A, b, rcond=1e-12, x0=None):
     The diagonal shift on the matrix can be 0 and the
     **rcond** variable can be used to truncate small
     eigenvalues.
-
-    Internally uses :obj:`jax.numpy.linalg.pinv`.
 
     Args:
         A: the matrix A in Ax=b
