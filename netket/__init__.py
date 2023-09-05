b78c70721ec4b0349fd22bdb825609ad07bb0a39
Filippo Vicentini <filippovicentini@gmail.com>
2023-09-05 10:30:09 +0200

    set flags later

--- netket/__init__.py
+++ netket/__init__.py
@@ -16,14 +16,6 @@ from ._version import version as __version__  # noqa: F401
 
 from .utils import config
 
-if config.netket_enable_x64:
-    # enable x64 on jax by default
-    # must be done at startup.
-    from jax.config import config as jax_config
-
-    jax_config.update("jax_enable_x64", True)
-    del jax_config
-
 from . import utils
 from . import errors
 
