1b318fd2f96dcd163f94a572dd7ba5e71d3b502e
Filippo Vicentini <filippovicentini@gmail.com>
2023-09-26 19:45:14 +0200

    remove old tensorboard thingy

--- netket/logging/__init__.py
+++ netket/logging/__init__.py
@@ -17,8 +17,6 @@ from .json_log import JsonLog
 from .state_log import StateLog
 from .tensorboard import TensorBoardLog
 
-from netket.utils import tensorboard_available as _tensorboard_available
-
 
 from netket.utils import _hide_submodules
 
