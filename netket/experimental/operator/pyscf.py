8b7459cc0f990f2d4344251b2e3bc323a4c0c99c
Filippo Vicentini <filippovicentini@gmail.com>
2023-11-06 00:06:12 +0100

    fix doctests of pyscf

--- netket/experimental/operator/pyscf.py
+++ netket/experimental/operator/pyscf.py
@@ -311,8 +311,8 @@ def from_pyscf_molecule(
         >>> geometry = [('Li', (0., 0., -bond_length/2)), ('H', (0., 0., bond_length/2))]
         >>> mol = gto.M(atom=geometry, basis='STO-3G')
         >>>
-        >>> mf = scf.RHF(mol).run()
-            converged SCF energy = -7.86338212728528
+        >>> mf = scf.RHF(mol).run()  # doctest:+ELLIPSIS
+            converged SCF energy = -7.86338...
         >>> E_hf = sum(mf.scf_summary.values())
         >>>
         >>> E_fci = fci.FCI(mf).kernel()[0]
@@ -335,8 +335,8 @@ def from_pyscf_molecule(
         >>> mol = gto.M(atom=geometry, basis='STO-3G')
         >>>
         >>> # compute the boys orbitals
-        >>> mf = scf.RHF(mol).run()
-            converged SCF energy = -7.86338212728528
+        >>> mf = scf.RHF(mol).run()  # doctest:+ELLIPSIS
+            converged SCF energy = -7.86338...
         >>> mo_coeff = lo.Boys(mol).kernel(mf.mo_coeff)
         >>> # use the boys orbitals to construct the netket hamiltonian
         >>> ha = nkx.operator.from_pyscf_molecule(mol, mo_coeff=mo_coeff)
