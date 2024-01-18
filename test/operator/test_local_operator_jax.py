748751f44e846885665ee339eec378615fc793e4
alleSini99 <alessandro.sinibaldi@epfl.ch>
2024-01-18 18:29:40 +0100

    Add test

---
+++ test/operator/test_local_operator_jax.py
@@ -0,0 +1,21 @@
+import jax.numpy as jnp
+
+from netket.operator import LocalOperatorJax
+from netket.operator import IsingJax
+from netket.hilbert import Spin
+from netket.graph import Chain
+
+import pytest
+
+
+def test_casting():
+    hi = Spin(s=0.5, N=8)
+    g = Chain(length=8)
+    H = IsingJax(hi, g, h=1.0)
+
+    a = 42
+    b = jnp.pi
+
+    H = b * (H + a)
+
+    assert isinstance(H, LocalOperatorJax)
