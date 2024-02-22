b014b14b272ca7f26a7aada2b17e058fc6e3d355
Filippo Vicentini <filippovicentini@gmail.com>
2024-02-22 21:43:42 +0100

    fixup

--- netket/utils/static_range.py
+++ netket/utils/static_range.py
@@ -15,8 +15,10 @@
 from numbers import Number
 from functools import partial
 
-import jax
 import numpy as np
+
+import jax
+import jax.numpy as jnp
 
 from netket.utils import struct
 from netket.utils.types import DType
