8f2f55f6f6102b1f6e56332eaecb6dbf6a2c6d71
Filippo Vicentini <filippovicentini@gmail.com>
2024-02-22 19:49:57 +0100

    fix tests of CustomHilbert

--- test/hilbert/test_hilbert.py
+++ test/hilbert/test_hilbert.py
@@ -29,6 +29,7 @@ from netket.hilbert import (
     Spin,
 )
 import netket.experimental as nkx
+from netket.utils import StaticRange
 
 import jax
 import jax.numpy as jnp
@@ -69,7 +70,7 @@ hilberts["Fock * Fock (non-indexable)"] = Fock(n_max=4, N=40) * Fock(n_max=7, N=
 hilberts["Qubit"] = nk.hilbert.Qubit(100)
 
 # Custom Hilbert
-hilberts["Custom Hilbert"] = CustomHilbert(local_states=[-1232, 132, 0], N=70)
+hilberts["Custom Hilbert"] = CustomHilbert(local_states=StaticRange(-153, 44, 3), N=70)
 
 # Heisenberg 1d
 hilberts["Heisenberg 1d"] = Spin(s=0.5, total_sz=0.0, N=20)
@@ -94,7 +95,9 @@ hilberts["Fock Small"] = Fock(n_max=3, N=5)
 hilberts["Qubit Small"] = Qubit(N=1)
 
 # Custom Hilbert
-hilberts["Custom Hilbert Small"] = CustomHilbert(local_states=[-1232, 132, 0], N=5)
+hilberts["Custom Hilbert Small"] = CustomHilbert(
+    local_states=StaticRange(-123, 10, 3), N=5
+)
 
 # Custom Hilbert
 hilberts["DoubledHilbert[Spin]"] = DoubledHilbert(Spin(0.5, N=5))
@@ -106,7 +109,7 @@ hilberts["DoubledHilbert[Spin(total_sz=0.5)]"] = DoubledHilbert(
 hilberts["DoubledHilbert[Fock]"] = DoubledHilbert(Spin(0.5, N=5))
 
 hilberts["DoubledHilbert[CustomHilbert]"] = DoubledHilbert(
-    CustomHilbert(local_states=[-1232, 132, 0], N=5)
+    CustomHilbert(local_states=StaticRange(-123, 10, 3), N=5)
 )
 
 # hilberts["Tensor: Spin x Fock"] = Spin(s=0.5, N=4) * Fock(4, N=2)
