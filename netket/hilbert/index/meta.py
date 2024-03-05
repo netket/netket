bcd93d5d1712af9921b6a2b467e75a1046842a80
Clemens Giuliani <clemens@inailuig.it>
2024-03-05 14:03:15 +0100

    use nk dataclass to generate UniformTensorProductHilbertIndex init

--- netket/hilbert/index/meta.py
+++ netket/hilbert/index/meta.py
@@ -21,30 +21,23 @@ from netket.utils.types import Array
 from .base import HilbertIndex, is_indexable
 
 
+@struct.dataclass
 class UniformTensorProductHilbertIndex(HilbertIndex):
     # tensor product with uniform local space
-    _local_index: HilbertIndex
-    _size: int = struct.field(pytree_node=False)
-
-    def __init__(self, local_index, size):
-        self._local_index = local_index
-        self._size = size
-
-    @property
-    def size(self) -> int:
-        return self._size
+    local_index: HilbertIndex
+    size: int = struct.field(pytree_node=False)
 
     @property
     def local_size(self) -> int:
-        return self._local_index.n_states
+        return self.local_index.n_states
 
     @property
     def n_states(self) -> int:
-        return self.local_size**self._size
+        return self.local_size**self.size
 
     @property
     def local_states(self) -> Array:
-        return self._local_index.all_states()
+        return self.local_index.all_states()
 
     @property
     def _basis(self) -> Array:
@@ -52,13 +45,13 @@ class UniformTensorProductHilbertIndex(HilbertIndex):
 
     @jax.jit
     def states_to_numbers(self, states: Array) -> Array:
-        local_numbers = self._local_index.states_to_numbers(states, dtype=jnp.int32)
+        local_numbers = self.local_index.states_to_numbers(states, dtype=jnp.int32)
         return local_numbers @ self._basis
 
     @jax.jit
     def numbers_to_states(self, numbers: Array) -> Array:
         local_numbers = (numbers[..., None] // self._basis) % self.local_size
-        return self._local_index.numbers_to_states(local_numbers)
+        return self.local_index.numbers_to_states(local_numbers)
 
     @jax.jit
     def all_states(self) -> Array:
