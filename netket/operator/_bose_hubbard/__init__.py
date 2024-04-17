213064f52cf483b5f75dd31779f8d49f22d28254
Z-Denis <zakari.denis@gmail.com>
2024-04-17 17:32:25 +0200

    Fix imports.

--- netket/operator/_bose_hubbard/__init__.py
+++ netket/operator/_bose_hubbard/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .base import IsingBase
-from .numba import Ising
-from .jax import IsingJax
+from .base import BoseHubbardBase
+from .numba import BoseHubbard
+from .jax import BoseHubbardJax
