938c0899bf23cbb7c621f67828e97e1504a78024
Z-Denis <zakari.denis@gmail.com>
2024-04-18 18:40:57 +0200

    Rewrite kernel for arbitrary batch dim.

--- netket/operator/_bose_hubbard/jax.py
+++ netket/operator/_bose_hubbard/jax.py
@@ -103,37 +103,39 @@ class BoseHubbardJax(BoseHubbardBase, DiscreteJaxOperator):
 def _bh_kernel_jax(x, edges, U, V, J, mu, n_max):
     i = edges[:, 0]
     j = edges[:, 1]
-    n_i = x[:, i]
-    n_j = x[:, j]
+    n_i = jnp.vectorize(lambda x: x[i], signature="(n)->(m)")(x)
+    n_j = jnp.vectorize(lambda x: x[j], signature="(n)->(m)")(x)
 
     Uh = 0.5 * U
 
-    x_prime0 = x[:, None]
+    _x = jnp.expand_dims(x, axis=-2)
+    x_prime0 = _x
     mels0 = 0
     mels0 -= mu * x.sum(axis=-1, keepdims=True)
     mels0 += Uh * (x * (x - 1)).sum(axis=-1, keepdims=True)
     mels0 += V * (n_i * n_j).sum(axis=-1, keepdims=True)
-    mask0 = jnp.full((x.shape[0], 1), True)
+    mask0 = jnp.full((*x.shape[:-1], 1), True)
 
     add_at = jax.vmap(
-        lambda x, idx, addend: x.at[:, idx].add(addend), (-2, 0, None), -2
+        lambda x, idx, addend: x.at[..., idx].add(addend), (-2, 0, None), -2
     )
 
     # destroy on i create on j
     mask1 = (n_i > 0) & (n_j < n_max)
     mels1 = mask1 * (-J * jnp.sqrt(n_i) * jnp.sqrt(n_j + 1))
-    x_prime1 = x[:, None] * mask1[..., None]
+    x_prime1 = jnp.repeat(_x, mask1.shape[-1], axis=-2)
     x_prime1 = add_at(x_prime1, i, -1)
     x_prime1 = add_at(x_prime1, j, +1)
 
     # destroy on j create on i
     mask2 = (n_j > 0) & (n_i < n_max)
     mels2 = mask2 * (-J * jnp.sqrt(n_j) * jnp.sqrt(n_i + 1))
-    x_prime2 = x[:, None] * mask2[..., None]
+    x_prime2 = jnp.repeat(_x, mask2.shape[-1], axis=-2)
     x_prime2 = add_at(x_prime2, j, -1)
     x_prime2 = add_at(x_prime2, i, +1)
 
     mask_all = jnp.concatenate([mask0, mask1, mask2], axis=-1)
     mels_all = jnp.concatenate([mels0, mels1, mels2], axis=-1)
     xp_all = jnp.concatenate([x_prime0, x_prime1, x_prime2], axis=-2)
-    return xp_all * mask_all[..., None], mels_all * mask_all
+
+    return xp_all, mels_all * mask_all
