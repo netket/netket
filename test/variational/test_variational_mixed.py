9530995fbf62e50c048aea61db7b5b3af8f5f656
Filippo Vicentini <filippovicentini@gmail.com>
2024-06-02 23:42:32 +0200

    fix last test

--- test/variational/test_variational_mixed.py
+++ test/variational/test_variational_mixed.py
@@ -195,11 +195,7 @@ def test_n_samples_diag_api(vstate, _device_count):
     check_consistent_diag(vstate)
 
     vstate.n_discard_per_chain_diag = None
-    assert (
-        vstate.n_discard_per_chain_diag
-        == vstate.diagonal.n_discard_per_chain
-        == vstate.n_samples_diag // 10
-    )
+    assert vstate.n_discard_per_chain_diag == vstate.diagonal.n_discard_per_chain == 5
 
     vstate.n_samples_diag = 3
     check_consistent_diag(vstate)
