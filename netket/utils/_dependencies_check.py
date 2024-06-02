cf012d124947c6b458ca7b8d467751d90d1b2de6
Filippo Vicentini <filippovicentini@gmail.com>
2024-06-02 18:20:54 +0200

    unvendor plum-dispatch, and depend again from pypi version

--- netket/utils/_dependencies_check.py
+++ netket/utils/_dependencies_check.py
@@ -72,10 +72,9 @@ if not module_version("flax") >= (0, 6, 5):  # pragma: no cover
                """
     raise ImportError(create_msg("flax", cur_version, "0.6.5", extra))
 
-# TODO: Uncomment and bump version once we unvendor plum.
-# if not module_version("plum") >= (2, 2, 2):  # pragma: no cover
-#     raise ImportError(
-#         create_msg(
-#             "plum", version_string("plum"), "2.2.2", pip_pkg_name="plum-dispatch"
-#         )
-#     )
+if not module_version("plum") >= (2, 4, 0):  # pragma: no cover
+    raise ImportError(
+        create_msg(
+            "plum", version_string("plum"), "2.4.0", pip_pkg_name="plum-dispatch"
+        )
+    )
