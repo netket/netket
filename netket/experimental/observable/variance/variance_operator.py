02b86c08295457dc010a81e68c89c3838a025efa
alleSini99 <alessandro.sinibaldi@epfl.ch>
2024-06-17 17:45:27 +0200

    docs

--- netket/experimental/observable/variance/variance_operator.py
+++ netket/experimental/observable/variance/variance_operator.py
@@ -15,9 +15,43 @@
 import netket as nk
 from netket.operator import AbstractOperator
 
+from netket.operator._abstract_observable import AbstractObservable
 
-class VarianceOperator(AbstractOperator):
-    def __init__(self, op, use_Oloc2=False):
+
+class VarianceOperator(AbstractObservable):
+    r"""
+    Observable corresponding to the variance of a an arbitrary quantum operator O:
+
+    .. math::
+
+        \text{Var} = \frac{\langle \Psi | O^2 | \Psi \rangle}{\langle \Psi | \Psi \rangle} - \bigg( \frac{\langle \Psi | O | \Psi \rangle}{\langle \Psi | \Psi \rangle}\bigg)^2
+
+    It can compute the variance using either the estimator of the squared operator :math:`O^2` (more precise but less efficient, since it requires
+    the connected configurations and the matrix elements of :math:`O^2`):
+
+    .. math::
+
+        \text{Var} = \mathbb{E}_{\sigma \sim |\Psi(\sigma)|^2}\bigg[\frac{\langle \sigma | O^2 | \Psi \rangle}{\langle \sigma | \Psi \rangle}\bigg] - \bigg(\mathbb{E}_{\sigma \sim |\Psi(\sigma)|^2}\bigg[\frac{\langle \sigma | O | \Psi \rangle}{\langle \sigma | \Psi \rangle}\bigg]\bigg)^2
+
+    or using the square modulus of the estimator of O (more noisy but more efficient, since it involves only :math:`O`):
+
+    .. math::
+
+        \text{Var} = \mathbb{E}_{\sigma \sim |\Psi(\sigma)|^2}\bigg[\bigg(\frac{\langle \sigma | O | \Psi \rangle}{\langle \sigma | \Psi \rangle} - \mathbb{E}_{\sigma \sim |\Psi(\sigma)|^2}\bigg[\frac{\langle \sigma | O | \Psi \rangle}{\langle \sigma | \Psi \rangle}\bigg]\bigg)^2\bigg]
+
+    """
+
+    def __init__(self, op: AbstractOperator, use_Oloc2: bool=False):
+        """
+        Constructs the VariationalOperator wrapping an operator such that the gradient will not be computed
+        with respect to the expectation value, but with respect to the variance.
+
+        Args:
+            op: The operator for which the variance is to be computed.
+            use_Oloc2: If True, uses the local estimator of the squared operator `O^2` for variance computation.
+                        If False, uses only the operator `O` for variance computation (defaults to False).
+
+        """
         super().__init__(op.hilbert)
         self._op = op
 
@@ -27,21 +61,18 @@ class VarianceOperator(AbstractOperator):
             self._op2 = op @ op
 
     @property
-    def op(self):
+    def op(self) -> AbstractOperator:
+        """
+        The opeator for which the variance is to be computed.
+        """
         return self._op
 
     @property
-    def op2(self):
+    def op2(self)-> AbstractOperator:
+        """
+        The squared of the opeator for which the variance is to be computed.
+        """
         return self._op2
-
-    @property
-    def dtype(self):
-        return float
-
-    def __eq__(self, o):
-        if isinstance(o, VarianceOperator):
-            return o.op == self.op and o.op2 == self.op2
-        return False
 
     def __repr__(self):
         return f"VarianceOperator(op={self.op})"
