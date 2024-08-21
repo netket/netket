d546c7db7e3d4af137405926d9fad62ca3c19140
Filippo Vicentini <filippovicentini@gmail.com>
2024-08-21 14:21:09 +0200

    fixes for CI

--- test_sharding/test_sharding_distributed.py
+++ test_sharding/test_sharding_distributed.py
@@ -3,4 +3,4 @@ import jax
 jax.config.update("jax_cpu_enable_gloo_collectives", True)
 jax.distributed.initialize()
 
-from test_sharding import *  # noqa
+from test.sharding.test_sharding import *  # noqa
