b75045d844dbeca013f10c26d03ea5b07b9d4fc7
Filippo Vicentini <filippovicentini@gmail.com>
2024-08-21 19:39:30 +0200

    fix

--- test_sharding/run_test_sharding_distributed.sh
+++ test_sharding/run_test_sharding_distributed.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export NETKET_EXPERIMENTAL_SHARDING=1
 export NETKET_MPI_WARNING=0 
-mpirun -np 2 python3 -m pytest --jax-distributed-gloo --cov=netket --cov-append -p no:warnings --color=yes --verbose -n 0 --tb=short $@ test/sharding/test_sharding.py
+mpirun -np 2 python3 -m coverage run -m pytest --jax-distributed-gloo -p no:warnings --color=yes --verbose -n 0 --tb=short $@ test/sharding/test_sharding.py
