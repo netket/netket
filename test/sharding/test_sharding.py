cf48202970a4b3be57055e8ccbf2ecfc28fc7579
Clemens Giuliani <clemens@inailuig.it>
2024-09-17 15:37:00 +0200

    black fmt

--- test/sharding/test_sharding.py
+++ test/sharding/test_sharding.py
@@ -128,7 +128,6 @@ def test_grad():
     not nk.config.netket_experimental_sharding, reason="Only run with sharding"
 )
 def test_vmc(Op, qgt, chunk_size, reset_chains):
-
     vs, g, _ = _setup(16, reset_chains=reset_chains)
     vs.chunk_size = chunk_size
     # initially the params are only on the first device of each process
