6306c0ad9674f60a168e789ad0c67374b4d8fb90
Detlef <ds2@physik.de>
2025-01-06 19:26:00 +0100

    first fixes for constraint operators

--- netket/operator/_discrete_operator.py
+++ netket/operator/_discrete_operator.py
@@ -208,11 +208,32 @@ class DiscreteOperator(AbstractOperator):
         sections = np.empty(x.shape[0], dtype=np.int32)
         x_prime, mels = concrete_op.get_conn_flattened(x, sections)
 
-        numbers = hilb.states_to_numbers(x_prime)
-
         sections1 = np.empty(sections.size + 1, dtype=np.int32)
         sections1[1:] = sections
         sections1[0] = 0
+        # This part removes x_primes, which have mels sum 0
+        # This is usefull, if inbetween states are not in the constraint hilbert space, as in this case
+        # states_to_numbers will fail
+        unique_x_prime, indices = np.unique(x_prime, axis=0, return_index=True)
+        sum_mels = np.zeros(indices.size, dtype=np.complex128)
+        for i in range(unique_x_prime.shape[0]):
+            for j in range(x_prime.shape[0]):
+                if (x_prime[j] == unique_x_prime[i]).all():
+                    sum_mels[i] += mels[j]
+
+        sections2 = sections1.copy()
+        removed_items = 0
+        position = 0
+        for i in range(1, sections2.size):
+            if sections2[i] - sections2[i-1] > 0:
+                if sum_mels[position] ==0:
+                    mels = np.delete(mels, slice(sections2[i-1]-removed_items, sections2[i]-removed_items))
+                    x_prime = np.delete(x_prime, slice(sections2[i-1]-removed_items, sections2[i]-removed_items), axis=0)
+                    removed_items += sections2[i] - sections2[i-1]
+                position += 1
+            sections1[i]-=removed_items
+
+        numbers = hilb.states_to_numbers(x_prime)
 
         ## eliminate duplicates from numbers
         # rows_indices = compute_row_indices(hilb.states_to_numbers(x), sections1)
