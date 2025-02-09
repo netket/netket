5a7a93e0eaca9c81544959ba85c86b96736da531
detlef <dschmicker@physik.de>
2025-02-09 15:45:15 +0100

    Revert all changes on branch

--- netket/operator/_fermion2nd/numba.py
+++ netket/operator/_fermion2nd/numba.py
@@ -130,7 +130,6 @@ class FermionOperator2nd(FermionOperator2ndBase):
             self._term_split_idxs,
             self._cutoff,
             pad,
-            self.hilbert.constraint is not None,
         )
 
     @staticmethod
@@ -147,15 +146,7 @@ class FermionOperator2nd(FermionOperator2ndBase):
         term_split_idxs,
         cutoff,
         pad=False,
-        constraint=False,
     ):
-        def hash_raw(x):
-            """Numba can not hash arrays, so we hash them manually"""
-            r = np.uint64(0)
-            for u in x:
-                r = (r * np.uint64(7) + np.uint64(u)) % np.uint64(576460752303423619)  # NextPrime[2^59]  (numba seems to be picky with the types)
-            return r
-        
         x_prime = np.empty((x.shape[0] * max_conn, x.shape[1]), dtype=x.dtype)
         mels = np.zeros((x.shape[0] * max_conn), dtype=weights.dtype)
 
@@ -164,11 +155,6 @@ class FermionOperator2nd(FermionOperator2ndBase):
         orb_idxs_list = np.split(orb_idxs, term_split_idxs)
         daggers_list = np.split(daggers, term_split_idxs)
 
-        
-        if constraint:
-            x_set = set() # set of all states in the hilbert space for quick lookup
-            for raw in x:
-                x_set.add(hash_raw(raw))
         # loop over the batch dimension
         n_c = 0
         for b in range(x.shape[0]):
@@ -193,7 +179,7 @@ class FermionOperator2nd(FermionOperator2ndBase):
                     if not op_has_xp:
                         has_xp = False
                         continue
-                if has_xp and (not constraint or hash_raw(xb) in x_set): # check if in the hilbert space
+                if has_xp:
                     x_prime[n_c, :] = np.copy(xb)  # should be untouched
                     mels[n_c] += mel
 
@@ -217,7 +203,7 @@ class FermionOperator2nd(FermionOperator2ndBase):
                     if not op_has_xp:  # detect zeros
                         has_xp = False
                         continue
-                if has_xp and (not constraint or hash_raw(xt) in x_set):  # check if in the hilbert space
+                if has_xp:
                     x_prime[n_c, :] = np.copy(xt)  # should be different
                     mels[n_c] += mel
                     n_c += 1
