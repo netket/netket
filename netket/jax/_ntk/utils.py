6710d13d3f4c591872ad71562a12ae823740e924
Filippo Vicentini <filippovicentini@gmail.com>
2025-05-15 15:42:07 +0200

    Remove a lot of useless stuff from ntk

--- netket/jax/_ntk/utils.py
+++ netket/jax/_ntk/utils.py
@@ -45,15 +45,6 @@ def is_list_or_tuple(x) -> bool:
   # otherwise this will return true for namedtuples.
   return type(x) == list or type(x) == tuple
 
-
-def is_nt_tree_of(x, dtype: Union[type, tuple[type, ...]]) -> bool:
-  if isinstance(x, dtype):
-    return True
-  if not is_list_or_tuple(x):
-    return False
-  return all(is_nt_tree_of(_x, dtype) for _x in x)
-
-
 def nt_tree_fn(
     nargs: Optional[int] = None,
     tree_structure_argnum: Optional[int] = None,
@@ -136,53 +127,6 @@ def all_none(x, attr: Optional[str] = None) -> bool:
   return tree_all(tree_map(lambda x: get_fn(x) is None, x))
 
 
-def canonicalize_get(get):
-  if get is None:
-    return True, get
-
-  if not get:
-    # NOTE(schsam): It seems slightly nicer to not support the empty-tuple
-    # case. Happy to add support later, if there's a use-case.
-    raise ValueError('"get" must be non-empty.')
-
-  get_is_not_tuple = isinstance(get, str)
-  if get_is_not_tuple:
-    get = (get,)
-
-  get = tuple(s.lower() for s in get)
-  if len(set(get)) < len(get):
-    raise ValueError('All entries in "get" must be unique. Got {}'.format(get))
-  return get_is_not_tuple, get
-
-
-_KERNEL_NAMED_TUPLE_CACHE: dict[Any, Any] = {}
-
-
-def _named_tuple_factory(name, get):
-  key = (name, get)
-  if key in _KERNEL_NAMED_TUPLE_CACHE:
-    return _KERNEL_NAMED_TUPLE_CACHE[key]
-  else:
-    _KERNEL_NAMED_TUPLE_CACHE[key] = namedtuple(name, get)
-    return _named_tuple_factory(name, get)
-
-
-def _output_to_dict(output):
-  if isinstance(output, dict):
-    return output
-
-  if hasattr(output, 'asdict'):
-    return output.asdict()
-
-  if hasattr(output, '_asdict'):
-    return output._asdict()
-
-  if isinstance(output, types.GeneratorType):
-    return (_output_to_dict(out) for out in output)
-
-  raise ValueError(type(output))
-
-
 def wraps(f):
   def wrapper(g):
     @functools.wraps(f)
@@ -192,64 +136,6 @@ def wraps(f):
     h.__signature__ = inspect.signature(f)
     return h
   return wrapper
-
-
-def get_namedtuple(name):
-  def getter_decorator(fn):
-    try:
-      argspec = inspect.getfullargspec(fn)
-      get_index = argspec.args.index('get')
-      defaults = argspec.defaults
-    except:
-      raise ValueError('`get_namedtuple` functions must have a `get` argument.')
-
-    @wraps(fn)
-    def getter_fn(*args, **kwargs):
-      canonicalized_args = list(args)
-
-      if 'get' in kwargs:
-        get_is_not_tuple, get = canonicalize_get(kwargs['get'])
-        kwargs['get'] = get
-      elif get_index < len(args):
-        get_is_not_tuple, get = canonicalize_get(args[get_index])
-        canonicalized_args[get_index] = get
-      elif defaults is None:
-        raise ValueError(
-            '`get_namedtuple` function must have a `get` argument provided or '
-            'set by default.')
-      else:
-        get_is_not_tuple, get = canonicalize_get(defaults[get_index -
-                                                          len(args)])
-
-      fn_out = fn(*canonicalized_args, **kwargs)
-
-      @nt_tree_fn()
-      def canonicalize_output(out):
-        if get is None:
-          if isinstance(out, dict):
-            ReturnType = _named_tuple_factory(name, tuple(out.keys()))
-            out = ReturnType(*out.values())
-          return out
-
-        out = _output_to_dict(out)
-
-        if get_is_not_tuple:
-          if isinstance(out, types.GeneratorType):
-            return (output[get[0]] for output in out)
-          else:
-            return out[get[0]]
-
-        ReturnType = _named_tuple_factory(name, get)
-        if isinstance(out, types.GeneratorType):
-          return (ReturnType(*tuple(output[g] for g in get)) for output in out)
-        else:
-          return ReturnType(*tuple(out[g] for g in get))
-
-      return canonicalize_output(fn_out)
-
-    return getter_fn
-
-  return getter_decorator
 
 
 @nt_tree_fn(nargs=2, reduce=lambda x: jnp.all(jnp.array(x)))
@@ -293,14 +179,6 @@ def _get_ndim(x: Union[int, Sized, jnp.ndarray]) -> int:
   else:
     raise TypeError(x, type(x))
   return n
-
-
-def mod(axis: Axes, x: Union[int, Sized, jnp.ndarray]) -> list[int]:
-  """Makes `axis` non-negative given number of dimensions / shape / array."""
-  n = _get_ndim(x)
-  if isinstance(axis, int):
-    axis = [axis]
-  return [(i % n) if n > 0 else i for i in axis]
 
 
 def canonicalize_axis(
@@ -402,57 +280,6 @@ def _zip_axes(
   return x
 
 
-def diagonal_between(
-    x: jnp.ndarray,
-    start_axis: int = 0,
-    end_axis: Optional[int] = None
-) -> jnp.ndarray:
-  """Returns the diagonal along all dimensions between start and end axes."""
-  if end_axis is None:
-    end_axis = x.ndim
-
-  half_ndim, ragged = divmod(end_axis - start_axis, 2)
-  if ragged:
-    raise ValueError(
-        f'Need even number of axes to flatten, got {end_axis - start_axis}.')
-  if half_ndim == 0:
-    return x
-
-  side_shape = x.shape[start_axis:start_axis + half_ndim]
-  side_size = size_at(side_shape)
-
-  shape_2d = x.shape[:start_axis] + (side_size, side_size) + x.shape[end_axis:]
-  shape_result = x.shape[:start_axis] + side_shape + x.shape[end_axis:]
-
-  x = jnp.diagonal(x.reshape(shape_2d), axis1=start_axis, axis2=start_axis + 1)
-  x = jnp.moveaxis(x, -1, start_axis)
-  return x.reshape(shape_result)
-
-
-def zip_flat(x, y):
-  return tuple(c for xy in zip(x, y) for c in xy)
-
-
-def interleave_ones(x, start_axis, end_axis, x_first):
-  x_axes = x.shape[start_axis:end_axis]
-  ones = (1,) * (end_axis - start_axis)
-  shape = x.shape[:start_axis]
-  if x_first:
-    shape += zip_flat(x_axes, ones)
-  else:
-    shape += zip_flat(ones, x_axes)
-  shape += x.shape[end_axis:]
-  return x.reshape(shape)
-
-
-def outer_prod(x, y, start_axis, end_axis, prod_op):
-  if y is None:
-    y = x
-  x = interleave_ones(x, start_axis, end_axis, True)
-  y = interleave_ones(y, start_axis, end_axis, False)
-  return prod_op(x, y)
-
-
 _ArrayOrShape = TypeVar(
     '_ArrayOrShape',
     np.ndarray,
@@ -460,33 +287,6 @@ _ArrayOrShape = TypeVar(
     list[int],
     tuple[int, ...],
 )
-
-
-def reverse_zipped(
-    x: _ArrayOrShape,
-    start_axis: int = 0
-) -> _ArrayOrShape:
-  if x is not None:
-    ndim = _get_ndim(x)
-    source_axes = tuple(j
-                        for i in range(ndim - 2, start_axis - 1, -2)
-                        for j in (i, i + 1))
-
-    if isinstance(x, (np.ndarray, jnp.ndarray)):
-      target_axes = range(start_axis, ndim)
-      x = jnp.moveaxis(x, source_axes, target_axes)
-    else:
-      x = x[:start_axis] + type(x)(x[i] for i in source_axes)
-  return x
-
-
-def mask(
-    x: Optional[jnp.ndarray],
-    mask_mat: Optional[jnp.ndarray]
-) -> Optional[jnp.ndarray]:
-  if x is None or mask_mat is None:
-    return x
-  return jnp.where(mask_mat, jnp.zeros((), x.dtype), x)
 
 
 def size_at(
@@ -500,57 +300,6 @@ def size_at(
     axes = range(len(x))
 
   return functools.reduce(operator.mul, [x[a] for a in axes], 1)
-
-
-def axis_after_dot(
-    axis: int,
-    contracting_dims: Sequence[int],
-    batch_dims: Sequence[int],
-    lhs_ndim: Optional[int] = None
-) -> int:
-  if axis in batch_dims:
-    return batch_dims.index(axis)
-
-  return (
-      axis -
-      sum(1 for i in contracting_dims if i < axis) +
-      sum(1 for i in batch_dims if i > axis) +
-      (0 if lhs_ndim is None
-       else lhs_ndim - len(batch_dims) - len(contracting_dims))
-  )
-
-
-def make_2d(
-    x: Optional[jnp.ndarray],
-    start_axis: int = 0,
-    end_axis: Optional[int] = None
-) -> Optional[jnp.ndarray]:
-  """Makes `x` 2D from `start_axis` to `end_axis`, preserving other axes.
-
-  `x` is assumed to follow the (`X, X, Y, Y, Z, Z`) axes layout.
-
-  Example:
-    >>> x = np.ones((1, 2, 3, 3, 4, 4))
-    >>> make_2d(x).shape == (12, 24)
-    >>> #
-    >>> make_2d(x, 2).shape == (1, 2, 12, 12)
-    >>> #
-    >>> make_2d(x, 2, 4).shape == (1, 2, 3, 3, 4, 4)
-  """
-  if x is None:
-    return x
-
-  if end_axis is None:
-    end_axis = x.ndim
-
-  x = unzip_axes(x, start_axis, end_axis)
-
-  half_ndim = (end_axis - start_axis) // 2
-  x = x.reshape(x.shape[:start_axis] +
-                (size_at(x.shape[start_axis:start_axis + half_ndim]),
-                 size_at(x.shape[start_axis + half_ndim:end_axis])) +
-                x.shape[end_axis:])
-  return x
 
 
 def _read_keys(key, x1, x2):
@@ -604,67 +353,3 @@ def split_kwargs(kwargs, x1=None, x2=None):
 
   return kwargs1, kwargs2
 
-
-_SingleSlice = Union[int, slice, type(Ellipsis)]
-
-
-SliceType = Union[_SingleSlice, tuple[_SingleSlice, ...]]
-"""A type to specify a slice of an array.
-
-For instance, when indexing `x[1, :, 2:8:3]` a slice tuple
-`(1, slice(None), slice(2, 8, 3))` is created. But since slice functions cannot
-accept slice specifications like `1, :, 2:8:3` as arguments, you must either
-pass this object, or, for convenience, an :cls:`~neural_tangents.stax.Slice`
-slice, such as `nt.stax.Slice[1, :, 2:8:3]`.
-"""
-
-
-def canonicalize_idx(
-    idx: SliceType,
-    ndim: int
-) -> tuple[Union[int, slice], ...]:
-  if idx is Ellipsis or isinstance(idx, (int, slice)):
-    idx = (idx,) + (slice(None),) * (ndim - 1)
-
-  for i, s in enumerate(idx):
-    if s is Ellipsis:
-      idx = idx[:i] + (slice(None),) * (ndim - len(idx) + 1) + idx[i + 1:]
-
-  idx += (slice(None),) * (ndim - len(idx))
-  return idx
-
-
-def slice_shape(shape: tuple[int, ...], idx: SliceType) -> tuple[int, ...]:
-  # Keep `None` or negative-sized axes if they aren't indexed into.
-  canonical_idx = canonicalize_idx(idx, len(shape))
-
-  np_shape = list(shape)
-  unknown_axes = {}
-  n_ints = 0  # Keep track of vanishing axes due to integer indexing.
-
-  for a, (i, s) in enumerate(zip(canonical_idx, shape)):
-    if s < 0 or s is None:
-      if i == slice(None):
-        np_shape[a] = 0
-        unknown_axes[a - n_ints] = s
-      else:
-        raise ValueError(
-            f'Trying to index with {i} axis {a} of unknown size {s}. '
-            f'Please provide input shape {shape} with non-negative integer '
-            f'size at axis {a}.')
-
-    if isinstance(i, int):
-      n_ints += 1
-
-  out_shape = list(np.empty(np_shape)[idx].shape)
-  for a, v in unknown_axes.items():
-    out_shape[a] = v
-
-  return tuple(out_shape)
-
-
-_T = TypeVar('_T')
-
-
-def double_tuple(x: Iterable[_T]) -> tuple[_T, ...]:
-  return tuple(v for v in x for _ in range(2))
