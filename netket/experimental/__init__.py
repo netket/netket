8cc71ca7a1c1b04368aeca29d98304af8a3b52d2
Giuseppe Carleo <28149892+gcarleo@users.noreply.github.com>
2025-06-03 22:37:34 +0200

    Move continuous space modules to experimental

--- netket/experimental/__init__.py
+++ netket/experimental/__init__.py
@@ -19,6 +19,7 @@ __all__ = [
     "vqs",
     "TDVP",
     "models",
+    "hilbert",
     "operator",
     "logging",
     "observable",
--- netket/geometry/__init__.py
+++ netket/geometry/__init__.py
@@ -1,19 +1,5 @@
-"""Deprecated geometry classes, moved to :mod:`netket.experimental.geometry`."""
-
-from netket.utils.deprecation import deprecation_getattr
+"""Geometry classes for continuous Hilbert spaces."""
 
 from netket.experimental.geometry import Cell, FreeSpace
 
-_deprecations = {
-    "Cell": (
-        "netket.geometry.Cell is deprecated: use netket.experimental.geometry.Cell",
-        Cell,
-    ),
-    "FreeSpace": (
-        "netket.geometry.FreeSpace is deprecated: use netket.experimental.geometry.FreeSpace",
-        FreeSpace,
-    ),
-}
-
-__getattr__ = deprecation_getattr(__name__, _deprecations)
 __all__ = ["Cell", "FreeSpace"]
