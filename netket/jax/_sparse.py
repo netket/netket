dae426359e8d26024b600be448325baf4f9ab018
Clemens Giuliani <clemens@inailuig.it>
2025-06-03 12:07:47 +0200

    document it

--- netket/jax/_sparse.py
+++ netket/jax/_sparse.py
@@ -9,7 +9,21 @@ from ._sort import searchsorted
 
 @struct.dataclass
 class COOArray:
-    _coords: Array  # needs to be unique and sorted, these are transposed w.r.t sparse
+    """
+    simple sparse coo array written in jax (similar to sparse.COO)
+    supports arbitrary number of dimensions
+    so far only indexing and conversion to/from dense is implemented
+
+    Args:
+        _coords: array containing the indices of the nonzero elements
+        data: array containing the values of the nonzero elements
+        shape: shape of the array
+        fill_value: default value of the nonzero elements (0)
+    """
+
+    _coords: (
+        Array  # needs to be unique and sorted, these are transposed w.r.t sparse.COO
+    )
     data: Array
     shape: tuple = struct.field(pytree_node=False, default=())
     fill_value: Array = 0
@@ -37,7 +51,7 @@ class COOArray:
         return jax.lax.select(mask, res, jnp.full_like(res, self.fill_value))
 
     @property
-    def coords(self):
+    def coords(self) -> Array:
         # for consistency with sparse
         # TODO implement transposed version?
         return self._coords.T
@@ -47,19 +61,19 @@ class COOArray:
         return self.data.dtype
 
     @property
-    def ndim(self):
+    def ndim(self) -> int:
         return len(self.shape)
 
     @property
-    def nnz(self):
+    def nnz(self) -> int:
         return self.data.size
 
-    def todense(self):
+    def todense(self) -> Array:
         A = jnp.full(self.shape, self.fill_value, self.dtype)
         return A.at[tuple(self.coords)].set(self.data)
 
     @classmethod
-    def fromdense(cls, A):
+    def fromdense(cls, A: Array) -> "COOArray":
         # assume that nonzero returns a sorted array
         i = jnp.nonzero(A)
         return cls(jnp.array(i).T, A[i], A.shape)
