d36482753de12d6c447338aaa829dd6630c58f89
Filippo Vicentini <filippovicentini@gmail.com>
2025-07-02 11:49:17 +0200

    [Sharding tools] add new tools
    sharding
    changes
    
    
    f
    
    pu
    
    fix
    
    remove lax
    
    clean

--- netket/jax/__init__.py
+++ netket/jax/__init__.py
@@ -45,7 +45,6 @@ from ._utils_random import (
     batch_choice,
 )
 
-
 from ._vjp import vjp
 from ._grad import grad, value_and_grad
 
@@ -68,4 +67,4 @@ from . import sharding
 
 from netket.utils import _hide_submodules
 
-_hide_submodules(__name__, ignore="sharding")
+_hide_submodules(__name__, ignore=("sharding"))
