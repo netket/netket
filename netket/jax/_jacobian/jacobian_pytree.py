7c4ac35f3b9bf00af24a710be9cf11b68df2ce8c
Clemens Giuliani <clemens@inailuig.it>
2025-07-03 11:10:02 +0200

    WIP take 2: workaround for now

--- netket/jax/_jacobian/jacobian_pytree.py
+++ netket/jax/_jacobian/jacobian_pytree.py
@@ -19,7 +19,6 @@ import jax
 import jax.flatten_util
 import jax.numpy as jnp
 
-import numpy as np
 
 from netket import jax as nkjax
 from netket.utils import wrap_to_support_scalar
@@ -42,7 +41,9 @@ def jacobian_real_holo(forward_fn: Callable, params: PyTree, samples: Array) -> 
     y, vjp_fun = jax.vjp(
         lambda pars: wrap_to_support_scalar(forward_fn)(pars, samples), params
     )
-    (res,) = vjp_fun(np.array(1.0, dtype=jnp.result_type(y)))
+    # TODO use this once jax bug is fixed
+    # (res,) = vjp_fun(jnp.full_like(y, 1))
+    (res,) = vjp_fun(y * 0 + 1)
     return res
 
 
@@ -71,8 +72,12 @@ def _jacobian_cplx(
     if not jnp.issubdtype(jnp.result_type(y), jnp.complexfloating):
         raise TypeError("Cannot build the complex jacobian for a real-valued function.")
 
-    (gr,) = vjp_fun(np.array(1.0, dtype=jnp.result_type(y)))
-    (gi,) = vjp_fun(np.array(-1.0j, dtype=jnp.result_type(y)))
+    # TODO use this once jax bug is fixed
+    # (gr,) = vjp_fun(jnp.full_like(y, 1.0))
+    # (gi,) = vjp_fun(jnp.full_like(y, -1.0j)
+    (gr,) = vjp_fun(y * 0 + 1)
+    (gi,) = vjp_fun(y * 0 - 1j)
+
     return _build_fn(gr, gi)
 
 
