cf9392d068af78736067ca8ae2488805ee8192a4
alleSini99 <alessandro.sinibaldi@epfl.ch>
2025-07-04 12:20:14 +0200

    add test

---
+++ test/operator/test_infidelity.py
@@ -0,0 +1,142 @@
+import netket as nk
+import netket.experimental as nkx
+import numpy as np
+import jax.numpy as jnp
+
+import pytest
+
+from ..variational.finite_diff import central_diff_grad, same_derivatives
+from .. import common
+
+seed = 123
+seed_target = 456
+
+
+def _setup(useExactSampler=True):
+    N = 3
+    hi = nk.hilbert.Spin(0.5, N)
+
+    ma = nk.models.RBM(alpha=1)
+    n_samples = 8192
+
+    if useExactSampler:
+        sa = nk.sampler.ExactSampler(hilbert=hi)
+        vs = nk.vqs.MCState(
+            sampler=sa,
+            model=ma,
+            n_samples=n_samples,
+            seed=seed,
+        )
+        vs_target = nk.vqs.MCState(
+            sampler=sa,
+            model=ma,
+            n_samples=n_samples,
+            seed=seed_target,
+        )
+
+    else:
+        sa = nk.sampler.MetropolisLocal(hilbert=hi, n_chains_per_rank=16)
+        vs = nk.vqs.MCState(
+            sampler=sa,
+            model=ma,
+            n_samples=n_samples,
+            n_discard_per_chain=1e3,
+            seed=seed,
+        )
+        vs_target = nk.vqs.MCState(
+            sampler=sa,
+            model=ma,
+            n_samples=n_samples,
+            seed=seed_target,
+        )
+
+    vs_exact = nk.vqs.FullSumState(
+        hilbert=hi,
+        model=ma,
+        seed=seed,
+    )
+    vs_exact_target = nk.vqs.FullSumState(
+        hilbert=hi,
+        model=ma,
+        seed=seed_target,
+    )
+
+    return vs, vs_target, vs_exact, vs_exact_target
+
+
+def I_exact_fun(params, vs, vs_target):
+    params_old = vs.parameters
+    vs.parameters = params
+    state = vs.to_array()
+    vs.parameters = params_old
+
+    state_target = vs_target.to_array()
+
+    I = 1 - jnp.absolute(state.conj() @ state_target) ** 2
+
+    return I
+
+
+@common.xfailif_mpi  # broken in recent jax versions
+@pytest.mark.parametrize(
+    "useExactSampler",
+    [
+        pytest.param(True, id="ExactSampler"),
+        pytest.param(False, id="MetropolisSampler"),
+    ],
+)
+def test_MCState(useExactSampler):
+    vs, vs_target, vs_exact, vs_exact_target = _setup(useExactSampler)
+    I_op = nkx.operator.InfidelityOperator(target_state=vs_target)
+
+    params, unravel = nk.jax.tree_ravel(vs.parameters)
+
+    I_stats1 = vs.expect(I_op)
+    I_stats, I_grad = vs.expect_and_grad(I_op)
+    I_grad, _ = nk.jax.tree_ravel(I_grad)
+
+    I_exact = I_exact_fun(vs.parameters, vs, vs_target)
+
+    def _I_exact_fun(params, vstate, vstate_target):
+        return I_exact_fun(unravel(params), vstate, vstate_target)
+
+    I_grad_exact = central_diff_grad(_I_exact_fun, params, 1.0e-5, vs, vs_target)
+
+    I_mean1 = I_stats1.mean
+    I_err1 = 5 * I_stats1.error_of_mean
+    I_mean = I_stats.mean
+    I_err = 5 * I_stats.error_of_mean
+
+    np.testing.assert_allclose(I_exact.real, I_mean1.real, atol=I_err1)
+    np.testing.assert_allclose(I_exact.real, I_mean.real, atol=I_err)
+
+    same_derivatives(I_grad, I_grad_exact, abs_eps=I_err, rel_eps=I_err)
+
+
+def test_FullSumState():
+    err = 1e-3
+    vs, vs_target, vs_exact, vs_exact_target = _setup()
+    I_op = nkx.operator.InfidelityOperator(target_state=vs_exact_target)
+
+    params, unravel = nk.jax.tree_ravel(vs_exact.parameters)
+
+    I_stats1 = vs_exact.expect(I_op)
+    I_stats, I_grad = vs_exact.expect_and_grad(I_op)
+    I_grad, _ = nk.jax.tree_ravel(I_grad)
+
+    I_exact = I_exact_fun(vs_exact.parameters, vs_exact, vs_exact_target)
+
+    def _I_exact_fun(params, vstate, vstate_target):
+        return I_exact_fun(unravel(params), vstate, vstate_target)
+
+    I_grad_exact = central_diff_grad(
+        _I_exact_fun, params, 1.0e-5, vs_exact, vs_exact_target
+    )
+
+    I_mean1 = I_stats1.mean
+    I_mean = I_stats.mean
+
+    np.testing.assert_allclose(I_exact.real, I_mean1.real, atol=1e-6)
+    np.testing.assert_allclose(I_exact.real, I_mean.real, atol=1e-6)
+
+    same_derivatives(I_grad, I_grad_exact, abs_eps=err, rel_eps=err)
