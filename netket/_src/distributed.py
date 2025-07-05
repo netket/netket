b5d0947be1bfb21fed21817f0fe1ba5b7d0c078c
Filippo Vicentini <filippovicentini@gmail.com>
2025-07-05 02:26:32 +0200

    WIP on pv/stuff-done-decently

--- netket/_src/distributed.py
+++ netket/_src/distributed.py
@@ -6,8 +6,6 @@ import jax
 import jax.numpy as jnp
 
 from netket import config as nkconfig
-from jax.lax import with_sharding_constraint
-from jax.sharding import PositionalSharding
 
 
 @lru_cache
@@ -74,83 +72,6 @@ def is_master_process() -> bool:
     return process_index() == 0
 
 
-def shard_replicated(array, *, axis=0):
-    """
-    Shards a replicated array across jax processes.
-
-    The input must be a replicated array, obtained either from
-    :func:`netket_pro.distributed.broadcast`, :func:`netket_pro.distributed.allgather` or
-    from executing the same function on all nodes.
-
-    When running under sharding, we set the sharding constraint accordingly.
-
-    Args:
-        array: The array to shard. Must be replicated!
-        axis: The axis along which to shard (Default 0).
-    """
-
-    def _shard(array):
-        lenght = array.shape[axis]
-        if not lenght % process_count() == 0:
-            raise ValueError(
-                "Sharded axis size must be a multiple of the number of processes"
-            )
-
-        if mode() == "sharding":
-            # Do not use process_count() because we could have more than
-            # 1 GPU per process
-
-            sharding_shape = [1 for _ in range(array.ndim)]
-            sharding_shape[axis] = len(jax.devices())
-            sharding = jax.sharding.PositionalSharding(jax.devices()).reshape(
-                sharding_shape
-            )
-            array = jax.lax.with_sharding_constraint(array, sharding)
-        else:
-            pass
-        return array
-
-    return jax.tree_util.tree_map(_shard, array)
-
-
-def allgather(array, *, axis: int = 0, token=None):
-    """
-    Gathers (unshard) a distributed (sharded) array to all processes.
-
-    The resulting array will have the same shape as the input array except
-    the first axis, which will be :ref:`netket_pro.distributed.process_count`
-    times longer.
-
-    .. note::
-
-        An input array of shape :math:`(M, N, ...)` will lead to a gathered
-        array of shape :math:`(P \times M, N, ...)`, where :math:`P` is the
-        number of processes.
-
-    .. note::
-
-        The resulting array will be unsharded, or fully addressable locally
-        and on every process.
-
-    Args:
-        array: The array to gather.
-
-    Returns:
-        A tuple of the gathered array and the token.
-
-    """
-    if axis != 0:
-        raise NotImplementedError("Only axis=0 is supported for now. Open a PR.")
-
-    if mode() == "sharding":
-        sharding = PositionalSharding(jax.devices()).replicate()
-        sharding = sharding.reshape(tuple(1 for _ in range(array.ndim)))
-        array = jax.lax.with_sharding_constraint(array, sharding)
-    else:
-        pass
-    return array, token
-
-
 def pad_axis_for_sharding(
     array: jax.Array, *, axis: int = 0, padding_value: float | jax.Array = 0
 ) -> jax.Array:
@@ -181,88 +102,29 @@ def pad_axis_for_sharding(
     return array
 
 
-def reshard(
-    array: jax.Array,
-    *,
-    sharded_axis: int = 0,
-    out_sharded_axis: int = 1,
-    token=None,
-    pad: bool = False,
-    pad_value: jax.Array = 0.0,
-) -> jax.Array:
-    """
-    Reshards an array to distribute another axis among the processes. Equivalent to
-    :ref:`~mpi4jax.mpi_alltoall` in MPI jargon.
-
-    The input array is assumed to be sharded along axis `sharded_axis`, and the resulting
-    array will be sharded along axis `out_sharded_axis`. The sharded axis will be collected
-    while the output sharded axis will be distributed.
-
-    .. note::
-
-        If the input array has shape :math:`(x, y, z)` and the input sharded axis is `y`,
-        and the output sharded axis is `x`, the resulting array will have shape :math:`(x, y*P, z/P)`.
-
-    Args:
-        array: The array to reshard / alltoall.
-        sharded_axis: The axis to be collected.
-        out_sharded_axis: The axis to be distributed.
-        pad: Whether to pad the axis to be sharded to be a multiple of the number of processes. If this is
-            set to `False`, the size of the sharded axis must be a multiple of the number of processes.
-            (Default: `False`)
-        pad_value: The value to use for padding. (Default: `0.0`)
-
-    """
-    assert sharded_axis != out_sharded_axis
-    assert 0 <= sharded_axis < array.ndim
-    assert 0 <= out_sharded_axis < array.ndim
-
-    # Pad the number of parameters to be a multiple of the number of MPI nodes
-    # -> (#n_nodes, np_padded)
-    if array.shape[out_sharded_axis] % device_count() != 0:
-        if pad:
-            array = pad_axis_for_sharding(
-                array, axis=out_sharded_axis, padding_value=pad_value
-            )
-        else:
-            raise ValueError(
-                "Sharded axis size must be a multiple of the number of processes"
-            )
-
-    if mode() == "sharding":
-        del sharded_axis  # unused
-
-        sharding = PositionalSharding(jax.devices())
-        sharding_shape = list(1 for _ in range(array.ndim))
-        sharding_shape[out_sharded_axis] = -1
-        sharding = sharding.reshape(sharding_shape)
-        array = with_sharding_constraint(array, sharding)
-    return array, token
-
-
-def _inspect_(name: str, x: jax.Array):
-    """
-    Internal function to inspect the sharding of an array. To be used for debugging inside
-    of :func:`jax.jit`-ted functions.
-
-    Args:
-        name: A string to identify the array, usually the name, but can contain anything else.
-        x: The array
-    """
-    if mode() == "sharding":
-
-        def _cb(y):
-            if process_index() == 0:
-                print(
-                    f"{name}: shape={x.shape}, sharding:",
-                    y,
-                    flush=True,
-                )
-
-        jax.debug.inspect_array_sharding(x, callback=_cb)
-
-
 def _inspect(name: str, tree: jax.Array):
+
+    def _inspect_(name: str, x: jax.Array):
+        """
+        Internal function to inspect the sharding of an array. To be used for debugging inside
+        of :func:`jax.jit`-ted functions.
+
+        Args:
+            name: A string to identify the array, usually the name, but can contain anything else.
+            x: The array
+        """
+        if mode() == "sharding":
+
+            def _cb(y):
+                if process_index() == 0:
+                    print(
+                        f"{name}: shape={x.shape}, sharding:",
+                        y,
+                        flush=True,
+                    )
+
+            jax.debug.inspect_array_sharding(x, callback=_cb)
+
     if isinstance(tree, jax.Array):
         _inspect_(name, tree)
     else:
--- netket/_src/ngd/srt.py
+++ netket/_src/ngd/srt.py
@@ -6,10 +6,10 @@ import jax.numpy as jnp
 from jax.sharding import PositionalSharding
 
 from netket import jax as nkjax
-from netket.utils import mpi
+from netket import config
 from netket.utils.types import Array
 
-from netket._src import distributed as distributed
+from netket._src import distributed
 
 
 @partial(
@@ -36,49 +36,50 @@ def _compute_srt_update(
 
     # Equivalent to MPI.alltoall, shards the data across axis 1
     # (#ns, np) -> (ns, #np)
-    O_LT, token = distributed.reshard(
-        O_L, sharded_axis=0, out_sharded_axis=1, pad=True, pad_value=0.0
-    )
-    dv, token = distributed.allgather(dv, token=token)
+    O_LT = O_L
+    if config.netket_experimental_sharding:
+        distributed.pad_axis_for_sharding(O_LT, axis=1, padding_value=0.0)
+        O_LT = jax.lax.with_sharding_constraint(
+            O_LT,
+            PositionalSharding(jax.devices()).reshape(1, -1),
+        )
+        dv = jax.lax.with_sharding_constraint(
+            dv, PositionalSharding(jax.devices()).replicate()
+        )
 
     # This does the contraction (ns, #np) x (#np, ns) -> (ns, ns).
     # When using sharding the sum over #ns is done automatically.
     # When using MPI we need to do it manually with an allreduce_sum.
-    matrix, token = mpi.mpi_reduce_sum_jax(O_LT @ O_LT.T, root=0, token=token)
+    matrix = O_LT @ O_LT.T
     matrix_side = matrix.shape[-1]  # * it can be ns or 2*ns, depending on mode
 
-    if mpi.rank == 0:
+    shifted_matrix = jax.lax.add(
+        matrix, diag_shift * jnp.eye(matrix_side, dtype=matrix.dtype)
+    )
+    # replicate
+
+    if proj_reg is not None:
         shifted_matrix = jax.lax.add(
-            matrix, diag_shift * jnp.eye(matrix_side, dtype=matrix.dtype)
+            shifted_matrix, jnp.full_like(shifted_matrix, proj_reg / matrix_side)
         )
 
-        if proj_reg is not None:
-            shifted_matrix = jax.lax.add(
-                shifted_matrix, jnp.full_like(shifted_matrix, proj_reg / matrix_side)
-            )
+    aus_vector = solver_fn(shifted_matrix, dv)
 
-        aus_vector = solver_fn(shifted_matrix, dv)
-
-        # Some solvers return a tuple, some others do not.
-        if isinstance(aus_vector, tuple):
-            aus_vector, info = aus_vector
-        else:
+    # Some solvers return a tuple, some others do not.
+    if isinstance(aus_vector, tuple):
+        aus_vector, info = aus_vector
+        if info is None:
             info = {}
-
-        aus_vector = aus_vector.reshape(mpi.n_nodes, -1)
-        aus_vector, token = mpi.mpi_scatter_jax(aus_vector, root=0, token=token)
     else:
-        aus_vector = jnp.zeros((int(matrix_side / mpi.n_nodes),), dtype=jnp.float64)
-        aus_vector, token = mpi.mpi_scatter_jax(aus_vector, root=0, token=token)
-        info = None
-
-    if info is None:
         info = {}
+
+    # aus_vector, token = mpi.mpi_scatter_jax(aus_vector, root=0, token=token)
 
     # (np, #ns) x (#ns) -> (np).
     # The sum over #ns is done automatically in sharding.
     # Under MPI we need to do it manually with an allreduce_sum.
-    updates, token = mpi.mpi_allreduce_sum_jax(O_L.T @ aus_vector, token=token)
+    # updates, token = mpi.mpi_allreduce_sum_jax(O_L.T @ aus_vector, token=token)
+    updates = O_L.T @ aus_vector
     if momentum is not None:
         updates += momentum * old_updates
         old_updates = updates
@@ -89,7 +90,7 @@ def _compute_srt_update(
         num_p = updates.shape[-1] // 2
         updates = updates[:num_p] + 1j * updates[num_p:]
 
-    if distributed.mode() == "sharding":
+    if config.netket_experimental_sharding:
         out_shardings = (
             PositionalSharding(jax.devices()).replicate().reshape((1,) * updates.ndim)
         ).replicate()
