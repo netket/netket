4638abbe24b1bcff3e55bc60ba19db2e617f9936
Adrien Kahn <adrien.kaet@gmail.com>
2025-07-23 15:31:04 +0200

    Add test for SumDiscreteJaxOperator

---
+++ test/operator/test_sum_operator.py
@@ -0,0 +1,49 @@
+import pytest
+
+import jax.numpy as jnp
+
+import netket as nk
+
+
+hilbert_space = nk.hilbert.Qubit(3)
+graph = nk.graph.Chain(hilbert_space.size)
+
+translation_1 = nk.utils.group.Permutation(
+    permutation_array=jnp.array([1, 2, 0]), name="translation_1"
+)
+transposition_12 = nk.utils.group.Permutation(
+    permutation_array=jnp.array([0, 2, 1]), name="transposition_12"
+)
+
+sigmax_local = nk.operator.spin.sigmax(hilbert_space, 0)
+sigmaz_pauli = nk.operator.spin.sigmaz(hilbert_space, 0).to_pauli_strings()
+ising = nk.operator.IsingJax(hilbert_space, graph, 1, 1)
+translation_1_operator = nk.symmetry.PermutationOperator(hilbert_space, translation_1)
+transposition_12_operator = nk.symmetry.PermutationOperator(
+    hilbert_space, transposition_12
+)
+
+operator_list = [
+    sigmax_local,
+    sigmaz_pauli,
+    ising,
+    translation_1_operator,
+    transposition_12_operator,
+]
+
+coefficient_list = [[1, 1.0j], [1.0j, -jnp.real(3.5)]]
+
+
+@pytest.mark.parametrize("operator_1", operator_list)
+@pytest.mark.parametrize("operator_2", operator_list)
+@pytest.mark.parametrize("coefficient", coefficient_list)
+def test_operator_sum(operator_1, operator_2, coefficient):
+
+    sum_op = coefficient[0] * operator_1 + coefficient[1] * operator_2
+
+    sum_dense_1 = sum_op.to_dense()
+    sum_dense_2 = (
+        coefficient[0] * operator_1.to_dense() + coefficient[1] * operator_2.to_dense()
+    )
+
+    assert jnp.linalg.norm(sum_dense_1 - sum_dense_2) < 1e-14
