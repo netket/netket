1a9c876d4305a2f52f7a7bb0829c74f91ba61e0c
Clemens Giuliani <clemens@inailuig.it>
2025-07-24 16:35:36 +0200

    cleanup

--- test_sharding/run_standard_tests_with_sharding.sh
+++ test_sharding/run_standard_tests_with_sharding.sh
@@ -3,6 +3,4 @@ export NETKET_EXPERIMENTAL_SHARDING=1
 export NETKET_EXPERIMENTAL_SHARDING_CPU=2
 export JAX_PLATFORM_NAME=cpu
 
-echo $@
-
-python3 -m pytest -n 0 "${@}" test
+python3 -m pytest -n 0 $@ test
