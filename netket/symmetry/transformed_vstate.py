0877e0e17c356004414c7292fc3154af3b28ee2c
Adrien Kahn <adrien.kaet@gmail.com>
2025-07-28 19:55:54 +0200

    Small fixes

--- netket/symmetry/transformed_vstate.py
+++ netket/symmetry/transformed_vstate.py
@@ -5,50 +5,55 @@ from netket import jax as nkjax
 from netket.operator import ContinuousOperator
 from netket.vqs import MCState, FullSumState
 
+# Eventually, we should have classes TransformedMCState and TransformedFullSumState that allow access to the underlying operator and model.
+# In this implementation, we have access to the operator in the new variables, but not to the original model/apply_fun.
 
-# chunk_size could be divided by the max number of connected elements of operator
+
 def apply_operator(operator, vstate):
 
+    if not isinstance(vstate, (FullSumState, MCState)):
+        raise TypeError("vstate must be either an MCState or a FullSumState.")
+
+    transformed_apply_fun, new_variables = make_logpsi_op_afun(
+        vstate._apply_fun, operator, vstate.variables
+    )
+    chunk_size = (
+        None
+        if vstate.chunk_size is None
+        else vstate.chunk_size / operator.max_conn_size
+    )
+
     if isinstance(vstate, FullSumState):
-        transformed_apply_fun, new_variables = make_logpsi_U_afun(
-            vstate._apply_fun, operator, vstate.variables
-        )
         transformed_vstate = FullSumState(
             hilbert=vstate.hilbert,
             apply_fun=transformed_apply_fun,
             variables=new_variables,
-            chunk_size=vstate.chunk_size,
+            chunk_size=chunk_size,
         )
         return transformed_vstate
 
     elif isinstance(vstate, MCState):
-        transformed_apply_fun, new_variables = make_logpsi_U_afun(
-            vstate._apply_fun, operator, vstate.variables
-        )
         transformed_vstate = MCState(
             sampler=vstate.sampler,
             apply_fun=transformed_apply_fun,
             variables=new_variables,
-            n_samples=vstate.n_samples,  # Is it fine to not pass n_samples_per_rank?
+            n_samples=vstate.n_samples,
             n_discard_per_chain=vstate.n_discard_per_chain,
-            chunk_size=vstate.chunk_size,
+            chunk_size=chunk_size,
         )
         return transformed_vstate
 
-    else:
-        raise TypeError("vstate must be either an MCState or a FullSumState.")
 
+def make_logpsi_op_afun(logpsi_fun, operator, variables):
+    """Wraps an apply_fun into another one that multiplies it by an operator.
 
-def make_logpsi_U_afun(logpsi_fun, U, variables):
-    """Wraps an apply_fun into another one that multiplies it by an Unitary transformation U.
-
-    This wrapper is made such that the Unitary is passed as the model_state
+    This wrapper is made such that the operator is passed as the model_state
     of the new wrapped function, and therefore changes to the angles/coefficients
-    of the Unitary should not trigger recompilation.
+    of the operator should not trigger recompilation.
 
     Args:
         logpsi_fun: a function that takes as input variables and samples
-        U: a {class}`nk.operator.JaxDiscreteOperator`
+        oeprator: a {class}`nk.operator.JaxDiscreteOperator`
         variables: The variables used to call *logpsi_fun*
 
     Returns:
@@ -56,28 +61,28 @@ def make_logpsi_U_afun(logpsi_fun, U, variables):
         the original **logpsi_fun** and a set of new variables to be used to call it.
 
     """
-    # wrap apply_fun into logpsi logpsi_U
-    logpsiU_fun = nkjax.HashablePartial(_logpsi_U_fun, logpsi_fun)
+    # Wrap logpsi into logpsi_op
+    logpsi_op_fun = nkjax.HashablePartial(_logpsi_op_fun, logpsi_fun)
 
-    # Insert a new 'model_state' key to store the Unitary. This only works
-    # if U is a pytree that can be flattened/unflattened.
-    new_variables = flax.core.copy(variables, {"unitary": U})
+    # Insert a new 'operator' key to store the operator. This only works
+    # if operator is a pytree that can be flattened/unflattened.
+    new_variables = flax.core.copy(variables, {"operator": operator})
 
-    return logpsiU_fun, new_variables
+    return logpsi_op_fun, new_variables
 
 
-def _logpsi_U_fun(apply_fun, variables, x, *args):
+def _logpsi_op_fun(apply_fun, variables, x, *args):
     """
     This should be used as a wrapper to the original apply function, adding
-    to the `variables` dictionary (in model_state) a new key `unitary` with
+    to the `variables` dictionary (in model_state) a new key `operator` with
     a jax-compatible operator.
     """
-    variables_applyfun, U = flax.core.pop(variables, "unitary")
+    variables_applyfun, operator = flax.core.pop(variables, "operator")
 
-    if isinstance(U, ContinuousOperator):
-        res = U._expect_kernel(apply_fun, variables_applyfun, x)
+    if isinstance(operator, ContinuousOperator):
+        res = operator._expect_kernel(apply_fun, variables_applyfun, x)
     else:
-        xp, mels = U.get_conn_padded(x)
+        xp, mels = operator.get_conn_padded(x)
         xp = xp.reshape(-1, x.shape[-1])
         logpsi_xp = apply_fun(variables_applyfun, xp, *args)
         logpsi_xp = logpsi_xp.reshape(mels.shape).astype(complex)
@@ -86,21 +91,22 @@ def _logpsi_U_fun(apply_fun, variables, x, *args):
     return res
 
 
+# Is it useful?
 def _lazy_apply_UV_to_afun(vstate, operator, extra_hash_data=None):
-    ψ_logfun = vstate._apply_fun
-    ψ_vars = vstate.variables
+    psi_logfun = vstate._apply_fun
+    psi_vars = vstate.variables
 
     if operator is None:
-        return ψ_logfun, ψ_vars, vstate.model
+        return psi_logfun, psi_vars, vstate.model
     else:
-        Uψ_logfun, Uψ_vars = make_logpsi_U_afun(ψ_logfun, operator, ψ_vars)
+        Upsi_logfun, Upsi_vars = make_logpsi_op_afun(psi_logfun, operator, psi_vars)
 
     # fix the has to include forward or backward info
     # we use this to give a different hash to 'forward' and 'backward'
     # distributions, even if they are identical, and only differ in the
     # parameters.
     if extra_hash_data is not None:
-        Uψ_logfun.__hash__()
-        Uψ_logfun._hash = hash((Uψ_logfun._hash, extra_hash_data))
+        Upsi_logfun.__hash__()
+        Upsi_logfun._hash = hash((Upsi_logfun._hash, extra_hash_data))
 
-    return Uψ_logfun, Uψ_vars, Uψ_logfun
+    return Upsi_logfun, Upsi_vars, Upsi_logfun
