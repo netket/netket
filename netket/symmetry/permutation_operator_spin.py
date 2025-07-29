774af7442c18b95944dbdd05d7cd17a8f8125296
Adrien Kahn <adrien.kaet@gmail.com>
2025-07-29 21:53:52 +0200

    Add PermutationOperatorBase and fix some other issues

--- netket/symmetry/permutation_operator_spin.py
+++ netket/symmetry/permutation_operator_spin.py
@@ -1,48 +1,20 @@
 import jax.numpy as jnp
-
 from jax.tree_util import register_pytree_node_class
 
-from netket.operator import DiscreteJaxOperator
-from netket.utils.group import Permutation, Identity
+from netket.symmetry import PermutationOperatorBase
 
 
 @register_pytree_node_class
-class PermutationOperator(DiscreteJaxOperator):
+class PermutationOperator(PermutationOperatorBase):
     """
-    Write something
+    Permutation operator on a spin or boson space.
+    For mathematical details on the definition of a permutation operator
+    and its justification, we refer to [SOMEWHERE].
+
+    Args:
+        hilbert: The Hilbert space.
+        permutation: The permutation represented by the operator.
     """
-
-    def __init__(self, hilbert, permutation):
-        super().__init__(hilbert)
-        if isinstance(permutation, Identity):
-            permutation = Permutation(
-                permutation_array=jnp.arange(hilbert.size), name="Identity"
-            )
-
-        if not isinstance(permutation, Permutation):
-            raise TypeError("permutation must be a Permutation object.")
-        if not hilbert.size == permutation.permutation_array.size:
-            raise ValueError(
-                "Permutation size does not correspond to Hilbert space size."
-            )
-
-        self.permutation = permutation
-
-    def tree_flatten(self):
-        struct_data = {"hilbert": self.hilbert, "permutation": self.permutation}
-        return (), struct_data
-
-    @classmethod
-    def tree_unflatten(cls, struct_data, array_data):
-        return cls(**struct_data)
-
-    @property
-    def max_conn_size(self) -> int:
-        return 1
-
-    @property
-    def dtype(self):
-        return jnp.float32
 
     def __repr__(self):
         if self.permutation._name is not None:
@@ -57,9 +29,6 @@ class PermutationOperator(DiscreteJaxOperator):
             )
         else:
             return False
-
-    def __hash__(self):
-        return hash((self.hilbert, self.permutation))
 
     def get_conn_padded(self, x):
         x = jnp.asarray(x)
