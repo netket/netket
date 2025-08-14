625974625811f81975fe9f2ac38e5823d7c399e1
Adrien Kahn <adrien.kaet@gmail.com>
2025-08-14 21:00:55 +0200

    Comment work in progress

--- netket/_src/symmetry/representation.py
+++ netket/_src/symmetry/representation.py
@@ -2,9 +2,8 @@ import numpy as np
 import jax.numpy as jnp
 
 from functools import reduce
-from itertools import product
 
-from netket.utils.group import Element, FiniteGroup, Identity
+from netket.utils.group import Element, FiniteGroup
 from netket.operator import DiscreteJaxOperator
 
 
@@ -126,56 +125,56 @@ class Representation:
 
         return cob_matrix, np.array(irrep_dims)
 
-    def construct_commuting_product(self, other, check_commute: bool = False):
-        """
-        Construct the product representation in the case where all operators of one representation
-        commute with all operators of the other.
-        It is a representation of the direct product of the underlying groups.
-        """
-        if check_commute:
-            assert self.is_commuting(other)
+    # def construct_commuting_product(self, other, check_commute: bool = False):
+    #     """
+    #     Construct the product representation in the case where all operators of one representation
+    #     commute with all operators of the other.
+    #     It is a representation of the direct product of the underlying groups.
+    #     """
+    #     if check_commute:
+    #         assert self.is_commuting(other)
 
-        # group_direct_product = self.group @ other.group
-        # operator_products = {G1*G2: self.representation_mapping[g1] * other.representation_mapping[g2] for
-        #         g1 in self.group for g2 in other.group}
+    #     # group_direct_product = self.group @ other.group
+    #     # operator_products = {G1*G2: self.representation_mapping[g1] * other.representation_mapping[g2] for
+    #     #         g1 in self.group for g2 in other.group}
 
-        # return Representation(group_direct_product, self.hilbert_space, operator_products)
+    #     # return Representation(group_direct_product, self.hilbert_space, operator_products)
 
-    def is_commuting(self, other):
-        """
-        Check whether all operators of one representation commute with all operators of
-        the other.
-        """
-        for operator_1 in self.representation_dict.values():
-            for operator_2 in other.representation_dict.values():
-                assert operator_1 @ operator_2 - operator_2 @ operator_1 == 0
+    # def is_commuting(self, other):
+    #     """
+    #     Check whether all operators of one representation commute with all operators of
+    #     the other.
+    #     """
+    #     for operator_1 in self.representation_dict.values():
+    #         for operator_2 in other.representation_dict.values():
+    #             assert operator_1 @ operator_2 - operator_2 @ operator_1 == 0
 
-    # We might want a fast mode where only the fast checks are made,
-    # and an exhaustive mode where all checks are made.
-    def check_representation(self):
-        """
-        Check whether the representation is valid by checking that the
-        representation properties are satisfied.
-        """
+    # # We might want a fast mode where only the fast checks are made,
+    # # and an exhaustive mode where all checks are made.
+    # def check_representation(self):
+    #     """
+    #     Check whether the representation is valid by checking that the
+    #     representation properties are satisfied.
+    #     """
 
-        is_representation = True
+    #     is_representation = True
 
-        # Identity property
-        for g in self.group:
-            if isinstance(g, Identity):
-                if (
-                    not jnp.linalg.norm(
-                        self[g].to_dense - jnp.eye(self.hilbert_space.n_states)
-                    )
-                    < 1e-14
-                ):
-                    is_representation = False
+    #     # Identity property
+    #     for g in self.group:
+    #         if isinstance(g, Identity):
+    #             if (
+    #                 not jnp.linalg.norm(
+    #                     self[g].to_dense - jnp.eye(self.hilbert_space.n_states)
+    #                 )
+    #                 < 1e-14
+    #             ):
+    #                 is_representation = False
 
-        # Compatibility property
-        for g_1, g_2 in product(self.group, self.group):
-            product_inside = self[g_1 @ g_2]
-            product_outside = self[g_1] @ self[g_2]
-            if not product_inside == product_outside:
-                is_representation = False
+    #     # Compatibility property
+    #     for g_1, g_2 in product(self.group, self.group):
+    #         product_inside = self[g_1 @ g_2]
+    #         product_outside = self[g_1] @ self[g_2]
+    #         if not product_inside == product_outside:
+    #             is_representation = False
 
-        return is_representation
+    #     return is_representation
