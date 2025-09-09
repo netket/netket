a366ea8f214b0e399c8c7ed07672d315c40786ba
Filippo Vicentini <filippovicentini@gmail.com>
2025-09-09 22:01:01 +0200

    fix

--- test_sharding/run_standard_tests_with_sharding.sh
+++ test_sharding/run_standard_tests_with_sharding.sh
@@ -2,4 +2,4 @@
 export NETKET_EXPERIMENTAL_SHARDING=1
 export NETKET_EXPERIMENTAL_SHARDING_CPU=2
 export JAX_PLATFORM_NAME=cpu
-python3 -m pytest -n 0 "$@" test
+python3 -m pytest --cov=netket --cov-append -n 0 "$@" test
