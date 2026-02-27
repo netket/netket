a372b77c1bd7a381319531bbe6338a1cc80ffbc1
Filippo Vicentini <filippovicentini@gmail.com>
2026-02-27 22:48:31 +0100

    fix test

--- test/optimizer/test_qgt_logic.py
+++ test/optimizer/test_qgt_logic.py
@@ -544,14 +544,14 @@ def test_qgt_jacobian_imaginary(dense):
     srd = Sr.to_dense()
     sid = Si.to_dense()
     n = shd.shape[0]
-    np.testing.assert_allclose(srd[:n, :n], shd.real)
+    np.testing.assert_allclose(srd[:n, :n], shd.real, atol=1e-14)
     np.testing.assert_allclose(srd[n:, :n], shd.imag, atol=1e-14)
     np.testing.assert_allclose(srd[:n, n:], -shd.imag, atol=1e-14)
     np.testing.assert_allclose(srd[n:, n:], shd.real, atol=1e-14)
 
     np.testing.assert_allclose(sid[:n, :n], shd.imag, atol=1e-14)
-    np.testing.assert_allclose(sid[:n, n:], shd.real)
-    np.testing.assert_allclose(sid[n:, :n], -shd.real)
+    np.testing.assert_allclose(sid[:n, n:], shd.real, atol=1e-14)
+    np.testing.assert_allclose(sid[n:, :n], -shd.real, atol=1e-14)
     np.testing.assert_allclose(sid[n:, n:], shd.imag, atol=1e-14)
 
 
