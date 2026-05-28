2cb7124e7f867267f8f202061050e69219aced0e
Filippo Vicentini <filippovicentini@gmail.com>
2026-05-28 09:22:34 +0200

    remove NETKET_EXPERIMENTAL_SHARDING from some tests

--- test_sharding/run_standard_tests_with_sharding.sh
+++ test_sharding/run_standard_tests_with_sharding.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export NETKET_EXPERIMENTAL_SHARDING=1
 export NETKET_EXPERIMENTAL_SHARDING_CPU=2
 export JAX_PLATFORM_NAME=cpu
 python3 -m pytest --cov=netket --cov-append -n 0 "$@" test
--- test_sharding/run_test_sharding_1cpu.sh
+++ test_sharding/run_test_sharding_1cpu.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-export NETKET_EXPERIMENTAL_SHARDING=1
 export JAX_PLATFORM_NAME=cpu
 python3 -m pytest --cov=netket --cov-append -p no:warnings --color=yes --verbose -n 0 --tb=short "$@" test/sharding/test_sharding.py
